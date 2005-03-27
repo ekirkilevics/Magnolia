30b07ab5aef33e447b5a3fff237ff49db204c878
Fabrizio Giustina <fgiust@openmindonline.it>
2005-03-27 17:30:10 +0000

    allow use of "level" alone
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@453 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- src/main/info/magnolia/cms/taglibs/LoadPage.java
+++ src/main/info/magnolia/cms/taglibs/LoadPage.java
@@ -31,7 +31,7 @@ import org.apache.log4j.Logger;
  * the corresponding unloadPage-tag will not revert to the previously loaded page, but restore actpage to the currently
  * displayed page, i.e. the value it held before loadPage was called for the first time.
  * @author Marcel Salathe
- * @version $Revision$ ($Author$)
+ * @version $Revision: 364 $ ($Author: fgiust $)
  */
 public class LoadPage extends BodyTagSupport {
 
@@ -99,19 +99,29 @@ public class LoadPage extends BodyTagSupport {
             }
             catch (Exception e) {
                 log.error(e.getMessage());
-                return SKIP_BODY;
+                return EVAL_PAGE;
             }
         }
-        if (StringUtils.isNotEmpty(this.path)) {
+        else if (StringUtils.isNotEmpty(this.path)) {
             try {
                 newActpage = SessionAccessControl.getHierarchyManager(req).getPage(this.path);
             }
             catch (Exception e) {
-                return SKIP_BODY;
+                log.error(e.getMessage());
+                return EVAL_PAGE;
+            }
+        }
+        else {
+            try {
+                newActpage = Resource.getCurrentActivePage(req).getAncestor(this.level);
+            }
+            catch (Exception e) {
+                log.error(e.getMessage());
+                return EVAL_PAGE;
             }
         }
         pageContext.setAttribute(Aggregator.CURRENT_ACTPAGE, newActpage, PageContext.REQUEST_SCOPE);
-        return EVAL_BODY_INCLUDE;
+        return EVAL_PAGE;
     }
 
     /**
