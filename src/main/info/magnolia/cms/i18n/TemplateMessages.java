be57b67100a77af04c63725afbb4f73ae99ead80
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2005-04-01 10:04:08 +0000

    i18n
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@475 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ src/main/info/magnolia/cms/i18n/TemplateMessages.java
@@ -0,0 +1,44 @@
+/*
+ * Created on Mar 30, 2005
+ *
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2005 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
+ */
+package info.magnolia.cms.i18n;
+
+import info.magnolia.cms.gui.dialog.DialogSuper;
+
+/**
+ * @author philipp
+ *
+ * This class helps to get the messages. First it make a lookup in messages_templating_custom and then in messages_templating..
+ */
+public class TemplateMessages {
+    public static String DEFAULT_BASENAME = "info.magnolia.modules.admininterface.messages_templating";
+    public static String CUSTOM_BASENAME = "info.magnolia.modules.admininterface.messages_templating_custom";
+    
+    public static String get(DialogSuper dialog, String key) {
+        Messages msgs = ContextMessages.getInstanceSavely(dialog.getRequest());
+        String msg = msgs.getWithDefault(key, DEFAULT_BASENAME, key);
+        if(!msg.equals(key)){
+            return msg;
+        }
+        return msgs.getWithDefault(key, CUSTOM_BASENAME, key);
+    }
+
+    public static String get(DialogSuper dialog, String key, Object[] args) {
+        Messages msgs = ContextMessages.getInstanceSavely(dialog.getRequest());
+        String msg = msgs.getWithDefault(key, DEFAULT_BASENAME, args, key);
+        if(!msg.equals(key)){
+            return msg;
+        }
+        return msgs.getWithDefault(key, CUSTOM_BASENAME, args, key);
+    }
+}
---
+++ src/webapp/admintemplates/js/messages.jsp
@@ -0,0 +1,7 @@
+<jsp:root version="1.2" xmlns:jsp="http://java.sun.com/JSP/Page">
+	<jsp:directive.page language="java"/>
+	<jsp:directive.page import="info.magnolia.cms.i18n.ContextMessages"/>
+	<jsp:expression>
+		ContextMessages.getInstanceSavely(request).generateJavaScript() 
+	</jsp:expression>
+</jsp:root>
