d236fbddf56bbe4e7c3fa68bc3c94a3a7a51318b
Sameer Charles <sameer.charles@magnolia-cms.com>
2005-04-04 16:30:16 +0000

    search package
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@479 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- src/main/info/magnolia/cms/core/search/QueryResult.java
+++ src/main/info/magnolia/cms/core/search/QueryResult.java
@@ -1,6 +1,17 @@
+/**
+ *
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2005 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
+ */
 package info.magnolia.cms.core.search;
 
-import info.magnolia.cms.security.AccessManager;
 
 import java.util.Iterator;
 
