f166780218d46378babe2903cd35e9def3ce9f90
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2005-04-20 13:57:04 +0000

    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@532 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ src/main/info/magnolia/cms/taglibs/PublicOnly.java
@@ -0,0 +1,50 @@
+/**
+ *
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2005 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
+ */
+package info.magnolia.cms.taglibs;
+
+import info.magnolia.cms.beans.config.Server;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.tagext.TagSupport;
+
+
+/**
+ * @author Sameer Charles
+ * @version $Revision: 364 $ ($Author: fgiust $)
+ */
+public class PublicOnly extends TagSupport {
+
+    /**
+     * Stable serialVersionUID.
+     */
+    private static final long serialVersionUID = 222L;
+
+    /**
+     * @see javax.servlet.jsp.tagext.Tag#doStartTag()
+     */
+    public int doStartTag() {
+        HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
+        String prev = (String) request.getSession().getAttribute("mgnlPreview");
+        // if (Server.isAdmin() && !Resource.showPreview((HttpServletRequest)pageContext.getRequest()))
+        if (!Server.isAdmin() || prev != null) {
+            return EVAL_BODY_INCLUDE;
+        }
+        return SKIP_BODY;
+    }
+
+    /**
+     * @see javax.servlet.jsp.tagext.Tag#doEndTag()
+     */
+    public int doEndTag() {
+        return EVAL_PAGE;
+    }
+}
