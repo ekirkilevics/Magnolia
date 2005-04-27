dec2aec822d9c37abf3dcedc4787403a88141929
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2005-04-27 13:06:05 +0000

    i18n user language
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@593 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- src/main/info/magnolia/cms/i18n/MessagesManager.java
+++ src/main/info/magnolia/cms/i18n/MessagesManager.java
@@ -28,201 +28,208 @@ import javax.jcr.RepositoryException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.jstl.core.Config;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
+
 /**
- * @author philipp From this class you get the i18n messages. You should pass a
- *         a request, but if you can't the getMessages method will handle it
- *         properly. The get() methods are easy to use.
+ * @author philipp From this class you get the i18n messages. You should pass a a request, but if you can't the
+ * getMessages method will handle it properly. The get() methods are easy to use.
  */
 public class MessagesManager {
 
-	protected static Logger log = Logger.getLogger(Messages.class);
+    protected static Logger log = Logger.getLogger(Messages.class);
 
-	public static String FALLBACK_LOCALE = "en";
+    public static String FALLBACK_LOCALE = "en";
 
-	public static String DEFAULT_BASENAME = "info.magnolia.module.admininterface.messages";
+    public static String DEFAULT_BASENAME = "info.magnolia.module.admininterface.messages";
 
-	private static String I18N_CONFIG_NAME = "i18n";
+    private static String I18N_CONFIG_NAME = "i18n";
 
-	private static String LOCALE_CONFIG_NAME = "language";
+    private static String LOCALE_CONFIG_NAME = "language";
 
-	private static String AVAILABLE_LOCALES_CONFIG_NAME = "availableLanguages";
+    private static String AVAILABLE_LOCALES_CONFIG_NAME = "availableLanguages";
 
-	private static Locale applicationLocale;
+    private static Locale applicationLocale;
 
-	private static Collection availableLocales = new ArrayList();
+    private static Collection availableLocales = new ArrayList();
 
-	private static ServletConfig config;
+    private static ServletConfig config;
 
-	/**
-	 * Called through the initialization precess
-	 */
-	public static void init(ServletConfig config) {
-		MessagesManager.config = config;
-		ServletContext context = config.getServletContext();
-		// setting fallback
-		context.setAttribute(Config.FMT_FALLBACK_LOCALE + ".application",
-				FALLBACK_LOCALE);
+    /**
+     * Called through the initialization precess
+     */
+    public static void init(ServletConfig config) {
+        MessagesManager.config = config;
+        ServletContext context = config.getServletContext();
+        // setting fallback
+        context.setAttribute(Config.FMT_FALLBACK_LOCALE + ".application", FALLBACK_LOCALE);
 
-		// setting basename
-		context.setAttribute(Config.FMT_LOCALIZATION_CONTEXT + ".application",
-				MessagesManager.DEFAULT_BASENAME);
+        // setting basename
+        context.setAttribute(Config.FMT_LOCALIZATION_CONTEXT + ".application", MessagesManager.DEFAULT_BASENAME);
 
-		// setting default language (en)
-		MessagesManager.setDefaultLocale(FALLBACK_LOCALE);
+        // setting default language (en)
+        MessagesManager.setDefaultLocale(FALLBACK_LOCALE);
 
-		// reading the configuration from the repository
-		HierarchyManager configHierarchyManager = ContentRepository
-				.getHierarchyManager(ContentRepository.CONFIG);
-		try {
-			log.info("Config : loading i18n configuration - "
-					+ I18N_CONFIG_NAME);
-			
-			Content serverNode = configHierarchyManager.getContent("/server");
-			
-			Content configNode;
-			try{
-				configNode = serverNode.getContent(I18N_CONFIG_NAME);
-			}
-			catch(javax.jcr.PathNotFoundException  e) {
-				configNode = serverNode.createContent(I18N_CONFIG_NAME, ItemType.NT_CONTENTNODE);
-				configHierarchyManager.save();
-			}
-			
-			NodeData languageNodeData = configNode.getNodeData(LOCALE_CONFIG_NAME);
-			
-			if (languageNodeData.getName() == "") {
-				languageNodeData = configNode.createNodeData(LOCALE_CONFIG_NAME);
-				languageNodeData.setValue(MessagesManager.FALLBACK_LOCALE);
-				configHierarchyManager.save();
-			}
+        // reading the configuration from the repository
+        HierarchyManager configHierarchyManager = ContentRepository.getHierarchyManager(ContentRepository.CONFIG);
+        try {
+            log.info("Config : loading i18n configuration - " + I18N_CONFIG_NAME);
 
-			MessagesManager.setDefaultLocale(languageNodeData.getString());
+            Content serverNode = configHierarchyManager.getContent("/server");
 
-			// get the available languages
-			Content availableLanguagesContentNode;
+            Content configNode;
+            try {
+                configNode = serverNode.getContent(I18N_CONFIG_NAME);
+            }
+            catch (javax.jcr.PathNotFoundException e) {
+                configNode = serverNode.createContent(I18N_CONFIG_NAME, ItemType.NT_CONTENTNODE);
+                configHierarchyManager.save();
+            }
 
-			NodeData availableLanguage;
-			
-			try{
-				availableLanguagesContentNode = configNode.getContent(AVAILABLE_LOCALES_CONFIG_NAME);
-			}
-			catch(javax.jcr.PathNotFoundException  e) {
-				availableLanguagesContentNode = configNode.createContent(AVAILABLE_LOCALES_CONFIG_NAME, ItemType.NT_CONTENTNODE);
-				availableLanguage = availableLanguagesContentNode.createNodeData(MessagesManager.FALLBACK_LOCALE);
-				availableLanguage.setValue(MessagesManager.FALLBACK_LOCALE);
-				configHierarchyManager.save();
-			}
-			
-			Collection locales = availableLanguagesContentNode.getNodeDataCollection();
-			for (Iterator iter = locales.iterator(); iter.hasNext();) {
-				availableLanguage = (NodeData) iter.next();
-                MessagesManager.availableLocales.add(availableLanguage.getString());
-			}
+            NodeData languageNodeData = configNode.getNodeData(LOCALE_CONFIG_NAME);
+            
+            if (languageNodeData.getName() == "") {
+                languageNodeData = configNode.createNodeData(LOCALE_CONFIG_NAME);
+                languageNodeData.setValue(MessagesManager.FALLBACK_LOCALE);
+                configHierarchyManager.save();
+            }
 
-		} catch (RepositoryException re) {
-			log.error("Config : Failed to load i18n configuration - "
-					+ I18N_CONFIG_NAME);
-			log.error(re.getMessage(), re);
-		}
+            MessagesManager.setDefaultLocale(languageNodeData.getString());
 
-	}
+            // get the available languages
+            Content availableLanguagesContentNode;
 
-	/**
-	 * Trys to make a new ContextMessages object. if not possible it creates a
-	 * new Messages object.
-	 * 
-	 * @return Messages
-	 */
-	public static Messages getMessages(HttpServletRequest req) {
-		if (req != null) {
-			return new ContextMessages(req);
-		} else {
-			log.debug("using i18n-messages without a request!");
-			return new Messages(MessagesManager.DEFAULT_BASENAME,
-					applicationLocale);
-		}
-	}
+            NodeData availableLanguage;
 
-	public static Messages getMessages(HttpServletRequest req, String basename) {
-		if (req != null) {
-			return new ContextMessages(req, basename);
-		} else {
-			log.debug("using i18n-messages without a request!");
-			return new Messages(basename, applicationLocale);
-		}
-	}
+            try {
+                availableLanguagesContentNode = configNode.getContent(AVAILABLE_LOCALES_CONFIG_NAME);
+            }
+            catch (javax.jcr.PathNotFoundException e) {
+                availableLanguagesContentNode = configNode.createContent(
+                    AVAILABLE_LOCALES_CONFIG_NAME,
+                    ItemType.NT_CONTENTNODE);
+                
+                availableLanguage = availableLanguagesContentNode.createNodeData("de");
+                availableLanguage.setValue("de");
+                availableLanguage = availableLanguagesContentNode.createNodeData(MessagesManager.FALLBACK_LOCALE);
+                availableLanguage.setValue(MessagesManager.FALLBACK_LOCALE);
+                configHierarchyManager.save();
+            }
 
-	public static Messages getMessages(HttpServletRequest req, String basename,
-			Locale locale) {
-		if (req != null) {
-			return new ContextMessages(req, basename, locale);
-		} else {
-			log.debug("using i18n-messages without a request!");
-			return new Messages(basename, locale);
-		}
-	}
+            Collection locales = availableLanguagesContentNode.getNodeDataCollection();
+            for (Iterator iter = locales.iterator(); iter.hasNext();) {
+                availableLanguage = (NodeData) iter.next();
+                MessagesManager.availableLocales.add(new Locale(availableLanguage.getString()));
+            }
 
-	/**
-	 * Trys to make a new ContextMessages object. if not possible it creates a
-	 * new Messages object.
-	 * 
-	 * @return Messages
-	 */
-	public static Messages getMessages(PageContext pc) {
-		if (pc != null && pc.getRequest() instanceof HttpServletRequest) {
-			return new ContextMessages((HttpServletRequest) pc.getRequest());
-		} else {
-			log
-					.debug("using i18n-messages without a request inside a control!");
-			return new Messages(MessagesManager.DEFAULT_BASENAME,
-					applicationLocale);
-		}
-	}
+        }
+        catch (RepositoryException re) {
+            log.error("Config : Failed to load i18n configuration - " + I18N_CONFIG_NAME);
+            log.error(re.getMessage(), re);
+        }
 
-	public static String get(HttpServletRequest req, String key) {
-		return getMessages(req).get(key);
-	}
+    }
 
-	public static String get(HttpServletRequest req, String key, Object[] args) {
-		return getMessages(req).get(key, args);
-	}
+    /**
+     * Trys to make a new ContextMessages object. if not possible it creates a new Messages object.
+     * @return Messages
+     */
+    public static Messages getMessages(HttpServletRequest req) {
+        if (req != null) {
+            return new ContextMessages(req);
+        }
+        else {
+            log.debug("using i18n-messages without a request!");
+            return new Messages(MessagesManager.DEFAULT_BASENAME, applicationLocale);
+        }
+    }
 
-	public static String getWithDefault(HttpServletRequest req, String key,
-			String defaultMsg) {
-		return getMessages(req).getWithDefault(key, defaultMsg);
-	}
+    public static Messages getMessages(HttpServletRequest req, String basename) {
+        if (req != null) {
+            return new ContextMessages(req, basename);
+        }
+        else {
+            log.debug("using i18n-messages without a request!");
+            return new Messages(basename, applicationLocale);
+        }
+    }
 
-	public static String getWithDefault(HttpServletRequest req, String key,
-			Object[] args, String defaultMsg) {
-		return getMessages(req).getWithDefault(key, args, defaultMsg);
-	}
+    public static Messages getMessages(HttpServletRequest req, String basename, Locale locale) {
+        if (req != null) {
+            return new ContextMessages(req, basename, locale);
+        }
+        else {
+            log.debug("using i18n-messages without a request!");
+            return new Messages(basename, locale);
+        }
+    }
 
-	/**
-	 * @return Returns the defaultLocale.
-	 */
-	public static Locale getDefaultLocale() {
-		return applicationLocale;
-	}
+    /**
+     * Trys to make a new ContextMessages object. if not possible it creates a new Messages object.
+     * @return Messages
+     */
+    public static Messages getMessages(PageContext pc) {
+        if (pc != null && pc.getRequest() instanceof HttpServletRequest) {
+            return new ContextMessages((HttpServletRequest) pc.getRequest());
+        }
+        else {
+            log.debug("using i18n-messages without a request inside a control!");
+            return new Messages(MessagesManager.DEFAULT_BASENAME, applicationLocale);
+        }
+    }
 
-	/**
-	 * @param defaultLocale
-	 *            The defaultLocale to set.
-	 */
-	public static void setDefaultLocale(String defaultLocale) {
-		MessagesManager.applicationLocale = new Locale(defaultLocale);
-		config.getServletContext().setAttribute(
-				Config.FMT_LOCALE + ".application", defaultLocale);
-	}
+    public static String get(HttpServletRequest req, String key) {
+        return getMessages(req).get(key);
+    }
 
-	/**
-	 * @return Returns the availableLocals.
-	 */
-	public static Collection getAvailableLocales() {
-		return availableLocales;
-	}
+    public static String get(HttpServletRequest req, String key, Object[] args) {
+        return getMessages(req).get(key, args);
+    }
+
+    public static String getWithDefault(HttpServletRequest req, String key, String defaultMsg) {
+        return getMessages(req).getWithDefault(key, defaultMsg);
+    }
+
+    public static String getWithDefault(HttpServletRequest req, String key, Object[] args, String defaultMsg) {
+        return getMessages(req).getWithDefault(key, args, defaultMsg);
+    }
+
+    /**
+     * @return Returns the defaultLocale.
+     */
+    public static Locale getDefaultLocale() {
+        return applicationLocale;
+    }
+
+    /**
+     * @param defaultLocale The defaultLocale to set.
+     */
+    public static void setDefaultLocale(String defaultLocale) {
+        MessagesManager.applicationLocale = new Locale(defaultLocale);
+        config.getServletContext().setAttribute(Config.FMT_LOCALE + ".application", defaultLocale);
+    }
+
+    /**
+     * @return Returns the availableLocals.
+     */
+    public static Collection getAvailableLocales() {
+        return availableLocales;
+    }
+
+    /**
+     * @param userPage
+     * @param session
+     */
+    public static void setUserLanguage(Content userPage, HttpSession session) {
+        String lang = userPage.getNodeData("language").getString();
+        if(StringUtils.isEmpty(lang)){
+            lang = MessagesManager.getDefaultLocale().getLanguage();
+        }
+        session.setAttribute(Config.FMT_LOCALE + ".session", lang);
+    }
 }
