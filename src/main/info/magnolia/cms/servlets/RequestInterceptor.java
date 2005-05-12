61ca697c21516412c6857bc0a811e154c948839d
Fabrizio Giustina <fgiust@openmindonline.it>
2005-05-12 20:04:39 +0000

    MAGNOLIA-387
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@762 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- src/main/info/magnolia/cms/servlets/RequestInterceptor.java
+++ src/main/info/magnolia/cms/servlets/RequestInterceptor.java
@@ -21,6 +21,7 @@ import info.magnolia.cms.util.Resource;
 
 import java.util.Iterator;
 
+import javax.jcr.RepositoryException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -35,6 +36,41 @@ import org.apache.log4j.Logger;
 public class RequestInterceptor extends HttpServlet {
 
     /**
+     * Action: sort a paragraph.
+     */
+    private static final String ACTION_NODE_SORT = "NODE_SORT";
+
+    /**
+     * Action: delete a paragraph.
+     */
+    private static final String ACTION_NODE_DELETE = "NODE_DELETE";
+
+    /**
+     * Action: preview a page.
+     */
+    private static final String ACTION_PREVIEW = "PREVIEW";
+
+    /**
+     * request parameter: repository name.
+     */
+    private static final String PARAM_REPOSITORY = "mgnlRepository";
+
+    /**
+     * request parameter: node path, used for paragraph deletion.
+     */
+    private static final String PARAM_PATH = "mgnlPath";
+
+    /**
+     * request parameter: sort-above paragraph.
+     */
+    private static final String PARAM_PATH_SORT_ABOVE = "mgnlPathSortAbove";
+
+    /**
+     * request parameter: selected paragraph.
+     */
+    private static final String PARAM_PATH_SELECTED = "mgnlPathSelected";
+
+    /**
      * Stable serialVersionUID.
      */
     private static final long serialVersionUID = 222L;
@@ -45,20 +81,18 @@ public class RequestInterceptor extends HttpServlet {
     private static Logger log = Logger.getLogger(RequestInterceptor.class);
 
     /**
-     * <p>
      * Request and Response here is same as receivced by the original page so it includes all post/get data. Sub action
-     * could be called from here once this action finishes, it will continue loading the requested page
-     * </p>
+     * could be called from here once this action finishes, it will continue loading the requested page.
      */
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
 
         String action = request.getParameter(EntryServlet.INTERCEPT);
-        String repository = request.getParameter("mgnlRepository");
+        String repository = request.getParameter(PARAM_REPOSITORY);
         if (repository == null) {
             repository = ContentRepository.WEBSITE;
         }
         HierarchyManager hm = SessionAccessControl.getHierarchyManager(request, repository);
-        if (action.equals("PREVIEW")) {
+        if (action.equals(ACTION_PREVIEW)) {
             // preview mode (button in main bar)
             String preview = request.getParameter(Resource.MGNL_PREVIEW_ATTRIBUTE);
             if (preview != null) {
@@ -70,21 +104,22 @@ public class RequestInterceptor extends HttpServlet {
                 }
             }
         }
-        else if (action.equals("NODE_DELETE")) {
+        else if (action.equals(ACTION_NODE_DELETE)) {
             // delete paragraph
             try {
-                String path = request.getParameter("mgnlPath");
+                String path = request.getParameter(PARAM_PATH);
                 hm.delete(path);
+                hm.save();
             }
-            catch (Exception e) {
-                log.debug("Exception caught: " + e.getMessage(), e);
+            catch (RepositoryException e) {
+                log.error("Exception caught: " + e.getMessage(), e);
             }
         }
-        else if (action.equals("NODE_SORT")) {
+        else if (action.equals(ACTION_NODE_SORT)) {
             // sort paragrpahs
             try {
-                String pathSelected = request.getParameter("mgnlPathSelected");
-                String pathSortAbove = request.getParameter("mgnlPathSortAbove");
+                String pathSelected = request.getParameter(PARAM_PATH_SELECTED);
+                String pathSortAbove = request.getParameter(PARAM_PATH_SORT_ABOVE);
                 String pathParent = pathSelected.substring(0, pathSelected.lastIndexOf("/"));
                 Iterator it = hm.getContent(pathParent).getChildren().iterator();
                 long seqPos0 = 0;
@@ -114,7 +149,7 @@ public class RequestInterceptor extends HttpServlet {
                 }
                 nodeSelected.save();
             }
-            catch (Exception e) {
+            catch (RepositoryException e) {
                 log.debug("Exception caught: " + e.getMessage(), e);
             }
         }
