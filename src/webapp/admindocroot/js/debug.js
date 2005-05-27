5cbfde95c88d26f30d5c73e75c77e4ef6b6c9bc6
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2005-05-27 14:12:11 +0000

    debug in javascript
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@895 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ src/webapp/admindocroot/js/debug.js
@@ -0,0 +1,39 @@
+// this is the main switch
+var mgnlDebugOn = false;
+
+// set the contextes which you want to debug
+var mgnlDebugContextes = {
+	tree: true
+}
+
+function mgnlDebug(str, context){
+	if(!mgnlDebugOn)
+		return;
+		
+	// is the context in debug mode?
+	if(context != null && (mgnlDebugContextes[context] == null || !mgnlDebugContextes[context]))
+		return;
+		
+	var console = window.top.mgnlDebugConsole;
+	var doc = null;
+	// create new window if not allready done
+	if(console == null){
+		console = window.open('','mgnlDebugConsole');
+		window.top.mgnlDebugConsole = console;
+		doc = console.document;
+		doc.write('<input type="button" value="Clear" onclick="document.getElementById(\'consoleDiv\').innerHTML=\'\';" > <p>');
+		doc.write('<div id="consoleDiv" style="font-family: sans-serif; font-size: 10pt">gugus');
+		doc.write('</div>');
+		doc.close();
+	}
+	else{
+		doc = console.document;
+	}
+	
+	if(doc == null)
+		return;
+	
+	// get the div to write in
+	var div = doc.getElementById('consoleDiv');
+	div.innerHTML += str + "<br>";
+}
