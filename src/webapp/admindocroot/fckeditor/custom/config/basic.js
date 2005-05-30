93aa2045fcb730768e9e5866c6bb1ebbac426ddd
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2005-05-30 10:11:40 +0000

    basic configuration
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@904 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ src/webapp/admindocroot/fckeditor/custom/config/basic.js
@@ -0,0 +1,12 @@
+// Style File to be used in the editable area.
+FCKConfig.EditorAreaCSS = FCKConfig.BasePath + 'css/fck_editorarea.css' ;
+
+FCKConfig.LinkBrowser = false;
+
+FCKConfig.ToolbarCanCollapse = false ;
+
+FCKConfig.ForcePasteAsPlainText	= true ;
+
+FCKConfig.ToolbarSets["basic"] = [
+	['Bold','Italic','-','OrderedList','UnorderedList','-','Link','Unlink']
+] ;
