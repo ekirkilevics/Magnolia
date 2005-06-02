f455d36a88808c9813cdfbcfb6734ba977bee4d3
Fabrizio Giustina <fgiust@openmindonline.it>
2005-06-02 12:51:44 +0000

    checkstyle
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@925 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- src/main/info/magnolia/cms/gui/control/ContextMenu.java
+++ src/main/info/magnolia/cms/gui/control/ContextMenu.java
@@ -1,8 +1,14 @@
-/*
- * Created on 25.05.2005
+/**
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2005 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
  */
 package info.magnolia.cms.gui.control;
 
@@ -12,26 +18,24 @@ import java.util.List;
 
 /**
  * @author philipp
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class ContextMenu extends ControlSuper {
+
     private List menuItems = new ArrayList();
-    
+
     public ContextMenu(String menuName) {
         super();
         this.setName(menuName);
     }
-    
+
     public List getMenuItems() {
         return this.menuItems;
     }
-    
+
     public void setMenuItems(List menuItems) {
         this.menuItems = menuItems;
     }
-    
+
     public ContextMenuItem getMenuItem(int col) {
         return (ContextMenuItem) this.getMenuItems().get(col);
     }
@@ -39,58 +43,55 @@ public class ContextMenu extends ControlSuper {
     public void addMenuItem(ContextMenuItem tmi) {
         this.getMenuItems().add(tmi);
     }
-    
-    
-    /* (non-Javadoc)
+
+    /*
+     * (non-Javadoc)
      * @see info.magnolia.cms.gui.control.ControlSuper#getHtml()
      */
     public String getHtml() {
-            StringBuffer html = new StringBuffer();
-            StringBuffer menuJavascript = new StringBuffer();
-            html.append("<div id=\"" + getName() + "_DivMenu\" class=\"mgnlTreeMenu\" >");
-            int counter = 0;
-            for (int i = 0; i < this.getMenuItems().size(); i++) {
-                ContextMenuItem item = this.getMenuItem(i);
-                if (item == null) {
-                    html.append("<div class=\"mgnlTreeMenuLine\"><!-- ie --></div>");
-                }
-                else {
-                    item.setJavascriptMenuName(getName());
-                    String id = getName() + "_MenuItem" + i;
-                    item.setId(id);
-                    menuJavascript.append(getName()
-                        + ".menuItems["
-                        + counter
-                        + "]=new mgnlContextMenuItem('"
-                        + id
-                        + "');\n");
-                    menuJavascript.append(getName()
-                        + ".menuItems["
-                        + counter
-                        + "].conditions=new Object();");
-                    for (int cond = 0; cond < item.getJavascriptConditions().size(); cond++) {
-                        menuJavascript.append(getName()
-                            + ".menuItems["
-                            + counter
-                            + "].conditions["
-                            + cond
-                            + "]="
-                            + item.getJavascriptCondition(cond)
-                            + ";");
-                    }
-                    html.append(item.getHtml());
-                    counter++;
-                }
+        StringBuffer html = new StringBuffer();
+        StringBuffer menuJavascript = new StringBuffer();
+        html.append("<div id=\"" + getName() + "_DivMenu\" class=\"mgnlTreeMenu\" >");
+        int counter = 0;
+        for (int i = 0; i < this.getMenuItems().size(); i++) {
+            ContextMenuItem item = this.getMenuItem(i);
+            if (item == null) {
+                html.append("<div class=\"mgnlTreeMenuLine\"><!-- ie --></div>");
             }
-            html.append("</div>");
-
-            // html.append(this.getJavascriptTree()+".columns=new Array();"); //->in tree.js
-            // add menu to tree object
-
-            html.append("<script type=\"text/javascript\">");
-            html.append("var " + getName() + "= new mgnlContextMenu('" + getName() + "');");
-            html.append(menuJavascript);
-            html.append("</script>");
-            return html.toString();
+            else {
+                item.setJavascriptMenuName(getName());
+                String id = getName() + "_MenuItem" + i;
+                item.setId(id);
+                menuJavascript.append(getName()
+                    + ".menuItems["
+                    + counter
+                    + "]=new mgnlContextMenuItem('"
+                    + id
+                    + "');\n");
+                menuJavascript.append(getName() + ".menuItems[" + counter + "].conditions=new Object();");
+                for (int cond = 0; cond < item.getJavascriptConditions().size(); cond++) {
+                    menuJavascript.append(getName()
+                        + ".menuItems["
+                        + counter
+                        + "].conditions["
+                        + cond
+                        + "]="
+                        + item.getJavascriptCondition(cond)
+                        + ";");
+                }
+                html.append(item.getHtml());
+                counter++;
+            }
         }
+        html.append("</div>");
+
+        // html.append(this.getJavascriptTree()+".columns=new Array();"); //->in tree.js
+        // add menu to tree object
+
+        html.append("<script type=\"text/javascript\">");
+        html.append("var " + getName() + "= new mgnlContextMenu('" + getName() + "');");
+        html.append(menuJavascript);
+        html.append("</script>");
+        return html.toString();
+    }
 }
