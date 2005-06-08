f4229be93c468af7c225bc6e0c50331a7517ace7
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2005-06-08 09:37:15 +0000

    new version 2.0 fc
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@984 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ src/webapp/admindocroot/fckeditor/custom/config/magnoliaStandard.js
@@ -0,0 +1,8 @@
+FCKConfig.ToolbarSets["MagnoliaStandard"] = [
+	['Bold','SpecialChar','-','Link','Unlink','-','Cut','Copy','Paste','PasteText','PasteWord','-','OrderedList','UnorderedList','-','Undo','Redo','-','Source']
+] ;
+
+FCKConfig.EditorAreaCSS = FCKConfig.BasePath + '../custom/css/magnoliaStandard.css';
+
+// do not show the advanced tab
+FCKConfig.LinkDlgHideAdvanced = true ;
---
+++ src/webapp/admindocroot/fckeditor/custom/init/magnoliaStandard.js
@@ -0,0 +1,1 @@
+fckInstance.ToolbarSet = 'MagnoliaStandard';
