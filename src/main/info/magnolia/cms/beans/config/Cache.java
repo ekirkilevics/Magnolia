640a60a94d7467204449d3bc2567521d15ca344a
Fabrizio Giustina <fgiust@openmindonline.it>
2005-06-14 08:12:16 +0000

    also delete files, not only clear the hashmap
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@1038 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- src/main/info/magnolia/cms/beans/config/Cache.java
+++ src/main/info/magnolia/cms/beans/config/Cache.java
@@ -90,7 +90,7 @@ public final class Cache {
 
         // @todo this should probably not be here, but it's important to remove cached entries when the configuration is
         // reloaded
-        info.magnolia.cms.beans.runtime.Cache.clearCachedURIList();
+        CacheHandler.flushCache();
 
         log.info("Config : loading cache mapping");
         try {
