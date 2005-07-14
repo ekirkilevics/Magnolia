96d9987938c5b3d4ee2cf95180a7ac1ffbf189cd
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2005-07-14 12:32:43 +0000

    proper header
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@1148 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- src/main/info/magnolia/cms/util/JSPIncludeUtil.java
+++ src/main/info/magnolia/cms/util/JSPIncludeUtil.java
@@ -17,6 +17,19 @@ import org.apache.taglibs.standard.resources.Resources;
 
 
 /**
+*
+* Magnolia and its source-code is licensed under the LGPL.
+* You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+* When copying, adapting, or redistributing this document in keeping with the guidelines above,
+* you are required to provide proper attribution to obinary.
+* If you reproduce or distribute the document without making any substantive modifications to its content,
+* please use the following attribution line:
+*
+* Copyright 1993-2005 obinary Ltd. (http://www.obinary.com) All rights reserved.
+*
+*/
+
+/**
  * The inner class ImportResponseWrapper is copied from the JSTL include tag
  * @author philipp
  */
