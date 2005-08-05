d224e259b9fb6312d118980377b8aff18830753d
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2005-08-05 16:28:21 +0000

    deleted the false file. RichEditorIFrameDialogPage undeleted
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@1298 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ src/main/info/magnolia/module/admininterface/dialogpages/RichEditorIFrameDialogPage.java
@@ -0,0 +1,51 @@
+package info.magnolia.module.admininterface.dialogpages;
+
+import info.magnolia.cms.gui.dialog.DialogRichedit;
+import info.magnolia.cms.gui.dialog.DialogSuper;
+import info.magnolia.module.admininterface.DialogPageMVCHandler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * @author Fabrizio Giustina
+ * @version $Id: $
+ */
+public class RichEditorIFrameDialogPage extends DialogPageMVCHandler {
+
+    public RichEditorIFrameDialogPage(String name, HttpServletRequest request, HttpServletResponse response) {
+        super(name, request, response);
+    }
+
+    /**
+     * Stable serialVersionUID.
+     */
+    private static final long serialVersionUID = 222L;
+
+    /**
+     * Logger.
+     */
+    private static Logger log = Logger.getLogger(RichEditorIFrameDialogPage.class);
+
+    protected void draw(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+
+        DialogRichedit richE = (DialogRichedit) request.getSession().getAttribute(
+            request.getParameter(DialogSuper.SESSION_ATTRIBUTENAME_DIALOGOBJECT));
+        if (richE != null) {
+            richE.removeSessionAttribute();
+            richE.drawHtmlEditor(out);
+        }
+        else {
+            log.error("DialogRichedit not found in session with name [" //$NON-NLS-1$
+                + request.getParameter(DialogSuper.SESSION_ATTRIBUTENAME_DIALOGOBJECT) + "]"); //$NON-NLS-1$
+        }
+    }
+
+}
