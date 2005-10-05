c81c89a9a797ad251ce95ad5ad5f07549ed700ff
Sameer Charles <sameer.charles@magnolia-cms.com>
2005-10-05 08:20:15 +0000

    BUG FIX : MAGNOLIA-554
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@1589 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-gui/src/main/java/info/magnolia/cms/gui/control/Tree.java
+++ magnolia-gui/src/main/java/info/magnolia/cms/gui/control/Tree.java
@@ -404,11 +404,27 @@ public class Tree extends ControlSuper {
         }
     }
 
+    /**
+     * Creates an <em>untitled</em> new node.
+     * The node will not be persisted, until he gets a name. The flow is createNode and
+     * the save will happen upon renameNode.
+     *
+     * @param itemType
+     * */
     public void createNode(String itemType) {
-        this.createNode("untitled", itemType); //$NON-NLS-1$
+        this.createNode("untitled", itemType, false); //$NON-NLS-1$
     }
 
-    public void createNode(String label, String itemType) {
+    /**
+     * Creates a new node (either <code>NodeData</code> or <code>Content</code>) with
+     * the specified name (<tt>label</tt>) and type. The node will be saved only if
+     * <tt>save</tt>.
+     *
+     * @param label new node name
+     * @param itemType new node type
+     * @param save <tt>true</tt> for immediately saving the node; <tt>false</tt> otherwise
+     * */
+    public void createNode(String label, String itemType, boolean save) {
         try {
             HierarchyManager hm = SessionAccessControl.getHierarchyManager(this.getRequest(), this.getRepository());
             Content parentNode = hm.getContent(this.getPath());
@@ -455,7 +471,9 @@ public class Tree extends ControlSuper {
                     }
                 }
             }
-            parentNode.save();
+            if (save) {
+                parentNode.save();
+            }
         }
         catch (Exception e) {
             log.error(e.getMessage(), e);
@@ -822,9 +840,8 @@ public class Tree extends ControlSuper {
             if (hm.isExist(dest)) {
                 newLabel = Path.getUniqueLabel(hm, parentPath, newLabel);
                 dest = parentPath + "/" + newLabel; //$NON-NLS-1$
+                this.deActivateNode(this.getPath()); // BUGFIX: MAGNOLIA-554
             }
-
-            this.deActivateNode(this.getPath());
 
             if (log.isInfoEnabled()) {
                 log.info("Moving node from " + this.getPath() + " to " + dest); //$NON-NLS-1$ //$NON-NLS-2$
