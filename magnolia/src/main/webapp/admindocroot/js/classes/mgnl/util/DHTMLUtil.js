a8a5c00f79d7ee18a80f2dded028d7f6c68cc8c9
Fabrizio Giustina <fgiust@openmindonline.it>
2006-03-01 22:31:27 +0000

    fix getIFrameDocument() to work with IE 7 (better use the standard getElementById() as the default method, there were reasons to not do so?)
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@2157 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia/src/main/webapp/admindocroot/js/classes/mgnl/util/DHTMLUtil.js
+++ magnolia/src/main/webapp/admindocroot/js/classes/mgnl/util/DHTMLUtil.js
@@ -17,127 +17,127 @@ importClass("mgnl.util.BrowserCheck");
  */
 classDef("mgnl.util.DHTMLUtil", {
 
-	/**
-	 * Returns the an object containing a .w and .h property 
-	 */
-	getWindowSize: function()	{
-		var obj=new Object();
-		if( typeof (window.innerWidth) == 'number' ){
-			//Non-IE
-			obj.w=window.innerWidth;
-			obj.h=window.innerHeight;
-		}
-		else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight ) ){
-			//IE 6+ in 'standards compliant mode'
-			obj.w=document.documentElement.clientWidth;
-			obj.h=document.documentElement.clientHeight;
-		}
-		else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ){
-			//IE 4 compatible
-			obj.w=document.body.clientWidth;
-			obj.h=document.body.clientHeight;
-		}
-		return obj;
-	},
+    /**
+     * Returns the an object containing a .w and .h property
+     */
+    getWindowSize: function()	{
+        var obj=new Object();
+        if( typeof (window.innerWidth) == 'number' ){
+            //Non-IE
+            obj.w=window.innerWidth;
+            obj.h=window.innerHeight;
+        }
+        else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight ) ){
+            //IE 6+ in 'standards compliant mode'
+            obj.w=document.documentElement.clientWidth;
+            obj.h=document.documentElement.clientHeight;
+        }
+        else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ){
+            //IE 4 compatible
+            obj.w=document.body.clientWidth;
+            obj.h=document.body.clientHeight;
+        }
+        return obj;
+    },
 
-	/**
-	 * Returns the inner document of an iframe
-	 */
-	getIFrameDocument: function(iFrameName){
-		if (document.frames && document.frames[iFrameName]) 
-			return document.frames[iFrameName].document;
-		else 
-			if ($(iFrameName)) 
-				return $(iFrameName).contentDocument;
-	    else 
-	    		return null;
-	},
-	
-	/**
-	 * Get the y position of the element
-	 */
-	getPosX: function(obj){
-		if (!obj) return 0;
+    /**
+     * Returns the inner document of an iframe
+     */
+    getIFrameDocument: function(iFrameName){
+        if ($(iFrameName))
+            return $(iFrameName).contentDocument;
+        else
+            if (document.frames && document.frames[iFrameName])
+            return document.frames[iFrameName].document;
+        else
+                return null;
+    },
 
-	  	var x=document.body.scrollLeft;
-	  	while (obj.offsetParent){
-	  		x+=obj.offsetLeft;
-	  		obj=obj.offsetParent;
-	  	}
-		return x; 
-	},
-	
-	/**
-	 * Get the y position of the element
-	 */
-	getPosY: function(obj){
-		if (!obj) 
-			return 0;
-		
-		var y=document.body.scrollTop;
-	  	while (obj.offsetParent){
-	  		y+=obj.offsetLeft;
-  			obj=obj.offsetParent;
-  		}
-		return y;
-	},
-	
-	/**
-	 * Get .x and .y of the mouse event (if scrolled)
-	 */
-	getMousePos: function(event){
-		var pos=new Object();
-		if (document.all){
-			pos.x=window.event.clientX + document.body.scrollLeft;
-			pos.y=window.event.clientY + document.body.scrollTop;
-		}
-		else{
-			pos.x=event.pageX;
-			pos.y=event.pageY;
-		}
-		return pos;
-	},
-	
-	/**
-	 * Get the root window (including opener)
-	 * @param current current window (optional)
-	 */
-	getRootWindow: function(current){
-		current = current?current:window;
-		
-		if(current.top != current)
-			return this.getRootWindow(current.top);
-		if(current.opener != null)
-			return this.getRootWindow(current.opener);
-		return current;
-	},
-	
-	findVariable:  function(name, current){
-		current = current?current:window;
-		if(current[name]!=null)
-			return current[name];
-		
-		if(current.top != current)
-			return this.findVariable(name, current.top);
-		if(current.opener != null)
-			return this.findVariable(name, current.opener);
-		return null;
-	},
-	
-	/**
-	 * Hide the element
-	 */
-	hide: function(element){
-		element.style.visibility='hidden';
-		element.style.display='none';
-	},
+    /**
+     * Get the y position of the element
+     */
+    getPosX: function(obj){
+        if (!obj) return 0;
 
-	/**
-	 * Show the element
-	 */
-	show: function(element){
-		element.style.visibility='visible';
-		element.style.display='block';
-	}
-	
-}); 
+          var x=document.body.scrollLeft;
+          while (obj.offsetParent){
+              x+=obj.offsetLeft;
+              obj=obj.offsetParent;
+          }
+        return x;
+    },
+
+    /**
+     * Get the y position of the element
+     */
+    getPosY: function(obj){
+        if (!obj)
+            return 0;
+
+        var y=document.body.scrollTop;
+          while (obj.offsetParent){
+              y+=obj.offsetLeft;
+              obj=obj.offsetParent;
+          }
+        return y;
+    },
+
+    /**
+     * Get .x and .y of the mouse event (if scrolled)
+     */
+    getMousePos: function(event){
+        var pos=new Object();
+        if (document.all){
+            pos.x=window.event.clientX + document.body.scrollLeft;
+            pos.y=window.event.clientY + document.body.scrollTop;
+        }
+        else{
+            pos.x=event.pageX;
+            pos.y=event.pageY;
+        }
+        return pos;
+    },
+
+    /**
+     * Get the root window (including opener)
+     * @param current current window (optional)
+     */
+    getRootWindow: function(current){
+        current = current?current:window;
+
+        if(current.top != current)
+            return this.getRootWindow(current.top);
+        if(current.opener != null)
+            return this.getRootWindow(current.opener);
+        return current;
+    },
+
+    findVariable:  function(name, current){
+        current = current?current:window;
+        if(current[name]!=null)
+            return current[name];
+
+        if(current.top != current)
+            return this.findVariable(name, current.top);
+        if(current.opener != null)
+            return this.findVariable(name, current.opener);
+        return null;
+    },
+
+    /**
+     * Hide the element
+     */
+    hide: function(element){
+        element.style.visibility='hidden';
+        element.style.display='none';
+    },
+
+    /**
+     * Show the element
+     */
+    show: function(element){
+        element.style.visibility='visible';
+        element.style.display='block';
+    }
+
+});
