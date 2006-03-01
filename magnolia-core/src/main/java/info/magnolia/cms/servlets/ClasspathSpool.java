60b08a57af9d0262bfe3f20e7b8adee19190754a
Fabrizio Giustina <fgiust@openmindonline.it>
2006-03-01 23:09:05 +0000

    a simple servlet that can be used to load resources from the classpath. Setup a (now empty) new module where all the fckeditor resources will be moved.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@2158 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/cms/servlets/ClasspathSpool.java
@@ -0,0 +1,75 @@
+package info.magnolia.cms.servlets;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * A simple spool servlet that load resources from the classpath. A simple rule for accessible resources: only files
+ * into a <code>mgnl-resources</code> folder will be loaded by this servlet (corresponding to the mapped url
+ * <code>/.resources/*</code>. This servlet should be used for authoring-only resources, like rich editor images and
+ * scripts. It's not suggested for public website resources. Content length and last modification date are not set on
+ * files returned from the classpath.
+ * @author Fabrizio Giustina
+ * @version $Revision$ ($Author$)
+ */
+public class ClasspathSpool extends HttpServlet {
+
+    /**
+     * Stable serialVersionUID.
+     */
+    private static final long serialVersionUID = 222L;
+
+    /**
+     * Logger.
+     */
+    private static Logger log = LoggerFactory.getLogger(Spool.class);
+
+    /**
+     * All static resource requests are handled here.
+     * @param request HttpServletRequest
+     * @param response HttpServletResponse
+     * @throws IOException for error in accessing the resource or the servlet output stream
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        InputStream in = getClass().getResourceAsStream("/mgnl-resources" + request.getPathInfo());
+
+        if (in == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        try {
+            ServletOutputStream os = response.getOutputStream();
+            byte[] buffer = new byte[8192];
+            int read = 0;
+            while ((read = in.read(buffer)) > 0) {
+                os.write(buffer, 0, read);
+            }
+            os.flush();
+            IOUtils.closeQuietly(os);
+        }
+        catch (IOException e) {
+            // only log at debug level
+            // tomcat usually throws a ClientAbortException anytime the user stop loading the page
+            log.debug("Unable to spool resource due to a {} exception", e.getClass().getName()); //$NON-NLS-1$
+            if (!response.isCommitted()) {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+        }
+        finally {
+            IOUtils.closeQuietly(in);
+        }
+    }
+
+}
