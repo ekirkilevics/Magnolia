95d48c093b08d28f34aa5f750d64b208d73de1bd
Nicolas Modrzyk <nicolas.modrzyk@macnica.com>
2006-03-08 23:55:52 +0000

    new updates from jackie. See mail from march8 on the workflow list
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@2230 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-openwfe/src/main/java/info/magnolia/module/owfe/FlowAdminTreeMVCHandler.java
+++ magnolia-module-openwfe/src/main/java/info/magnolia/module/owfe/FlowAdminTreeMVCHandler.java
@@ -62,24 +62,36 @@ public abstract class FlowAdminTreeMVCHandler extends AdminTreeMVCHandler {
 			li.addAttribute("Action",new StringAttribute("Activate"));
 
 			// Retrieve and add the flow definition to the LaunchItem
-			String flowDef = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-					+ "<process-definition "
-					+ "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
-					+ "xsi:noNamespaceSchemaLocation=\"http://www.openwfe.org/flowdef_r1.5.0.xsd\" "
-					+ "name=\"docflow\" "
-					+ "revision=\"1.0\">"
-					+ "<description language=\"default\"> "
-					+ "This just the complete flow definition of docflow process. "
-					+ "</description>" + "<sequence>"
-					+ "<participant ref=\"user-superuser\"/>" + "</sequence>"
-					+ "</process-definition>";
+
+//			String flowDef = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+//					+ "<process-definition "
+//					+ "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
+//					+ "xsi:noNamespaceSchemaLocation=\"http://www.openwfe.org/flowdef_r1.5.0.xsd\" "
+//					+ "name=\"docflow\" "
+//					+ "revision=\"1.0\">"
+//					+ "<description language=\"default\"> "
+//					+ "This just the complete flow definition of docflow process. "
+//					+ "</description>" + "<sequence>"
+//					+ "<participant ref=\"user-superuser\"/>" + "</sequence>"
+//					+ "</process-definition>";
+//			li.getAttributes().puts("__definition__", flowDef);
+			
+			
+			//String url_base = "http://"+ request.getServerName()+":" + request.getServerPort() + "/magnolia/FlowDef";
+			//String flowURL = url_base+"?name=activation";
+			//log.info("flow URL:" + flowURL);
+			//li.setWorkflowDefinitionUrl(flowURL);
+			
+			String flowDef =  new JCRFlowDefinition().getflowDefAsString("webActivation");
 			li.getAttributes().puts("__definition__", flowDef);
 			
 			// Start the engine
 			engine.launch(li, true);
+//			engine.launch(flowURL, false);
+			
 			
 			// Notify by email ? Should be in the flow itself, not here.
-			new Thread(new MailSender(pathSelected)).start();
+			//new Thread(new MailSender(pathSelected)).start();
 			
 			// Alert the user using a javascript notification
 			String message = "Your request has been dispatched, please wait for approval";
