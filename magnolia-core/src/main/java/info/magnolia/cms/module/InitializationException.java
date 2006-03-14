b02953c927485b1444ab16a533edeec2fb7fb09a
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2006-03-14 20:09:06 +0000

    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@2276 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/cms/module/InitializationException.java
@@ -0,0 +1,28 @@
+package info.magnolia.cms.module;
+
+import org.apache.commons.lang.exception.NestableException;
+
+public class InitializationException extends NestableException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1300543420918121871L;
+
+	public InitializationException() {
+		super();
+	}
+
+	public InitializationException(String arg0) {
+		super(arg0);
+	}
+
+	public InitializationException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public InitializationException(Throwable arg0) {
+		super(arg0);
+	}
+
+}
