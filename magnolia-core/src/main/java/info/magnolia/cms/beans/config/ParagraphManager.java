6bb3ba9bae37b8be4b2f9655460a46d662184a20
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2006-03-14 20:03:20 +0000

    separated objects and manager
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@2268 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/cms/beans/config/ParagraphManager.java
@@ -0,0 +1,126 @@
+/**
+ *
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2006 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
+ */
+package info.magnolia.cms.beans.config;
+
+import info.magnolia.cms.core.Content;
+import info.magnolia.cms.core.ItemType;
+import info.magnolia.cms.util.FactoryUtil;
+
+import java.util.Collection;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Manages the paragraph ot the system. The modules register the nodes where the paragraph are defined.
+ * @author philipp
+ */
+public class ParagraphManager extends ObservedManager {
+
+    /**
+     * Logger
+     */
+    private static Logger log = LoggerFactory.getLogger(ParagraphManager.class);
+
+    /**
+     * The current implementation of the ParagraphManager. Defeined in magnolia.properties.
+     */
+    private static ParagraphManager instance = (ParagraphManager) FactoryUtil.getSingleton(ParagraphManager.class);
+
+    /**
+     * Cached paragraphs
+     */
+    protected Map paragraphs = new Hashtable();
+
+    /**
+     * Returns the cached content of the requested template. TemplateInfo properties :
+     * <ol>
+     * <li>title - title describing template</li>
+     * <li>type - jsp / servlet</li>
+     * <li>path - jsp / servlet path</li>
+     * <li>description - description of a template</li>
+     * </ol>
+     * @return TemplateInfo
+     */
+    public Paragraph getInfo(String key) {
+        return (Paragraph) paragraphs.get(key);
+    }
+
+    /**
+     * Get a map of all registered paragraphs.
+     * @return
+     */
+    public Map getParagraphs() {
+        return paragraphs;
+    }
+
+    /**
+     * Register all the paragraphs under this and subnodes.
+     * @param node
+     * @param observate true if an eventlistener should get registered
+     */
+    protected void onRegister(Content node) {
+        // register a listener
+
+        Collection paragraphNodes = node.getChildren(ItemType.CONTENTNODE);
+        for (Iterator iter = paragraphNodes.iterator(); iter.hasNext();) {
+            Content paragraphNode = (Content) iter.next();
+            addParagraphToCache(paragraphNode);
+        }
+
+        Collection subDefinitions = node.getChildren(ItemType.CONTENT);
+        Iterator it = subDefinitions.iterator();
+        while (it.hasNext()) {
+            Content subNode = (Content) it.next();
+            // do not register other observations
+            onRegister(subNode);
+        }
+
+    }
+
+    /**
+     * Adds paragraph definition to ParagraphInfo cache.
+     */
+    protected Paragraph addParagraphToCache(Content c) {
+        Paragraph pi = new Paragraph();
+        pi.setName(c.getNodeData("name").getString()); //$NON-NLS-1$
+        pi.setTemplatePath(c.getNodeData("templatePath").getString()); //$NON-NLS-1$
+        pi.setDialogPath(c.getNodeData("dialogPath").getString()); //$NON-NLS-1$
+        pi.setTemplateType(c.getNodeData("type").getString()); //$NON-NLS-1$
+        pi.setTitle(c.getNodeData("title").getString()); //$NON-NLS-1$
+        pi.setDescription(c.getNodeData("description").getString()); //$NON-NLS-1$
+        if (Paragraph.log.isDebugEnabled()) {
+            Paragraph.log.debug("Registering paragraph [" + pi.getName() + "]"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        paragraphs.put(pi.getName(), pi);
+        log.info("paragraph added [" + pi.getName() + "]");
+        return pi;
+    }
+
+    /**
+     * Get the current singleton object
+     * @return
+     */
+    public static ParagraphManager getInstance() {
+        return instance;
+    }
+
+    public void onClear() {
+        this.paragraphs.clear();
+    }
+
+}
---
+++ magnolia-core/src/main/java/info/magnolia/cms/beans/config/TemplateManager.java
@@ -0,0 +1,198 @@
+/**
+ *
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2006 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
+ */
+package info.magnolia.cms.beans.config;
+
+import info.magnolia.cms.core.Content;
+import info.magnolia.cms.core.ItemType;
+import info.magnolia.cms.security.AccessManager;
+import info.magnolia.cms.security.Permission;
+import info.magnolia.cms.util.FactoryUtil;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.jcr.RepositoryException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Manages the templates of the system
+ * @author philipp
+ */
+public class TemplateManager extends ObservedManager {
+
+    /**
+     * Logger
+     */
+    private static Logger log = LoggerFactory.getLogger(TemplateManager.class);
+
+    /**
+     * The current implementation of the ParagraphManager. Defeined in magnolia.properties.
+     */
+    private static TemplateManager instance = (TemplateManager) FactoryUtil.getSingleton(TemplateManager.class);
+
+    /**
+     * The cached templates
+     */
+    private Map cachedContent = new Hashtable();
+
+    /**
+     * The templates visible in the templates selection
+     */
+    private List visibleTemplates = new ArrayList();
+
+    /**
+     * Called by the ObervedManager
+     */
+    protected void onRegister(Content node) {
+        try {
+            log.info("Config : loading Template info - " + node.getHandle()); //$NON-NLS-1$
+
+            // It makes possibly to use templates defined within subfolders of /module/templating/Templates
+            Collection children = collectChildren(node);
+
+            if ((children != null) && !(children.isEmpty())) {
+                Iterator templates = children.iterator();
+                cacheContent(templates);
+            }
+
+            log.info("Config : Template info loaded - " + node.getHandle()); //$NON-NLS-1$
+        }
+        catch (Exception re) {
+            log.error("Config : Failed to load Template info - " + node.getHandle()); //$NON-NLS-1$
+            log.error(re.getMessage(), re);
+        }
+
+    }
+
+    protected void onClear() {
+        this.cachedContent.clear();
+        this.visibleTemplates.clear();
+    }
+
+    /**
+     * Returns the cached content of the requested template. TemplateInfo properties:
+     * <ol>
+     * <li> title - title describing template</li>
+     * <li> type - jsp / servlet</li>
+     * <li> path - jsp / servlet path</li>
+     * <li> description - description of a template</li>
+     * </ol>
+     * @return TemplateInfo
+     */
+    public Template getInfo(String key) throws Exception {
+        return (Template) cachedContent.get(key);
+    }
+
+    /**
+     * Adds templates definition to TemplatesInfo cache.
+     * @param templates iterator as read from the repository
+     * @param visibleTemplates List in with all visible templates will be added
+     */
+    private void addTemplatesToCache(Iterator templates, List visibleTemplates) {
+        while (templates.hasNext()) {
+            Content c = (Content) templates.next();
+            try {
+                Template ti = new Template(c);
+                cachedContent.put(ti.getName(), ti);
+                if (ti.isVisible()) {
+                    visibleTemplates.add(ti);
+                }
+
+                log.info(MessageFormat.format("Registering template [{0}]", new Object[]{ti.getName()})); //$NON-NLS-1$
+
+            }
+            catch (RepositoryException re) {
+                log.error("Failed to cache TemplateInfo"); //$NON-NLS-1$
+            }
+        }
+    }
+
+    /**
+     * Load content of this template info page in a hash table caching at the system load, this will save lot of time on
+     * every request while matching template info.
+     */
+    private void cacheContent(Iterator templates) {
+        if (templates != null) {
+            addTemplatesToCache(templates, visibleTemplates);
+        }
+    }
+
+    /**
+     * Recursive search for content nodes contains template data (looks up subfolders)
+     * @author <a href="mailto:tm@touk.pl">Tomasz Mazan</a>
+     * @param cnt current folder to look for template's nodes
+     * @return collection of template's content nodes from current folder and descendants
+     */
+    private Collection collectChildren(Content cnt) {
+        // Collect template's content node - children of current node
+        Collection children = cnt.getChildren(ItemType.CONTENTNODE);
+
+        // Look into subfolders
+        Collection subFolders = cnt.getChildren(ItemType.CONTENT);
+        if ((subFolders != null) && !(subFolders.isEmpty())) {
+
+            Iterator it = subFolders.iterator();
+            while (it.hasNext()) {
+                Content subCnt = (Content) it.next();
+                Collection grandChildren = collectChildren(subCnt);
+
+                if ((grandChildren != null) && !(grandChildren.isEmpty())) {
+                    children.addAll(grandChildren);
+                }
+            }
+
+        }
+
+        return children;
+    }
+
+    /**
+     * Get templates collection after access control filter applied using specified AccessManager
+     * @return Collection list containing templates as Template objects
+     */
+    public Iterator getAvailableTemplates(AccessManager accessManager) {
+        List templateList = new ArrayList();
+        Iterator it = visibleTemplates.iterator();
+        while (it.hasNext()) {
+            Template template = (Template) it.next();
+            if (accessManager.isGranted(template.getLocation(), Permission.READ)) {
+                templateList.add(template);
+            }
+        }
+        return templateList.iterator();
+    }
+
+    /**
+     * Get templates collection.
+     * @return Collection list containing templates as Template objects
+     */
+    public Iterator getAvailableTemplates() {
+        return visibleTemplates.iterator();
+    }
+
+    /**
+     * @return Returns the instance.
+     */
+    public static TemplateManager getInstance() {
+        return instance;
+    }
+
+}
