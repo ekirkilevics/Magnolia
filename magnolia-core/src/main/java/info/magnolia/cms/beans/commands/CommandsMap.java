1afd87ed23c79b0f5c111058a2d5f2634a6b4111
Jackie Ju <noreply@magnolia-cms.com>
2006-03-28 11:31:27 +0000

    change getCommandFromFullName()
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@2466 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/cms/beans/commands/CommandsMap.java
+++ magnolia-core/src/main/java/info/magnolia/cms/beans/commands/CommandsMap.java
@@ -8,44 +8,54 @@ import org.apache.log4j.Logger;
 /**
  * This is used for backward compatibility to access tree and flow commands. We
  * should move to CatalogFactory straight from the calling code.
- *
+ * 
  * @author Jackie
  * @author Nicolas
  */
 public class CommandsMap {
 
-    private static Logger log = Logger.getLogger(CommandsMap.class);
+	private static Logger log = Logger.getLogger(CommandsMap.class);
 
-    static Class klass;
+	static Class klass;
 
-    static {
-        try {
+	static {
+		try {
 
-            klass = Class.forName("info.magnolia.cms.beans.commands.MgnlCatalogFactory");
-        } catch (Exception e) {
-            log.error("Could not load command factory", e);
-        }
-    }
+			klass = Class
+					.forName("info.magnolia.cms.beans.commands.MgnlCatalogFactory");
+		} catch (Exception e) {
+			log.error("Could not load command factory", e);
+		}
+	}
 
-    public static MgnlCommand getCommand(String catalogName, String commandName) {
-        CatalogFactory factory = (CatalogFactory) FactoryUtil.getSingleton(klass);
-        Catalog catalog = factory.getCatalog(catalogName);
-        return (MgnlCommand) catalog.getCommand(commandName);
-    }
+	public static MgnlCommand getCommand(String catalogName, String commandName) {
+		CatalogFactory factory = (CatalogFactory) FactoryUtil
+				.getSingleton(klass);
+		Catalog catalog = factory.getCatalog(catalogName);
+		return (MgnlCommand) catalog.getCommand(commandName);
+	}
 
-    public static MgnlCommand getCommand(String commandName) {
-        CatalogFactory factory = (CatalogFactory) FactoryUtil.getSingleton(klass);
-        Catalog catalog = factory.getCatalog("");
-        return (MgnlCommand) catalog.getCommand(commandName);
-    }
+	public static MgnlCommand getCommand(String commandName) {
+		CatalogFactory factory = (CatalogFactory) FactoryUtil
+				.getSingleton(klass);
+		Catalog catalog = factory.getCatalog("");
+		return (MgnlCommand) catalog.getCommand(commandName);
+	}
 
-    public static MgnlCommand getCommandFromFullName(String commandName) {
-        String command_ = commandName.substring(MgnlCommand.PREFIX_COMMAND_LEN);
-        int index1 = command_.indexOf(MgnlCommand.COMMAND_DELIM);
-        int index2 = command_.indexOf(MgnlCommand.COMMAND_DELIM, index1);
-        String catalog = command_.substring(index1 + 1, index2);
-        String name = command_.substring(index2 + 1);
-        return getCommand(catalog, name);
-    }
+	public static MgnlCommand getCommandFromFullName(String commandName) {
+		String command_ = commandName.substring(MgnlCommand.PREFIX_COMMAND_LEN);
+		int index1 = command_.indexOf(MgnlCommand.COMMAND_DELIM);
+		int index2 = command_.indexOf(MgnlCommand.COMMAND_DELIM, index1);
+		String catalog = null;
+		String name = null;
+		if (index2 < 0) {
+			catalog = "";
+			name = command_.substring(index1 + 1);
+		} else {
+			catalog = command_.substring(index1 + 1, index2);
+			name = command_.substring(index2 + 1);
+		}
+		return getCommand(catalog, name);
+	}
 
 }
