804ec821bd89abeed3f92ac43b02077a9976ad23
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2006-04-27 14:47:58 +0000

    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@2720 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-module-admininterface/src/main/java/info/magnolia/module/admininterface/lists/AbstractList.java
@@ -0,0 +1,203 @@
+/**
+ *
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2005 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
+ */
+package info.magnolia.module.admininterface.lists;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import info.magnolia.cms.gui.control.ContextMenu;
+import info.magnolia.cms.gui.control.FunctionBar;
+import info.magnolia.cms.gui.controlx.RenderKit;
+import info.magnolia.cms.gui.controlx.RenderKitFactory;
+import info.magnolia.cms.gui.controlx.list.ListControl;
+import info.magnolia.cms.gui.controlx.list.ListModel;
+import info.magnolia.cms.util.FreeMarkerUtil;
+import info.magnolia.module.admininterface.TemplatedMVCHandler;
+
+
+/**
+ * @author Philipp Bracher
+ * @version $Revision$ ($Author$)
+ */
+public abstract class AbstractList extends TemplatedMVCHandler {
+
+    private String sortBy = "";
+
+    private String sortByOrder = "asc";
+
+    private String groupBy = "";
+
+    private String groupByOrder = "asc";
+
+    /**
+     * Control used.
+     */
+    private ListControl list;
+
+    /**
+     * The function bar shown at the bottom
+     */
+    private FunctionBar functionBar;
+
+    /**
+     * @param name
+     * @param request
+     * @param response
+     */
+    protected AbstractList(String name, HttpServletRequest request, HttpServletResponse response) {
+        super(name, request, response);
+    }
+    
+    /**
+     * Don't use the class name.
+     */
+    protected String getTemplateName(String viewName) {
+        return FreeMarkerUtil.createTemplateName(AbstractList.class, "html");
+    }
+    
+    /**
+     * @see com.obinary.magnolia.professional.PageMVCHandler#show()
+     */
+    public String show() {
+        String view = super.show();
+        ListControl list = this.getList();
+        configureList(list);
+        return view;
+    }
+
+    /**
+     * @param list
+     */
+    protected void configureList(ListControl list) {
+        list.setName("list");
+        list.setRenderKit(this.getRenderKit());
+        list.setContextMenu(this.getContextMenu());
+        list.setModel(this.getModel());
+        list.setSortBy(this.getSortBy());
+        list.setSortByOrder(this.getSortByOrder());
+        list.setGroupBy(this.getGroupBy());
+        list.setGroupByOrder(this.getGroupByOrder());
+    }
+
+
+    /**
+     * Returns the model used by this list
+     */
+    protected abstract ListModel getModel();
+
+    /**
+     * @param list The list to set.
+     */
+    public void setList(ListControl list) {
+        this.list = list;
+    }
+
+    /**
+     * @return Returns the list.
+     */
+    public ListControl getList() {
+        if (list == null) {
+            list = new ListControl();
+        }
+        return list;
+    }
+
+    public ContextMenu getContextMenu() {
+        return null;
+    }
+
+    /**
+     * Returns the default admin interface render kit.
+     */
+    protected RenderKit getRenderKit() {
+        return RenderKitFactory.getRenderKit(RenderKitFactory.ADMIN_INTERFACE_RENDER_KIT);
+    }
+
+    /**
+     * @return Returns the functionBar.
+     */
+    public FunctionBar getFunctionBar() {
+        return this.functionBar;
+    }
+
+    /**
+     * @param functionBar The functionBar to set.
+     */
+    public void setFunctionBar(FunctionBar functionBar) {
+        this.functionBar = functionBar;
+    }
+
+    /**
+     * @return Returns the groupBy.
+     */
+    public String getGroupBy() {
+        return this.groupBy;
+    }
+
+    /**
+     * @param groupBy The groupBy to set.
+     */
+    public void setGroupBy(String groupBy) {
+        this.groupBy = groupBy;
+    }
+
+    /**
+     * @return Returns the groupByOrder.
+     */
+    public String getGroupByOrder() {
+        return this.groupByOrder;
+    }
+
+    /**
+     * @param groupByOrder The groupByOrder to set.
+     */
+    public void setGroupByOrder(String groupByOrder) {
+        this.groupByOrder = groupByOrder;
+    }
+
+    /**
+     * @return Returns the sortBy.
+     */
+    public String getSortBy() {
+        return this.sortBy;
+    }
+
+    /**
+     * @param sortBy The sortBy to set.
+     */
+    public void setSortBy(String sortBy) {
+        this.sortBy = sortBy;
+    }
+
+    /**
+     * @return Returns the sortByOrder.
+     */
+    public String getSortByOrder() {
+        return this.sortByOrder;
+    }
+
+    /**
+     * @param sortByOrder The sortByOrder to set.
+     */
+    public void setSortByOrder(String sortByOrder) {
+        this.sortByOrder = sortByOrder;
+    }
+
+    /**
+     * Do some additional rendering in the subclass
+     */
+    public String onRender() {
+        return "";
+    }
+
+}
