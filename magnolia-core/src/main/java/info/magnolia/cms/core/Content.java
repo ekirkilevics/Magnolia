0bb46c2a84802ef111d671067b814e186ee6b06f
Sameer Charles <sameer.charles@magnolia-cms.com>
2006-04-27 13:14:29 +0000

    add meta data properties needed for version must be added by VersionManager
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@2713 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/cms/core/Content.java
+++ magnolia-core/src/main/java/info/magnolia/cms/core/Content.java
@@ -130,7 +130,6 @@ public class Content extends ContentHandler implements Cloneable {
         this.setRootNode(rootNode);
         this.node = this.rootNode.addNode(this.path, contentType);
         this.setAccessManager(manager);
-        this.getMetaData().setProperty(MetaData.NAME, this.getName());
         this.addMixin(ItemType.MIX_VERSIONABLE);
     }
 
