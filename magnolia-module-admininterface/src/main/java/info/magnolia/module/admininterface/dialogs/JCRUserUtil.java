07aab8c7c8fc271d6361caada5feb320aefb85a6
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2006-05-03 06:44:23 +0000

    this class should not exist --> marked as deprecated
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@2810 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-admininterface/src/main/java/info/magnolia/module/admininterface/dialogs/JCRUserUtil.java
+++ magnolia-module-admininterface/src/main/java/info/magnolia/module/admininterface/dialogs/JCRUserUtil.java
@@ -14,6 +14,9 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * @deprecated This class should NOT exist. All the mothods should be moved to the Group or User interface.
+ */
 public class JCRUserUtil {
 	HierarchyManager hm = ContentRepository
 	.getHierarchyManager(ContentRepository.USERS);
