9212b1197634bf4fa44981c98f275d19914c6492
Fabrizio Giustina <fgiust@openmindonline.it>
2006-05-06 10:04:17 +0000

    cleanup
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@2945 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-openwfe/src/main/java/info/magnolia/module/owfe/Engine.java
+++ magnolia-module-openwfe/src/main/java/info/magnolia/module/owfe/Engine.java
@@ -42,7 +42,6 @@ import org.apache.log4j.Logger;
 
 /**
  * Module "templating" main class.
- *
  * @author Sameer Charles
  * @author Fabrizio Giustina
  * @version 2.0
@@ -66,32 +65,17 @@ public class Engine extends AbstractAdminModule {
      */
     protected void onInit() {
 
-        // register servlet
         try {
-            String servletClassName = FlowDefServlet.class.getName();
-            ModuleUtil.registerServlet("FlowDef", servletClassName, new String[]{"/FlowDef"}, "registered by Jackie");
-        }
-        catch (Exception e) {
-            log.error("Error while loading the xml rpc module", e);
-        }
 
-        log.info("****************************************");
-        log.info("Module: " + this.getName()); //$NON-NLS-1$
-        log.info(this.getName() + ": starting OpenWFE"); //$NON-NLS-1$
-        try {
-            if (log.isDebugEnabled())
-                log.debug("create owfe engine ...");
+            log.debug("create owfe engine ...");
             new OWFEEngine();// .run();
-            if (log.isDebugEnabled())
-                log.debug("create owfe engine ok.");
+
+            log.debug("create owfe engine ok.");
 
         }
         catch (Exception e) {
             log.error("An exception arised when creating the engine", e);
         }
-
-        log.info(this.getName() + ": start OpenWFE OK."); //$NON-NLS-1$      
-        log.info("****************************************");
 
     }
 
@@ -108,23 +92,7 @@ public class Engine extends AbstractAdminModule {
             ret += (p.getName() + "=" + p.getString() + "\r\n");
         }
         ret += "\r\n";
-        //		
-        // List list = node.collectAllChildren();
-        // for (int i = 0; i < list.size(); i++){
-        // Content n = (Content) list.get(i);
-        // ret += "node name: " + n.getName() + "\r\n";
-        // ret += "title: " + n.getTitle() + "\r\n";
-        // ret += "JCRNode: " + n.getJCRNode().getName() + "(" +
-        // n.getJCRNode().getPath() + ")" + "\r\n";
-        // ret += "type: " + n.getNodeType().getName() + "\r\n";
-        // ret += "Propertie: \r\n";
-        // r = n.getNodeDataCollection().iterator();
-        // while (r.hasNext()) {
-        // NodeData p = (NodeData) r.next();
-        // ret += (p.getName() + "=" + p.getString() + "\r\n");
-        // }
-        // ret += "\r\n";
-        // }
+
         ret += ("\r\n");
 
         if (child) {
@@ -142,13 +110,6 @@ public class Engine extends AbstractAdminModule {
         ret += "node name: " + node.getName() + "\r\n";
         ret += "path: " + node.getPath() + "\r\n";
         ret += "Propertie: \r\n";
-        // PropertyIterator r = node.getProperties();
-        // while (r.hasNext()){
-        // Property p = r.nextProperty();
-        //			
-        // ret += (p.getName() + "=" + p.toString() + "\r\n");
-        //			
-        // }
 
         ret += ("defintion: " + node.getDefinition().getName() + "\r\n");
         ret += ("\r\n");
@@ -163,7 +124,6 @@ public class Engine extends AbstractAdminModule {
         }
         return ret;
     }
-
 
     void testJCREngine() {
 
@@ -180,7 +140,6 @@ public class Engine extends AbstractAdminModule {
 
             Content root = hm.getRoot();
             log.info("root = " + root);
-            log.info("--------------HierarchyManager----------:\r\n" + printHMNode(root, true) + "\r\n---------------");
 
             // create one node
             Content nc = root.createContent("test", ItemType.WORKITEM);
@@ -196,28 +155,11 @@ public class Engine extends AbstractAdminModule {
 
             // list content nodes
             Collection items;
-            // items = root.getChildren(ItemType.WORKITEM);
-            // if (items != null) {
-            // log.info("work items number = " + items.size());
-            // Iterator it = items.iterator();
-            // int i = 0;
-            // while (it.hasNext()) {
-            // Content tmp = (Content) it.next();
-            // log.info("node " + i++);
-            // log.info("----------------");
-            // log.info("node name = " + tmp.getName());
-            // log.info("title = " + tmp.getTitle());
-            // log.info("ID= " + tmp.getNodeData("ID").getString());
-            // log.info("----------------");
-            // }
-            //
-            // }
 
             // test query manager
-            log.info("------Test query manager--------");
             QueryManager qm = hm.getQueryManager();
             if (qm == null) {
-                throw new Exception("no query manager for repoistory");
+                throw new Exception("no query manager for repository");
             }
             Query q = qm.createQuery("test", Query.XPATH);
             QueryResult result = q.execute();
@@ -235,17 +177,11 @@ public class Engine extends AbstractAdminModule {
 
                 }
             }
-            log.info("----------------");
         }
         catch (Exception e) {
             log.error(e.getMessage(), e);
         }
 
-        /*
-         * // travesal the repository try { Node rn = hm.getWorkspace().getSession().getRootNode();
-         * log.info("--------------repository----------:\r\n" + printJCRNode(rn, true) + "\r\n---------------"); } catch
-         * (Exception e) { e.printStackTrace(); return; }
-         */
     }
 
     public void destroy() {
@@ -253,10 +189,11 @@ public class Engine extends AbstractAdminModule {
             log.info("Workflow engine is running, trying to stop...");
             try {
                 // before try to stop purge and scheduling tasks
-                ((SimpleExpressionPool)OWFEEngine.getEngine().getExpressionPool()).stop();
+                ((SimpleExpressionPool) OWFEEngine.getEngine().getExpressionPool()).stop();
                 OWFEEngine.getEngine().stop();
                 log.info("Workflow engine successfully stopped");
-            } catch (ServiceException se) {
+            }
+            catch (ServiceException se) {
                 log.error("Failed to stop Open WFE engine");
                 log.error(se.getMessage(), se);
             }
