bcfb5e269e65222dad03a84104428669e10a272f
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2006-05-10 13:25:06 +0000

    scripts for the website list
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@3020 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-module-admininterface/src/main/resources/mgnl-resources/js-classes/mgnl/admininterface/WebsiteSearchList.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2006 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
+ */
+ 
+ /**
+  * Functions to handle the js in the website search result
+  */
+ classDef("mgnl.admininterface.WebsiteSearchList", {
+    selected: null,
+    
+    search: function(value){
+        $('searchStr').value = value;
+        document.mgnlForm.submit();
+    },
+    
+    show: function(){
+        var url= contextPath + this.selected + ".html";
+        var w = window.open(url, "mgnlInline", "");
+            if (w) w.focus();
+    },
+    
+    navigate: function(){
+        MgnlAdminCentral.showTree('website', this.selected);
+    }
+ });
