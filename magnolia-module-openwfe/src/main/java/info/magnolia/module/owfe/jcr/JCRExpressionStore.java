85e4a4608b17167aa33aaee32bac24bb9a9faac1
Nicolas Modrzyk <nicolas.modrzyk@macnica.com>
2006-05-11 05:41:40 +0000

    fix as proposed by john to restore the application context to a freshly 'rehydrated' expression.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@3041 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-openwfe/src/main/java/info/magnolia/module/owfe/jcr/JCRExpressionStore.java
+++ magnolia-module-openwfe/src/main/java/info/magnolia/module/owfe/jcr/JCRExpressionStore.java
@@ -13,6 +13,7 @@
 package info.magnolia.module.owfe.jcr;
 
 import info.magnolia.cms.beans.config.ContentRepository;
+import info.magnolia.cms.beans.runtime.MgnlContext;
 import info.magnolia.cms.core.Content;
 import info.magnolia.cms.core.HierarchyManager;
 import info.magnolia.cms.core.ItemType;
@@ -222,6 +223,7 @@ public class JCRExpressionStore extends AbstractExpressionStore {
 
                     continue;
                 }
+                fe.setApplicationContext(getContext());
                 ret.add(fe);
             }
 
