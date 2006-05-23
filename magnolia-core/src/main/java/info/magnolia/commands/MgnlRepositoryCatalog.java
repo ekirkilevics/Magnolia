b866cd9f4b28f060cc81e86f851edb55b893a9de
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2006-05-23 18:14:51 +0000

    set properties of a command if configured
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@3244 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/commands/MgnlRepositoryCatalog.java
+++ magnolia-core/src/main/java/info/magnolia/commands/MgnlRepositoryCatalog.java
@@ -3,11 +3,12 @@ package info.magnolia.commands;
 import info.magnolia.cms.core.Content;
 import info.magnolia.cms.core.ItemType;
 import info.magnolia.cms.core.NodeData;
+import info.magnolia.cms.util.ContentUtil;
+
 import org.apache.commons.chain.Chain;
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.impl.CatalogBase;
 import org.apache.commons.chain.impl.ChainBase;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +16,7 @@ import java.util.Collection;
 import java.util.Iterator;
 
 /**
+ * A command catalog using a repository node as a configuration. Chains are supported.
  * Date: Mar 27, 2006 Time: 10:58:22 AM
  * @author <a href="mailto:niko@macnica.com">Nicolas Modrzyk</a>
  */
@@ -36,28 +38,16 @@ public class MgnlRepositoryCatalog extends CatalogBase {
             Content actionNode = (Content) iter.next();
             String actionName = actionNode.getName();
 
-            String className = StringUtils.EMPTY;
-
             NodeData impl = actionNode.getNodeData(CLASS_NODE_DATA);
             if (impl != null && impl.getString() != null && !(impl.getString().equals(""))) {
 
-                log.debug("This is a simple action: {}", actionName);
-
-                // this is a simple command
-                className = impl.getString();
-
                 Command command;
-                try {
-                    Class klass = Class.forName(className);
-                    command = (Command) klass.newInstance();
-                }
-                catch (Exception e) {
-                    log.error("Could not load action:" + actionName, e);
-                    continue;
-                }
-
-                this.addCommand(actionName, command);
-
+				try {
+					command = createCommand(actionNode);
+	                this.addCommand(actionName, command);
+				} catch (Exception e) {
+					log.error("can't initialize command [" + actionName + "]", e);
+				}
                 // continue with next action
             }
             else {
@@ -76,11 +66,7 @@ public class MgnlRepositoryCatalog extends CatalogBase {
                 while (iterNode.hasNext()) {
                     try {
                         Content commandNode = (Content) iterNode.next();
-                        className = commandNode.getNodeData(CLASS_NODE_DATA).getString();
-
-                        log.debug("Found class {} for action {}", className, actionName);
-                        Class klass = Class.forName(className);
-                        Command command = (Command)klass.newInstance();
+                        Command command = createCommand(commandNode);
                         chain.addCommand(command);
                     }
                     catch (Exception te) {
@@ -101,4 +87,24 @@ public class MgnlRepositoryCatalog extends CatalogBase {
         }
 
     }
+
+
+    /**
+     * Create a command object based on a node
+     * @param commandNode
+     * @return
+     * @throws ClassNotFoundException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     */
+	private Command createCommand(Content commandNode) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		String className;
+		className = commandNode.getNodeData(CLASS_NODE_DATA).getString();
+
+		log.debug("Found class {} for action {}", className, commandNode.getName());
+		Class klass = Class.forName(className);
+		Command command = (Command)klass.newInstance();
+		ContentUtil.setProperties(command, commandNode);
+		return command;
+	}
 }
