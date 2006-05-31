3b82011fc814ac050855f06b34c23a53d0bbf894
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2006-05-31 09:49:32 +0000

    import
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@3315 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-gui/src/main/java/info/magnolia/cms/gui/fckeditor/FCKEditorSimpleUploadServlet.java
+++ magnolia-gui/src/main/java/info/magnolia/cms/gui/fckeditor/FCKEditorSimpleUploadServlet.java
@@ -44,7 +44,6 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
