1cdd4e4b56381f4f66914444e72834d8dcb70e93
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2006-07-28 15:45:07 +0000

    format and comments
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@5653 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/cms/beans/config/URI2RepositoryMapping.java
+++ magnolia-core/src/main/java/info/magnolia/cms/beans/config/URI2RepositoryMapping.java
@@ -12,21 +12,21 @@
  */
 package info.magnolia.cms.beans.config;
 
-import info.magnolia.cms.core.Path;
-
 import org.apache.commons.lang.StringUtils;
 
+
 /**
- * Describes a mapping
+ * Describes a uri to repository mapping
  * @author Philipp Bracher
  * @version $Id$
  */
-public class URI2RepositoryMapping{
+public class URI2RepositoryMapping {
+
     /**
      * The prefix which triggers this mapping
      */
     private String uriPrefix;
-    
+
     /**
      * The repository used for this mapping
      */
@@ -48,72 +48,87 @@ public class URI2RepositoryMapping{
         this.repository = repository;
         this.handlePrefix = handlePrefix;
     }
-    
+
     public URI2RepositoryMapping() {
     }
 
-    public boolean matches(String uri){
+    /**
+     * True if this mapping can get applied to the specified uri
+     * @param uri
+     * @return
+     */
+    public boolean matches(String uri) {
         return uri.startsWith(uriPrefix);
     }
-    
-    public String getHandle(String uri){
+
+    /**
+     * Create a node handle based on an uri
+     * @param uri
+     * @return
+     */
+    public String getHandle(String uri) {
         String handle;
         handle = StringUtils.removeStart(uri, this.uriPrefix);
-        if(StringUtils.isNotEmpty(this.handlePrefix)){
+        if (StringUtils.isNotEmpty(this.handlePrefix)) {
             StringUtils.removeStart(handle, "/");
             handle = this.handlePrefix + "/" + handle;
         }
         return cleanHandle(handle);
     }
 
+    /**
+     * Clean a handle. Remove double / and add allways a leading /
+     * @param handle
+     * @return
+     */
     private String cleanHandle(String handle) {
-        if(!handle.startsWith("/")){
+        if (!handle.startsWith("/")) {
             handle = "/" + handle;
         }
         handle = StringUtils.replace(handle, "//", "/");
         return handle;
     }
 
+    /**
+     * Create a uri based on a handle
+     * @param handle
+     * @return
+     */
     public String getURI(String handle) {
-        if(StringUtils.isNotEmpty(this.handlePrefix)){
+        if (StringUtils.isNotEmpty(this.handlePrefix)) {
             handle = StringUtils.removeStart(handle, this.handlePrefix);
         }
-        if(StringUtils.isNotEmpty(this.uriPrefix)){
+        if (StringUtils.isNotEmpty(this.uriPrefix)) {
             handle = this.uriPrefix + "/" + handle;
         }
         return cleanHandle(handle);
     }
-    
+
     public String getHandlePrefix() {
         return handlePrefix;
     }
 
-    
     public void setHandlePrefix(String handlePrefix) {
         this.handlePrefix = handlePrefix;
     }
 
-    
     public String getRepository() {
         return repository;
     }
 
-    
     public void setRepository(String repository) {
         this.repository = repository;
     }
 
-    
     public String getUriPrefix() {
         return uriPrefix;
     }
 
-    
     public void setUriPrefix(String uriPrefix) {
         this.uriPrefix = uriPrefix;
     }
-    
+
     public String toString() {
-        return this.uriPrefix + " --> " + repository + ":" +  this.handlePrefix;
+        return this.uriPrefix + " --> " + repository + ":" + this.handlePrefix;
     }
 }
