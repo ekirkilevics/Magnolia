606c559184cad1c157bbf93f06273bae95511501
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2006-08-02 12:35:31 +0000

    link is using uuid instead of path if it is of type uuidLink - UUIDSaveHandler is the default one - UUIDDialogControl marker interface - link paragraph adapted
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@5706 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-module-admininterface/src/main/java/info/magnolia/module/admininterface/UUIDSaveHandler.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package info.magnolia.module.admininterface;
+
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+
+import org.apache.commons.lang.StringUtils;
+
+import info.magnolia.cms.beans.config.ContentRepository;
+import info.magnolia.cms.core.Content;
+import info.magnolia.cms.gui.dialog.Dialog;
+import info.magnolia.cms.gui.dialog.DialogControlImpl;
+import info.magnolia.cms.gui.dialog.UUIDDialogControl;
+import info.magnolia.cms.security.AccessDeniedException;
+import info.magnolia.cms.util.ContentUtil;
+
+
+/**
+ * This save handler checks each control if it is implementing the UUIDDialogControl interface. If so it stores the uuid
+ * instead of the path.
+ * @author Philipp Bracher
+ * @version $Id$
+ */
+public class UUIDSaveHandler extends SaveHandlerImpl implements DialogAwareSaveHandler {
+
+    /**
+     * The dialog we are saving
+     */
+    private Dialog dialog;
+
+    public Dialog getDialog() {
+        return dialog;
+    }
+
+    public void setDialog(Dialog dialog) {
+        this.dialog = dialog;
+    }
+
+    /**
+     * Process a singel value
+     */
+    protected void processString(Content node, String name, int type, int encoding, String[] values, String valueStr)
+        throws PathNotFoundException, RepositoryException, AccessDeniedException {
+        DialogControlImpl control = getControl(name);
+        if (control instanceof UUIDDialogControl) {
+            // convert it to an uuid
+            if (StringUtils.isNotEmpty(valueStr)) {
+                valueStr = getUUID(name, valueStr);
+            }
+        }
+
+        super.processString(node, name, type, encoding, values, valueStr);
+    }
+
+    /**
+     * Process a multiple value
+     */
+    protected void processMultiple(Content node, String name, int type, String[] values) throws RepositoryException,
+        PathNotFoundException, AccessDeniedException {
+        DialogControlImpl control = getControl(name);
+
+        if (control instanceof UUIDDialogControl) {
+            if (values != null && values.length != 0) {
+                String[] uuidValues = new String[values.length];
+                for (int i = 0; i < values.length; i++) {
+
+                }
+                values = uuidValues;
+            }
+        }
+        super.processMultiple(node, name, type, values);
+    }
+
+    /**
+     * Get the contorl which is processed now
+     * @param name
+     * @return the control or null if not found
+     */
+    protected DialogControlImpl getControl(String name) {
+        Object control = this.getDialog().getSub(name);
+        if (control instanceof DialogControlImpl) {
+            return (DialogControlImpl) control;
+        }
+        return null;
+    }
+
+    /**
+     * Convert the path to an uuid. It uses the config value 'repository' of the control if found.
+     * @param name
+     * @param path
+     * @return thr uuid or the path if the path was not found
+     */
+    private String getUUID(String name, String path) {
+        String repository = ((UUIDDialogControl)getControl(name)).getRepository();
+        Content node = ContentUtil.getContent(repository, path);
+        if (node == null) {
+            return path;
+        }
+        return node.getUUID();
+    }
+
+}
