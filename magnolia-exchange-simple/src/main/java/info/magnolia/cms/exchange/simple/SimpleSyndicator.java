bd058885ba23ca4088fea67f27684b0c1e1a1ad1
Sameer Charles <sameer.charles@magnolia-cms.com>
2006-08-08 15:42:44 +0000

    Bug fix : added parameter 'noRecurse' to control export
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@5773 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-exchange-simple/src/main/java/info/magnolia/cms/exchange/simple/SimpleSyndicator.java
+++ magnolia-exchange-simple/src/main/java/info/magnolia/cms/exchange/simple/SimpleSyndicator.java
@@ -611,11 +611,11 @@ public class SimpleSyndicator implements Syndicator {
          * nt:file node type has mandatory sub nodes
          */
         if (content.isNodeType(ItemType.NT_FILE)) {
-            DataTransporter.parseAndFormat(gzipOutputStream, elementfilter, "", content.getHandle(), session);
+            DataTransporter.parseAndFormat(gzipOutputStream, elementfilter, "", content.getHandle(), session, true);
         }
         else {
             DataTransporter
-                .parseAndFormat(gzipOutputStream, elementfilter, "", content.getJCRNode().getPath(), session);
+                .parseAndFormat(gzipOutputStream, elementfilter, "", content.getJCRNode().getPath(), session, true);
         }
         IOUtils.closeQuietly(gzipOutputStream);
         // add file entry in mapping.xml
