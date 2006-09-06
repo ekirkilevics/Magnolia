3857c281d0b5813359734f12fccd23dc0f0e4ee5
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2006-09-06 07:42:37 +0000

    handling the uuid is delegated to the parent (still not nice at all)
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@6232 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-gui/src/main/java/info/magnolia/cms/gui/dialog/DialogUUIDLink.java
+++ magnolia-gui/src/main/java/info/magnolia/cms/gui/dialog/DialogUUIDLink.java
@@ -12,14 +12,7 @@
  */
 package info.magnolia.cms.gui.dialog;
 
-import javax.jcr.RepositoryException;
-
-import org.apache.commons.lang.StringUtils;
-
 import info.magnolia.cms.beans.config.ContentRepository;
-import info.magnolia.cms.core.Content;
-import info.magnolia.cms.core.HierarchyManager;
-import info.magnolia.context.MgnlContext;
 
 
 /**
@@ -27,42 +20,6 @@ import info.magnolia.context.MgnlContext;
  * @version $Id$
  */
 public class DialogUUIDLink extends DialogLink implements UUIDDialogControl {
-
-    /**
-     * The lazy bound uuid
-     */
-    private String path;
-
-    /**
-     * Convert the stored uuid to a path
-     */
-    public String getValue() {
-        if (path == null) {
-            String value = super.getValue();
-            path = getPath(value);
-        }
-        return path;
-    }
-
-    /**
-     * Convert the uuid to a path or return the uuid if the path can't get found.
-     * @param uuid
-     * @return the path
-     */
-    private String getPath(String uuid) {
-        if(StringUtils.isNotEmpty(uuid)){
-            HierarchyManager hm = MgnlContext.getHierarchyManager(this.getRepository());
-            try {
-                Content node = hm.getContentByUUID(uuid);
-                return node.getHandle();
-            }
-            catch (Exception e) {
-                // return the uuid
-            }
-            
-        }
-        return uuid;
-    }
 
     /**
      * Get the configured repository
