e5b71e47c5715a958945ff03944b2d93f8329a9d
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2006-09-11 18:54:45 +0000

    allows an other command name instead of a class in the impl. default_activate for example
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@6322 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/commands/DelegateCommand.java
@@ -0,0 +1,43 @@
+package info.magnolia.commands;
+
+import org.apache.commons.chain.Command;
+import org.apache.commons.chain.Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Delegate to an other command at runtime
+ * @author Philipp Bracher
+ * @version $Id$
+ *
+ */
+public class DelegateCommand implements Command {
+    /**
+     * Log
+     */
+    Logger log = LoggerFactory.getLogger(DelegateCommand.class);
+
+    /**
+     * The command name used to delegate to
+     */
+    String commandName;
+    
+    /**
+     * @param commandName
+     */
+    public DelegateCommand(String commandName) {
+        this.commandName = commandName;
+    }
+
+    public boolean execute(Context ctx) throws Exception {
+        Command cmd = CommandsManager.getInstance().getCommand(commandName);
+        if(cmd != null){
+            return cmd.execute(ctx);
+        }
+        else{
+            log.error("can't find command {}", this.commandName);
+        }
+        return false;
+    }
+
+}
