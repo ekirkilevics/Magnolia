9f954403d3d6950a9086a0c4282aca5e65ce3380
Nicolas Modrzyk <nicolas.modrzyk@macnica.com>
2006-09-17 03:48:51 +0000

    - tree handler and configuration to view workflow items in the jcr (also added one key  for the menu shortcut in messages.properties)
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@6401 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-module-workflow/src/main/java/info/magnolia/module/workflow/trees/WorkflowTreeConfiguration.java
@@ -0,0 +1,20 @@
+package info.magnolia.module.workflow.trees;
+
+import info.magnolia.cms.core.ItemType;
+import info.magnolia.cms.gui.control.Tree;
+import info.magnolia.module.admininterface.trees.ConfigTreeConfiguration;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Tree to view workitems and expressions
+ */
+public class WorkflowTreeConfiguration extends ConfigTreeConfiguration {
+
+    public void prepareTree(Tree tree, boolean browseMode, HttpServletRequest request) {
+        super.prepareTree(tree,browseMode,request);
+        tree.addItemType(ItemType.WORKITEM);
+        tree.addItemType(ItemType.NT_UNSTRUCTRUED);
+    }
+
+}
