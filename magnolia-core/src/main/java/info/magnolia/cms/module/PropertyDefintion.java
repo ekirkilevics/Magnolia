526bf7d1f7f5add158e40891ba9610a873b110a8
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2006-11-09 18:54:52 +0000

    additional properties can get defined in the module descriptor - properties loaded according to the dependencies - separation of reading (init) and registration of the modules (registerModules) - PropertyDefintion
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@7279 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/cms/module/PropertyDefintion.java
@@ -0,0 +1,47 @@
+/**
+ *
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2005 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
+ */
+package info.magnolia.cms.module;
+
+public class PropertyDefintion {
+    private String name;
+
+    private String value;
+
+    /**
+     * @return Returns the name.
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * @param name The name to set.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return Returns the value.
+     */
+    public String getValue() {
+        return this.value;
+    }
+
+    /**
+     * @param value The value to set.
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
