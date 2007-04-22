5c33cd9e05b1813d640f17d9f7bba2d6a21fa0b9
Fabrizio Giustina <fgiust@openmindonline.it>
2007-04-22 16:03:40 +0000

    Some addictional cleanup and check for MAGNOLIA-1446 : always check mkdirs return value before trying to create a new file. Remove a bunch of duplicated code between standard/gzipped cache handling.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@9139 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-cache/src/main/java/info/magnolia/cms/cache/simple/CacheImpl.java
+++ magnolia-module-cache/src/main/java/info/magnolia/cms/cache/simple/CacheImpl.java
@@ -1,28 +1,27 @@
 package info.magnolia.cms.cache.simple;
 
+import info.magnolia.cms.beans.config.ContentRepository;
 import info.magnolia.cms.cache.Cache;
 import info.magnolia.cms.cache.CacheConfig;
 import info.magnolia.cms.cache.CacheKey;
 import info.magnolia.cms.cache.CacheableEntry;
 import info.magnolia.cms.core.Path;
-import info.magnolia.cms.beans.config.ContentRepository;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Date;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.zip.GZIPOutputStream;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.jcr.observation.EventListener;
-import javax.jcr.observation.EventIterator;
-import javax.jcr.observation.ObservationManager;
-import javax.jcr.observation.Event;
 import javax.jcr.RepositoryException;
+import javax.jcr.observation.Event;
+import javax.jcr.observation.EventIterator;
+import javax.jcr.observation.EventListener;
+import javax.jcr.observation.ObservationManager;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
@@ -67,7 +66,7 @@ public class CacheImpl implements Cache {
         if (currentlyRemoving) {
             return; // simply ignore this cache request
         }
-        int compressedSize = 0;
+
         File file = getFile(key, false);
 
         if (file.isDirectory()) {
@@ -75,54 +74,59 @@ public class CacheImpl implements Cache {
             return;
         }
 
+        long fileSize = cacheSingleFile(key, entry, file, false);
+
+        long compressedSize = 0;
+        if (canCompress) {
+            compressedSize = cacheSingleFile(key, entry, getFile(key, true), true);
+        }
+
+        if (fileSize > 0) {
+            addToCachedURIList(key, System.currentTimeMillis(), (int) fileSize, (int) compressedSize);
+        }
+        else {
+            log.debug("Not caching {}, empty file saved.", key.toString());
+        }
+    }
+
+    /**
+     * @param key
+     * @param entry
+     * @param file
+     */
+    private long cacheSingleFile(CacheKey key, CacheableEntry entry, File file, boolean compress) {
         // it should not cache again if the resource already existing
-        // its a responsibility of a cache manager to call remove or flush if resource needs to be
-        // invalidated
+        // its a responsibility of a cache manager to call remove or flush if resource needs to be invalidated
         if (!file.exists()) {
-            FileOutputStream out = null;
+            OutputStream out = null;
             if (log.isDebugEnabled()) {
                 log.debug("creating file {}", file.getAbsolutePath());
             }
             try {
-                mkdirs(file.getParentFile());
+                boolean parentDirectoryReady = mkdirs(file.getParentFile());
+                if (!parentDirectoryReady) {
+                    log.warn("Failed to cache {}, unable to create parent directory", key.toString(), file
+                        .getParentFile()
+                        .getAbsolutePath());
+                    return 0;
+                }
                 file.createNewFile();
 
                 out = new FileOutputStream(file);
+                if (compress) {
+                    out = new GZIPOutputStream(out);
+                }
                 out.write(entry.getOut());
                 out.flush();
             }
-            catch (Exception e) {
-                log.error("Failed to cache " + key.toString() + " to " + file.getAbsolutePath(), e);
+            catch (IOException e) {
+                log.warn("Failed to cache " + key.toString() + " to " + file.getAbsolutePath(), e);
             }
             finally {
                 IOUtils.closeQuietly(out);
             }
         }
-
-        if (canCompress) {
-            File gzipFile = getFile(key, true);
-            if (!gzipFile.exists()) {
-                GZIPOutputStream gzipOut = null;
-                try {
-                    mkdirs(gzipFile.getParentFile());
-                    gzipFile.createNewFile();
-                    FileOutputStream out = new FileOutputStream(gzipFile);
-                    gzipOut = new GZIPOutputStream(out);
-                    gzipOut.write(entry.getOut());
-                    gzipOut.flush();
-                }
-                catch (Exception e) {
-                    log.error("Failed to create compressed entry for " + key.toString(), e);
-                }
-                finally {
-                    IOUtils.closeQuietly(gzipOut);
-                }
-            }
-
-            compressedSize = (new Long(gzipFile.length())).intValue();
-        }
-
-        addToCachedURIList(key, new Date().getTime(), (int) file.length(), compressedSize);
+        return file.length();
     }
 
     public synchronized void flush() {
@@ -134,7 +138,13 @@ public class CacheImpl implements Cache {
             }
 
             // this will create cache start directory again
-            mkdirs(cacheDir);
+            boolean mainCacheDirCreated = mkdirs(cacheDir);
+
+            if (!mainCacheDirCreated) {
+                log
+                    .warn("Unable to create cache dir {}. Will retry while caching entries.", cacheDir
+                        .getAbsolutePath());
+            }
 
             // clear in-memory cache also
             clearCachedURIList();
@@ -391,9 +401,10 @@ public class CacheImpl implements Cache {
      */
     private boolean mkdirs(File file) {
         if (file.exists()) {
-            return true;
+            return file.isDirectory(); // return true only if it's a directory
         }
-        if (file.mkdir()) {
+        if (file.mkdir() || (file.exists() && file.isDirectory())) { // the additional file.exists() check could
+            // prevent concurrency problems
             return true;
         }
         File canonFile;
@@ -407,8 +418,12 @@ public class CacheImpl implements Cache {
         if (null == parent) {
             return false;
         }
-        mkdirs(parent);
-        return canonFile.mkdir();
+
+        if (mkdirs(parent)) {
+            return canonFile.mkdir();
+        }
+
+        return false;
     }
 
     private static class CachedItem {
