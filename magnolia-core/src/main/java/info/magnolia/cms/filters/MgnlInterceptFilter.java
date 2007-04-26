6c89f106828e8d6ff42ebe15e81b4774d57aa703
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2007-04-26 08:45:02 +0000

    MAGNOLIA-1432: - don't do the same security checks as ContentSecurity
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@9183 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/cms/filters/MgnlInterceptFilter.java
+++ magnolia-core/src/main/java/info/magnolia/cms/filters/MgnlInterceptFilter.java
@@ -12,27 +12,19 @@
  */
 package info.magnolia.cms.filters;
 
+import info.magnolia.api.HierarchyManager;
 import info.magnolia.cms.beans.config.ContentRepository;
-import info.magnolia.cms.beans.config.URI2RepositoryManager;
-import info.magnolia.cms.beans.config.URI2RepositoryMapping;
 import info.magnolia.cms.core.Aggregator;
 import info.magnolia.cms.core.Content;
-import info.magnolia.cms.core.Path;
-import info.magnolia.cms.security.Permission;
 import info.magnolia.cms.util.ExclusiveWrite;
 import info.magnolia.cms.util.Resource;
 import info.magnolia.context.MgnlContext;
-import info.magnolia.api.HierarchyManager;
 
 import java.io.IOException;
 
 import javax.jcr.RepositoryException;
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -48,7 +40,7 @@ import org.slf4j.LoggerFactory;
  * @author Fabrizio Giustina
  * @version $Id$
  */
-public class MgnlInterceptFilter implements Filter {
+public class MgnlInterceptFilter extends AbstractMagnoliaFilter {
 
     /**
      * Request parameter: the INTERCEPT holds the name of an administrative action to perform.
@@ -95,71 +87,15 @@ public class MgnlInterceptFilter implements Filter {
      */
     private static Logger log = LoggerFactory.getLogger(MgnlInterceptFilter.class);
 
-    /**
-     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
-     */
-    public void init(FilterConfig filterConfig) throws ServletException {
-        // unused
-    }
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException{
 
-    /**
-     * @see javax.servlet.Filter#destroy()
-     */
-    public void destroy() {
-        // unused
-    }
-
-    /**
-     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse,
-     * javax.servlet.FilterChain)
-     */
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain filterChain) throws IOException,
-        ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) resp;
-
-        // @todo the isAuthorized method is duplicated from EntryServlet!
-        if (isAuthorized(request, response)) {
-            if (req.getParameter(INTERCEPT) != null) {
-            	MgnlInterceptFilter.setHandleAndMapping(request);
-                this.intercept(request, response);
-            }
+        if (request.getParameter(INTERCEPT) != null) {
+            this.intercept(request, response);
         }
 
-        filterChain.doFilter(req, resp);
+        chain.doFilter(request, response);
     }
 
-    private static void setHandleAndMapping(HttpServletRequest request) {
-        String uri = Path.getURI(request);
-        int firstDotPos = StringUtils.indexOf(uri, '.', StringUtils.lastIndexOf(uri, '/'));
-        String handle;
-        String selector;
-        String extension;
-        if (firstDotPos > -1) {
-            int lastDotPos = StringUtils.lastIndexOf(uri, '.');
-            handle = StringUtils.substring(uri, 0, firstDotPos);
-            selector = StringUtils.substring(uri, firstDotPos + 1, lastDotPos);
-            extension = StringUtils.substring(uri, lastDotPos + 1);
-        }
-        else {
-            // no dots (and no extension)
-            handle = uri;
-            selector = "";
-            extension = "";
-        }
-
-        URI2RepositoryMapping mapping = URI2RepositoryManager.getInstance().getMapping(uri);
-
-        // remove prefix if any
-        handle = mapping.getHandle(handle);
-
-        request.setAttribute(Aggregator.REPOSITORY, mapping.getRepository());
-        request.setAttribute(Aggregator.MAPPING, mapping);
-        request.setAttribute(Aggregator.HANDLE, handle);
-        request.setAttribute(Aggregator.SELECTOR, selector);
-        request.setAttribute(Aggregator.EXTENSION, extension);
-    }
 
     /**
      * Request and Response here is same as receivced by the original page so it includes all post/get data. Sub action
@@ -169,10 +105,10 @@ public class MgnlInterceptFilter implements Filter {
         String action = request.getParameter(INTERCEPT);
         String repository = request.getParameter(PARAM_REPOSITORY);
         String nodePath = request.getParameter(PARAM_PATH);
-        String handle = (String) request.getAttribute(Aggregator.HANDLE);
-        
+        String handle = Aggregator.getHandle();
+
         if (repository == null) {
-        	repository = (String) request.getAttribute(Aggregator.REPOSITORY);
+            repository = Aggregator.getRepository();
         }
 
         if (repository == null) {
@@ -185,7 +121,7 @@ public class MgnlInterceptFilter implements Filter {
                 // preview mode (button in main bar)
                 String preview = request.getParameter(Resource.MGNL_PREVIEW_ATTRIBUTE);
                 if (preview != null) {
-                	
+
                     // @todo IMPORTANT remove use of http session
                     HttpSession httpsession = request.getSession(true);
                     if (BooleanUtils.toBoolean(preview)) {
@@ -199,8 +135,8 @@ public class MgnlInterceptFilter implements Filter {
             else if (action.equals(ACTION_NODE_DELETE)) {
                 // delete paragraph
                 try {
-					Content page = hm.getContent(handle);
-					page.updateMetaData();
+                    Content page = hm.getContent(handle);
+                    page.updateMetaData();
                     hm.delete(nodePath);
                     hm.save();
                 }
@@ -229,23 +165,6 @@ public class MgnlInterceptFilter implements Filter {
                 }
             }
         }
-    }
-
-    /**
-     * Uses access manager to authorise this request.
-     * @param req HttpServletRequest as received by the service method
-     * @param res HttpServletResponse as received by the service method
-     * @return boolean true if read access is granted
-     * @throws IOException can be thrown when the servlet is unable to write to the response stream
-     */
-    protected boolean isAuthorized(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        if (MgnlContext.getAccessManager(ContentRepository.WEBSITE) != null) {
-            String path = StringUtils.substringBefore(Path.getURI(req), "."); //$NON-NLS-1$
-            if (!MgnlContext.getAccessManager(ContentRepository.WEBSITE).isGranted(path, Permission.READ)) {
-                res.sendError(HttpServletResponse.SC_FORBIDDEN);
-            }
-        }
-        return true;
     }
 
 }
