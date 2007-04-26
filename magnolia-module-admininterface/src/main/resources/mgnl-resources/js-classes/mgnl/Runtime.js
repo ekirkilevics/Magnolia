2df9f0d7c579c3e62694682dbfa42a4c96456b61
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2007-04-26 12:48:06 +0000

    MAGNOLIA-1488
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@9219 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-admininterface/src/main/resources/mgnl-resources/js-classes/mgnl/Runtime.js
+++ magnolia-module-admininterface/src/main/resources/mgnl-resources/js-classes/mgnl/Runtime.js
@@ -10,58 +10,88 @@
  * Copyright 1993-2006 obinary Ltd. (http://www.obinary.com) All rights reserved.
  *
  */
- 
+
 // we can not use package command yet: make sure that the var mgnl exists
 var mgnl = mgnl ? mgnl : new Object();
 
 // used to set the browser flags
-var agent = navigator.userAgent.toLowerCase() ; 
+var agent = navigator.userAgent.toLowerCase() ;
 
 /**
  * This is the main class which must be loaded to use the importClass method.
  */
 MgnlRuntime = mgnl.Runtime = {
-    
+
     /**
      * This flag is false if all classes are loaded in one file in a final version.
      */
     loadingOn: true,
-    
+
     /**
      * If one script is loading synchronized. The others mast load synchronized too.
      */
     loadSynchronized: false,
-    
+
     /**
      * Remember allready loaded scripts (do not load twice)
      */
     loaded: new Object(),
-    
+
     /**
      * Check the browser
      */
-    isIE            : ( agent.indexOf("msie") != -1 ),
-    isGecko     : !( agent.indexOf("msie") != -1 ),     // Firefox and others
-    isSafari        : ( agent.indexOf("safari") != -1 ),
-    isNetscape  : ( agent.indexOf("netscape") != -1 ),
-     
+    isIE       : ( agent.indexOf("msie") != -1 ),
+    isGecko    : !( agent.indexOf("msie") != -1 ),     // Firefox and others
+    isSafari   : ( agent.indexOf("safari") != -1 ),
+    isNetscape : ( agent.indexOf("netscape") != -1 ),
+
     /**
-     * Creates the package object structure if not yet existing and register the class. 
+     * Creates the package object structure if not yet existing and register the class.
      * @param name the full name (including the class name)
-     * @param prototypeOrKlass same as klass but can be the prototype of this class (used for a nicer syntax)
-     * @param klass a constructor or object defining the class
+     * @param superKlass (optional) the supper class or an object used as prototype
+     * @param klass a constructor or an object defining the class
+     * @param members (optional) an object containing the members (methods/properties)
+     * @param statics (optional) an object containing the static members
      */
-    classDef: function(name, prototypeOrKlass, klass){
+    classDef: function(name, superKlass, klass, members, statics ){
         var names = name.split(".");
         var current = window;
 
-        if(!klass){
-            klass = prototypeOrKlass;
+        // if super was not passed change the parameters
+        if(arguments.length <= 2 || typeof klass != "function"){
+            statics = members;
+            members = klass;
+            klass = superKlass;
+            superKlass = null;
         }
-        else{
-            klass.prototype = prototypeOrKlass;
+
+        if(superKlass){
+            // if this is a constructor
+            if(typeof superKlass == "function"){
+                klass.superKlass = superKlass;
+
+                // clone prototype of the supper
+                for (var property in klass.superKlass.prototype) {
+                    klass.prototype[property] = klass.superKlass.prototype[property];
+                  }
+
+                  // add nice shortcut to call the super method(s)
+                  klass.prototype.super = function(){
+                      // remember latest constructor to follow the chain
+                      var superKlass = klass.superKlass;
+                       if(this.nextSuperKlass){
+                          superKlass = this.nextSuperKlass;
+                      }
+                      this.nextSuperKlass = superKlass.superKlass;
+                      superKlass.apply(this, arguments);
+                  }
+            }
+            // in case this is an object
+            else{
+                klass.prototype = superKlass;
+            }
         }
-        
+
         // create package structure
         for(i=0; i<names.length; i++){
             var varName = names[i];
@@ -73,19 +103,34 @@ MgnlRuntime = mgnl.Runtime = {
             }
             current = current[varName];
         }
-        
+
         // add the the class name
         if(klass.prototype)
-            klass.prototype._class = name; 
+            klass.prototype._class = name;
         else
             klass._class = name;
+
+        // add members (prototype)
+        if(members){
+            for (var property in members) {
+                   klass.prototype[property] = members[property];
+            }
+        }
+
+        // add statics
+        if(statics){
+            for (var property in statics) {
+                   klass[property] = statics[property];
+            }
+        }
+
         return klass;
     },
 
     /**
-     * Loads a script if needed and make the class avaiable under it's name. A 
+     * Loads a script if needed and make the class avaiable under it's name. A
      * shortcut with a Mgnl prefix is created. Set sync = true if you load
-     * a script after the page was loaded.<b> 
+     * a script after the page was loaded.<b>
      * Pest practise
      * <ul>
      *  <li>load scripts in the header
@@ -99,7 +144,7 @@ MgnlRuntime = mgnl.Runtime = {
 
         if(!this.existVariable(name)){
             if(this.loadingOn){
-                var url = name.replace(/\./g,"/");              
+                var url = name.replace(/\./g,"/");
                 // load the script and set the shortcut after the script is loaded
                 this.load(contextPath + "/admindocroot/js/classes/" + url + ".js", sync,
                     function(){
@@ -116,7 +161,7 @@ MgnlRuntime = mgnl.Runtime = {
             window["Mgnl" + className] = eval(name);
         }
     },
-    
+
     /**
      * Checks savely if this variable exists or not.
      */
@@ -133,7 +178,7 @@ MgnlRuntime = mgnl.Runtime = {
     },
 
     /**
-     * This method loads a script dynamically. Set sync only to true if you load 
+     * This method loads a script dynamically. Set sync only to true if you load
      * a script after the page was loaded. If you load scripts in the header you
      * and use them after the onload event is fired (in the onload method for example)
      * you can savely load in the faster async mode.
@@ -146,7 +191,7 @@ MgnlRuntime = mgnl.Runtime = {
         if(this.loaded[url] == null){
             // do not load twice, but is not yet loaded
             this.loaded[url]=false;
-           
+
             // if this is a synchronized call we must synchronize until this is finished
             var oldSynchronization;
             if(sync){
@@ -158,25 +203,25 @@ MgnlRuntime = mgnl.Runtime = {
             }
 
             this.debug("load " + url + "(snc: " + sync + ")");
-            
+
             // save this for the follwing function
             var myRuntime = this;
 
             // create the function called after the script is loaded
             var onLoad =  function(){
                 myRuntime.loaded[url] = true;
-                
+
                 // set to the original mode again
                 if(oldSynchronization != null){
                     myRuntime.loadSynchronized = oldSynchronization;
                 }
-                
+
                 // call callback function
                 if(callback){
                     callback();
                 }
             }
-           
+
            // in safari we must make a sync AJAX call
            // ie does not give a onload event if the page is already loaded
            // therfore we make a AJAX call if the page was loaded
@@ -194,11 +239,11 @@ MgnlRuntime = mgnl.Runtime = {
                 else{
                     xmlRequest = new XMLHttpRequest() ;
                 }
-        
+
                 // Load the script synchronously.
                 xmlRequest.open( "GET", url, false ) ;
                 xmlRequest.send( null ) ;
-                
+
                 // Evaluate the script.
                 if ( xmlRequest.status == 200 ){
                     try{
@@ -214,13 +259,13 @@ MgnlRuntime = mgnl.Runtime = {
                     alert( 'Error loading ' + url ) ;
                 }
            }
-           
+
            // async load
            else{
                var e = document.createElement("script");
                e.src = url;
                e.type="text/javascript";
-    
+
                e.onload = e.onreadystatechange = function (){
                     // Gecko doesn't have a "readyState" property
                     if ( !this.readyState || this.readyState == 'loaded' ){
@@ -231,12 +276,12 @@ MgnlRuntime = mgnl.Runtime = {
            }
        }
     },
-    
+
     /**
      * Cache for not yet printed messages
      */
     notYetDebuged: new Array(),
-    
+
     /**
      * Debug the message. Check if the debug class is already loaded. If not cache.
      */
@@ -254,11 +299,11 @@ MgnlRuntime = mgnl.Runtime = {
             notYetDebuged.append({msg: msg, o:o, level:level});
         }
     }
-    
+
 };
 
 // define shortcuts: the wrapping function is used to save the this variable
-classDef = function(name, prototypeOrKlass, klass){mgnl.Runtime.classDef(name, prototypeOrKlass, klass)};
+classDef = function(name, superKlass, klass, members, statics){mgnl.Runtime.classDef(name, superKlass, klass, members, statics)};
 importClass = function(name, sync){mgnl.Runtime.importClass(name,sync)};
 
 // define this class properly
