13abd2480ca0550c7b27213cb427ea353b03cde0
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2007-04-26 08:42:11 +0000

    MAGNOLIA-1185 - bypass interface - default implementation
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@9181 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/cms/filters/BypassImpl.java
@@ -0,0 +1,48 @@
+/**
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2006 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ */
+package info.magnolia.cms.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @author philipp
+ * @version $Id$
+ *
+ */
+public class BypassImpl implements Bypass{
+
+    /**
+     * Logger.
+     */
+    private static Logger log = LoggerFactory.getLogger(BypassImpl.class);
+
+    private String pattern;
+
+    public boolean bypass(String uri) {
+        if(pattern != null){
+            return uri.startsWith(pattern);
+        }
+        return false;
+    }
+
+
+    public String getPattern() {
+        return this.pattern;
+    }
+
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+}
