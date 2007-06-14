5e17e8ad7877d7ed588cf2cf56e5f2f6d02cdef1
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2007-06-14 08:34:59 +0000

    - make it a real util (no instantiation) - method to get the acitvation status icon
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@9711 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/cms/util/MetaDataUtil.java
+++ magnolia-core/src/main/java/info/magnolia/cms/util/MetaDataUtil.java
@@ -33,22 +33,48 @@ public class MetaDataUtil {
      */
     private static Logger log = LoggerFactory.getLogger(MetaDataUtil.class);
 
+    /**
+     * @deprecated this is a util
+     */
     private Content content;
 
+    /**
+     * @deprecated this is a util
+     */
     public MetaDataUtil(Content c) {
         this.setContent(c);
     }
 
+    /**
+     * @deprecated this is a util
+     */
     public void setContent(Content c) {
         this.content = c;
     }
 
+    /**
+     * @deprecated this is a util
+     */
     public Content getContent() {
         return this.content;
     }
 
+    /**
+     * @deprecated this is a util
+     */
     public String getPropertyValueString(String propertyName) {
-        return getPropertyValueString(propertyName, null);
+        return getPropertyValueString(content, propertyName, null);
+    }
+
+    public static String getPropertyValueString(Content content, String propertyName) {
+        return getPropertyValueString(content, propertyName, null);
+    }
+
+    /**
+     * @deprecated this is a util
+     */
+    public String getPropertyValueString(String propertyName, String dateFormat) {
+        return getPropertyValueString(content, propertyName, dateFormat);
     }
 
     /**
@@ -56,20 +82,18 @@ public class MetaDataUtil {
      * </p>
      * @return String
      */
-    public String getPropertyValueString(String propertyName, String dateFormat) {
+    public static String getPropertyValueString(Content content, String propertyName, String dateFormat) {
         try {
-            if (propertyName.equals(MetaData.CREATION_DATE)
-                || propertyName.equals(MetaData.LAST_MODIFIED)
-                || propertyName.equals(MetaData.LAST_ACTION)) {
-                Date date = this.getContent().getMetaData().getDateProperty(propertyName).getTime();
+            if (propertyName.equals(MetaData.CREATION_DATE) || propertyName.equals(MetaData.LAST_MODIFIED) || propertyName.equals(MetaData.LAST_ACTION)) {
+                Date date = content.getMetaData().getDateProperty(propertyName).getTime();
 
                 return DateUtil.format(date, dateFormat);
             }
             else if (propertyName.equals(MetaData.ACTIVATED)) {
-                return Boolean.toString(this.getContent().getMetaData().getBooleanProperty(propertyName));
+                return Boolean.toString(content.getMetaData().getBooleanProperty(propertyName));
             }
             else {
-                return this.getContent().getMetaData().getStringProperty(propertyName);
+                return content.getMetaData().getStringProperty(propertyName);
             }
         }
         catch (Exception e) {
@@ -79,4 +103,20 @@ public class MetaDataUtil {
         }
         return StringUtils.EMPTY;
     }
+
+    public static String getActivationStatusIcon(Content content) {
+        String imgSrc;
+        switch (content.getMetaData().getActivationStatus()) {
+            case MetaData.ACTIVATION_STATUS_MODIFIED :
+                imgSrc = "indicator_yellow.gif";
+                break;
+            case MetaData.ACTIVATION_STATUS_ACTIVATED :
+                imgSrc = "indicator_green.gif";
+                break;
+            default :
+                imgSrc = "indicator_red.gif";
+        }
+        return imgSrc;
+    }
+
 }
