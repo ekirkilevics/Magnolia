88b0f974737b6c2cb37e2b1ece4c9b65d75c7143
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2007-08-03 15:42:43 +0000

    merging updatemechanism ( -r 10038:HEAD http://svn.magnolia.info/svn/magnolia/branches/updatemechanism/trunk)
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@10337 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/module/ModuleManager.java
@@ -0,0 +1,130 @@
+/**
+ *
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2006 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
+ */
+package info.magnolia.module;
+
+import info.magnolia.cms.util.FactoryUtil;
+import info.magnolia.module.delta.DeltaType;
+import info.magnolia.module.model.ModuleDefinition;
+import info.magnolia.module.model.Version;
+
+import javax.jcr.RepositoryException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ModuleManager is responsible for the lifecycle of modules.
+ * (loads definitions, install/update/uninstall, start/stop)
+ *
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ */
+public interface ModuleManager {
+
+    /**
+     * Loads modules definitions, validates dependencies and sorts modules
+     * by dependencies.
+     */
+    List loadDefinitions() throws ModuleManagementException;
+
+    /**
+     * Checks if we need to do any module installation, update or uninstall.
+     */
+    ModuleManagementState checkForInstallOrUpdates() throws ModuleManagementException;
+
+    /**
+     * Returns the same status as checkForInstallOrUpdates(),
+     * @throws IllegalStateException if checkForInstallOrUpdates was never called.
+     */
+    ModuleManagementState getStatus();
+
+    void performInstallOrUpdate() throws RepositoryException;
+
+    InstallContext getInstallContext();
+
+    void startModules();
+
+    void stopModules();
+
+    /**
+     * Use this to retrieve the configured singleton impl of ModuleManager.
+     */
+    public class Factory {
+        public static ModuleManager getInstance() {
+            return (ModuleManager) FactoryUtil.getSingleton(ModuleManager.class);
+        }
+    }
+
+    /**
+     * Represent what's to be done for all modules.
+     */
+    public final static class ModuleManagementState {
+        private final List list;
+        private boolean done = true;
+
+        public ModuleManagementState() {
+            this.list = new ArrayList();
+        }
+
+        public boolean needsUpdateOrInstall() {
+            return !(list.isEmpty() && done);
+        }
+
+        void done() {
+            done = true;
+        }
+
+        void addModule(DeltaType type, ModuleDefinition module, Version currentVersion, List deltas) {
+            done = false;
+            list.add(new ModuleAndDeltas(type, module, currentVersion, deltas));
+        }
+
+        public List getList() {
+            return list;
+        }
+    }
+
+    /**
+     * Represents what's to be done for each module.
+     */
+    public final static class ModuleAndDeltas {
+        private final DeltaType type;
+        private final ModuleDefinition module;
+        private final Version currentVersion;
+        private final List deltas;
+
+        public ModuleAndDeltas(DeltaType type, ModuleDefinition module, Version currentVersion, List deltas) {
+            this.type = type;
+            this.module = module;
+            this.currentVersion = currentVersion;
+            this.deltas = deltas;
+        }
+
+        public DeltaType getType() {
+            return type;
+        }
+
+        public ModuleDefinition getModule() {
+            return module;
+        }
+
+        public Version getCurrentVersion() {
+            return currentVersion;
+        }
+
+        public List getDeltas() {
+            return deltas;
+        }
+    }
+
+
+}
