0a081a206d98777b3ce05e10adeca6395d73e3cd
Fabrizio Giustina <fgiust@openmindonline.it>
2007-08-06 08:24:35 +0000

    A base abstract class for filters that should not be executed more than once for each request (still not used in existing filters at the moment)
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@10347 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/cms/filters/OncePerRequestAbstractMagnoliaFilter.java
@@ -0,0 +1,40 @@
+package info.magnolia.cms.filters;
+
+import info.magnolia.context.Context;
+import info.magnolia.context.MgnlContext;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * A base abstract class for filters that should not be executed more than once for each request.
+ * @author Fabrizio Giustina
+ * @version $Revision: $ ($Author: $)
+ */
+public abstract class OncePerRequestAbstractMagnoliaFilter extends AbstractMagnoliaFilter {
+
+    private String requestKeyName = "__" + getClass() + "_FILTERED";
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+        ServletException {
+
+        request.setAttribute(requestKeyName, Boolean.TRUE);
+        doFilter((HttpServletRequest) request, (HttpServletResponse) response, chain);
+    }
+
+    public boolean isEnabled() {
+
+        if (!super.isEnabled()) {
+            return false;
+        }
+
+        return MgnlContext.getAttribute(requestKeyName, Context.LOCAL_SCOPE) == null;
+    }
+}
