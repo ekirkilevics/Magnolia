d4c2d8857cb6c97b79d950ccb2f36907980160c6
Vivian Steller <vivian@steller.info>
2007-09-06 15:10:04 +0000

    MGNLFORUM-46: added support for anonymous posts; fixed tests; updated forum config bootstrap file with default configuration parameters MAGNOLIA: added security utils class MGNLPUR: updated paragraph action to use new security utils class.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@11059 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/cms/security/SecurityUtil.java
@@ -0,0 +1,36 @@
+/**
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2006 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ */
+package info.magnolia.cms.security;
+
+import info.magnolia.context.MgnlContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author vsteller
+ * @version $Id$
+ */
+public class SecurityUtil {
+
+    /**
+     * Checks if the currently acting user is anonymous.
+     */
+    public static boolean isAnonymous() {
+        User user = MgnlContext.getUser();
+        return (user != null && user.getName().equals(UserManager.ANONYMOUS_USER));
+    }
+    
+    public static boolean isAuthenticated() {
+        User user = MgnlContext.getUser();
+        return (user != null && !user.getName().equals(UserManager.ANONYMOUS_USER));
+    }
+}
