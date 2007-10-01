2184750b5c2026702f581c90d582caef22f85400
Fabrizio Giustina <fgiust@openmindonline.it>
2007-10-01 20:51:07 +0000

    MAGNOLIA-540 looks like svn was confused by the duplicate renaming, recommitting the commons-fileupload implementation instead of the cos based one
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@11660 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/cms/filters/MultipartRequestFilter.java
+++ magnolia-core/src/main/java/info/magnolia/cms/filters/MultipartRequestFilter.java
@@ -15,78 +15,235 @@ package info.magnolia.cms.filters;
 import info.magnolia.cms.beans.runtime.MultipartForm;
 import info.magnolia.cms.core.Path;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadBase;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.servlet.ServletRequestContext;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
-
-import com.oreilly.servlet.MultipartRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
- * @author Sameer Charles
+ * A <code>Filter</code> that determines if a <code>HttpServletRequest</code> contains multipart content and if so
+ * parses it into a request attribute for further processing. This implementation uses jakarta commons-fileupload for
+ * parsing multipart requests. Maximum file size can be configured using the "maxFileSize" init parameter, defaulting to
+ * 2 GB.
+ * @author Andreas Brenk
+ * @author Fabrizio Giustina
  * @version $Id$
  */
 public class MultipartRequestFilter extends AbstractMagnoliaFilter {
 
     /**
-     * Max file upload size.
+     * Logger.
      */
-    private static final int MAX_FILE_SIZE = 2000000000; // 2GB
+    protected static Logger log = LoggerFactory.getLogger(CosMultipartRequestFilter.class);
 
-    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException{
+    /**
+     * Default max file upload size (2 GB).
+     */
+    private static final int DEFAULT_MAX_FILE_SIZE = 2000000000; // 2GB
 
-        String type = null;
-        String type1 = request.getHeader("Content-Type"); //$NON-NLS-1$
-        String type2 = request.getContentType();
-        if (type1 == null && type2 != null) {
-            type = type2;
+    /**
+     * Config parameter name for max file size.
+     */
+    private static final String PARAM_MAX_FILE_SIZE = "maxFileSize";
+
+    /**
+     * The maximum size a file upload may have.
+     */
+    private long maxFileSize = DEFAULT_MAX_FILE_SIZE;
+
+    /**
+     * The directory for temporary storage of uploaded files.
+     */
+    private File tempDir;
+
+    /**
+     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+     */
+
+    public void init(FilterConfig config) throws ServletException {
+        super.init(config);
+        String maxFileSize = config.getInitParameter(PARAM_MAX_FILE_SIZE);
+        if (maxFileSize != null) {
+            this.maxFileSize = Long.parseLong(maxFileSize);
         }
-        else if (type2 == null && type1 != null) {
-            type = type1;
+
+        this.tempDir = new File(Path.getTempDirectoryPath());
+    }
+
+    /**
+     * Determine if the request has multipart content and if so parse it into a <code>MultipartForm</code> and store
+     * it as a request attribute.
+     */
+
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+
+        boolean isMultipartContent = FileUploadBase.isMultipartContent(new ServletRequestContext(request));
+        if (isMultipartContent) {
+            try {
+                MultipartForm mpf = parseRequest(request);
+
+                // wrap the request
+                MultipartRequestWrapper mrw = new MultipartRequestWrapper(request, mpf);
+                chain.doFilter(mrw, response);
+                return;
+
+            }
+            catch (IOException e) {
+                throw e;
+            }
+            catch (Exception e) {
+                throw new ServletException(e);
+            }
         }
-        else if (type1 != null) {
-            type = (type1.length() > type2.length() ? type1 : type2);
-        }
-        if ((type != null) && type.toLowerCase().startsWith("multipart/form-data")) { //$NON-NLS-1$
-            parseParameters(request);
-        }
+
         chain.doFilter(request, response);
     }
 
     /**
-     * Adds all request paramaters as request attributes.
-     * @param request HttpServletRequest
+     * Parse the request and store it as a request attribute.
      */
-    private static void parseParameters(HttpServletRequest request) throws IOException {
+    private MultipartForm parseRequest(HttpServletRequest request) throws Exception {
         MultipartForm form = new MultipartForm();
-        String encoding = StringUtils.defaultString(request.getCharacterEncoding(), "UTF-8"); //$NON-NLS-1$
-        MultipartRequest multi = new MultipartRequest(
-            request,
-            Path.getTempDirectoryPath(),
-            MAX_FILE_SIZE,
-            encoding,
-            null);
-        Enumeration params = multi.getParameterNames();
-        while (params.hasMoreElements()) {
-            String name = (String) params.nextElement();
-            String value = multi.getParameter(name);
-            form.addParameter(name, value);
-            String[] s = multi.getParameterValues(name);
-            if (s != null) {
-                form.addparameterValues(name, s);
+
+        ServletFileUpload upload = newServletFileUpload();
+
+        List fileItems = upload.parseRequest(request);
+
+        for (Iterator fileItemIterator = fileItems.iterator(); fileItemIterator.hasNext();) {
+            FileItem item = (FileItem) fileItemIterator.next();
+            if (item.isFormField()) {
+                addField(request, item, form);
+            }
+            else {
+                addFile(item, form);
             }
         }
-        Enumeration files = multi.getFileNames();
-        while (files.hasMoreElements()) {
-            String name = (String) files.nextElement();
-            form.addDocument(name, multi.getFilesystemName(name), multi.getContentType(name), multi.getFile(name));
-        }
+
         request.setAttribute(MultipartForm.REQUEST_ATTRIBUTE_NAME, form);
+        return form;
+    }
+
+    /**
+     * Create a new <code>DiskFileUpload</code>.
+     */
+    private ServletFileUpload newServletFileUpload() {
+        ServletFileUpload upload = new ServletFileUpload();
+        upload.setSizeMax(this.maxFileSize);
+        DiskFileItemFactory fif = new DiskFileItemFactory();
+        fif.setRepository(Path.getTempDirectory());
+        upload.setFileItemFactory(fif);
+
+        return upload;
+    }
+
+    /**
+     * Add the <code>FileItem</code> as a paramter into the <code>MultipartForm</code>.
+     */
+    private void addField(HttpServletRequest request, FileItem item, MultipartForm form) {
+        String name = item.getFieldName();
+
+        String value;
+        try {
+            String encoding = StringUtils.defaultString(request.getCharacterEncoding(), "UTF-8");
+            value = item.getString(encoding);
+        }
+        catch (UnsupportedEncodingException ex) {
+            value = item.getString();
+        }
+
+        form.addParameter(name, value);
+
+        String[] values = form.getParameterValues(name);
+        if (values == null) {
+            form.addparameterValues(name, new String[]{value});
+        }
+        else {
+            form.addparameterValues(name, (String[]) ArrayUtils.add(values, value));
+        }
+    }
+
+    /**
+     * Add the <code>FileItem</code> as a document into the <code>MultipartForm</code>.
+     */
+    private void addFile(FileItem item, MultipartForm form) throws Exception {
+        String atomName = item.getFieldName();
+        String fileName = item.getName();
+        String type = item.getContentType();
+        File file = File.createTempFile(atomName, null, this.tempDir);
+        item.write(file);
+
+        form.addDocument(atomName, fileName, type, file);
+    }
+
+    static class MultipartRequestWrapper extends HttpServletRequestWrapper {
+
+        private MultipartForm form;
+
+        /**
+         * @param request
+         */
+        public MultipartRequestWrapper(HttpServletRequest request, MultipartForm form) {
+            super(request);
+            this.form = form;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+
+        public String getParameter(String name) {
+            String value = form.getParameter(name);
+            log.debug("getParameter: {}={}", name, value);
+            return value;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+
+        public Map getParameterMap() {
+            return form.getParameters();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+
+        public Enumeration getParameterNames() {
+            return form.getParameterNames();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+
+        public String[] getParameterValues(String name) {
+            String[] value = form.getParameterValues(name);
+            log.debug("getParameterValues: {}={}", name, value);
+            return value;
+        }
+
     }
 }
