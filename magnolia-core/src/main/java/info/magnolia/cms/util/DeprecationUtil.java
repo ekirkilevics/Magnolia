94923b06b6d86d9e46aa31b1cd45ba6c16c7370f
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2007-10-12 11:55:30 +0000

    MAGNOLIA-1778 :  Provide a utility class so deprecated classes can signal themselves easily
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@11842 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/cms/util/DeprecationUtil.java
@@ -0,0 +1,64 @@
+/**
+ *
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2006 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
+ */
+package info.magnolia.cms.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A silly utility class to help marking stuff as deprecated even if they're not used directly
+ * (taglib attributes for instance).
+ * Logs in std out, std err, the caller's logger, and this class' logger too.
+ *
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ */
+public class DeprecationUtil {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(DeprecationUtil.class);
+    private static final int MAX_STACKTRACE_ELEMENTS = 3;
+
+    public static void isDeprecated() {
+        internal_isDeprecated("No reason given");
+    }
+
+    public static void isDeprecated(String reason) {
+        internal_isDeprecated(reason);
+    }
+
+    private static void internal_isDeprecated(String reason) {
+        final StringBuffer out = new StringBuffer("A deprecated method was called: ");
+        out.append(reason);
+        out.append(". Check the following trace: ");
+        final Throwable fakeException = new Throwable();
+        final StackTraceElement[] elements = fakeException.getStackTrace();
+        StackTraceElement el = null;
+        // we start at 2 since we don't need the latest elements (ie the call to DeprecationUtil)
+        for (int i = 2; (i < elements.length && i <= MAX_STACKTRACE_ELEMENTS); i++) {
+            el = elements[i];
+            out.append(el);
+            out.append(", ");
+        }
+        out.append("the full stracktrace will be logged in debug mode in the ");
+        out.append(log.getName());
+        out.append(" category.");
+        final String outStr = out.toString();
+        if (el != null) {
+            final Logger theClassLog = LoggerFactory.getLogger(el.getClassName());
+            theClassLog.warn(outStr);
+        }
+        log.warn(outStr);
+        log.debug(outStr, fakeException);
+        System.err.println(outStr);
+        System.out.println(outStr);
+    }
+}
