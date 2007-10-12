f4a4506c3f142c306d8d46bf57a21ab1b8a2adc6
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2007-10-12 12:00:13 +0000

    SimpleFlowDefinitionManager is deprecated in 3.1, DefaultFlowDefinitionManager is part of CE now.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@11843 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-workflow/src/main/java/info/magnolia/module/workflow/flows/SimpleFlowDefinitionManager.java
+++ magnolia-module-workflow/src/main/java/info/magnolia/module/workflow/flows/SimpleFlowDefinitionManager.java
@@ -3,71 +3,37 @@
  */
 package info.magnolia.module.workflow.flows;
 
-import info.magnolia.cms.util.ClasspathResourcesUtil;
-import info.magnolia.module.workflow.WorkflowConstants;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
+import info.magnolia.cms.util.DeprecationUtil;
 import openwfe.org.engine.workitem.LaunchItem;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.util.List;
 
 /**
- * Used in the community edition. Only the default activation flow is supported.
+ *
  * @author philipp
  * @version $Id$
- * @deprecated this was used in the comunity edition to restrict to a single flow definition
+ * @deprecated This was used in the 3.0 community edition to restrict to a single flow definition. Since 3.1,
+ *             DefaultFlowDefinitionManager is part of CE.
  */
-public class SimpleFlowDefinitionManager implements FlowDefinitionManager {
-
-    /**
-     * Logger.
-     */
-    private static Logger log = LoggerFactory.getLogger(SimpleFlowDefinitionManager.class);
+public class SimpleFlowDefinitionManager extends DefaultFlowDefinitionManager {
 
     public void configure(LaunchItem launchItem, String workflowName) throws FlowDefinitionException {
-        if(!StringUtils.equals(workflowName, WorkflowConstants.DEFAULT_WORKFLOW)){
-            throw new FlowDefinitionException("only default workflow supported in Community Edition, will fallback to default flow");
-        }
-
-        launchItem.setWorkflowDefinitionUrl(WorkflowConstants.ATTRIBUTE_WORKFLOW_DEFINITION_URL);
-        String flowDef = readDefinition(WorkflowConstants.DEFAULT_WORKFLOW);
-        launchItem.getAttributes().puts(WorkflowConstants.ATTRIBUTE_DEFINITION, flowDef);
+        DeprecationUtil.isDeprecated("Use DefaultFlowDefinitionManager");
+        super.configure(launchItem, workflowName);
     }
 
     public String readDefinition(String workflowName) throws FlowDefinitionException {
-        if(!StringUtils.equals(workflowName, WorkflowConstants.DEFAULT_WORKFLOW)){
-            throw new FlowDefinitionException("only default workflow supported in Community Edition, will fallback to default flow");
-        }
-
-        InputStream stream = null;
-        try {
-            stream = ClasspathResourcesUtil.getStream("info/magnolia/module/workflow/default.xml");
-            return IOUtils.toString(stream);
-        }
-        catch (IOException e) {
-            log.error("can't read flow definition", e);
-        }
-        finally{
-            IOUtils.closeQuietly(stream);
-        }
-        return StringUtils.EMPTY;
+        DeprecationUtil.isDeprecated("Use DefaultFlowDefinitionManager");
+        return super.readDefinition(workflowName);
     }
 
     public void saveDefinition(String definition) throws FlowDefinitionException {
-        throw new FlowDefinitionException("saving of workflow definitions is not supported in the Community Edition");
+        DeprecationUtil.isDeprecated("Use DefaultFlowDefinitionManager");
+        super.saveDefinition(definition);
     }
 
     public List getDefinitionNames() {
-        List list = new ArrayList();
-        list.add("default");
-        return list;
+        DeprecationUtil.isDeprecated("Use DefaultFlowDefinitionManager");
+        return super.getDefinitionNames();
     }
 }
