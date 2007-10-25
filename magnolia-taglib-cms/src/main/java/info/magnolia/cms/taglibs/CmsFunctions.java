0dc4a50b55da7d8a3d6124e52ef73ff94f215ce9
Fabrizio Giustina <fgiust@openmindonline.it>
2007-10-25 14:18:59 +0000

    new isEditMode() utility function
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@12061 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-taglib-cms/src/main/java/info/magnolia/cms/taglibs/CmsFunctions.java
+++ magnolia-taglib-cms/src/main/java/info/magnolia/cms/taglibs/CmsFunctions.java
@@ -72,7 +72,7 @@ public class CmsFunctions {
      * Check if a user is currently logged in (not anonymous)
      * @return true if a user is currently logged in.
      */
-    public boolean isLoggedIn() {
+    public static boolean isLoggedIn() {
         return SecurityUtil.isAuthenticated();
     }
 
@@ -80,8 +80,21 @@ public class CmsFunctions {
      * Check if the current user can edit the active page
      * @return true if the current user can edit the active page.
      */
-    public boolean canEdit() {
+    public static boolean canEdit() {
         return Resource.getActivePage().isGranted(Permission.SET);
+    }
+
+    /**
+     * Check if the current page is open in editing mode. Shortcut for checking if the server is admin, preview unset,
+     * permissions to modify the page available for the current user.
+     * @return true if the page is open in edit mode and user has permissions to edit
+     */
+    public static boolean isEditMode() {
+        Content activePage = Resource.getActivePage();
+        return Server.isAdmin()
+            && !Resource.showPreview()
+            && activePage != null
+            && activePage.isGranted(Permission.SET);
     }
 
 }
