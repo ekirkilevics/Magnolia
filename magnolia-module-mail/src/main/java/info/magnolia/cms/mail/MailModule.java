6999f551637f7c79a4a0474d5550051a2d58ff81
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2007-10-28 19:11:06 +0000

    updated mail module (update mech / lifecycle)
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@12090 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-mail/src/main/java/info/magnolia/cms/mail/MailModule.java
+++ magnolia-module-mail/src/main/java/info/magnolia/cms/mail/MailModule.java
@@ -1,9 +1,8 @@
 package info.magnolia.cms.mail;
 
 import info.magnolia.cms.core.Content;
-import info.magnolia.cms.module.AbstractModule;
-import info.magnolia.cms.module.InitializationException;
-import info.magnolia.cms.module.InvalidConfigException;
+import info.magnolia.module.ModuleLifecycle;
+import info.magnolia.module.ModuleLifecycleContext;
 
 import javax.jcr.RepositoryException;
 
@@ -14,17 +13,24 @@ import org.slf4j.LoggerFactory;
 /**
  * @author <a href="mailto:niko@macnica.com">Nicolas Modrzyk</a>
  */
-public class MailModule extends AbstractModule {
+public class MailModule implements ModuleLifecycle {
     private static final Logger log = LoggerFactory.getLogger(MailModule.class);
 
     public static final String SERVER_MAIL_CONFIG = "smtp";
 
     public static final String MAIL_TEMPLATES_PATH = "templates";
 
-    /**
-     * Init cache manager
-     */
-    public void init(Content configNode) throws InvalidConfigException, InitializationException {
+    private Content configNode;
+
+    public Content getConfigNode() {
+        return configNode;
+    }
+
+    public void setConfigNode(Content configNode) {
+        this.configNode = configNode;
+    }
+
+    public void start(ModuleLifecycleContext moduleLifecycleContext) {
         try {
             Content smtpNode = configNode.getContent(SERVER_MAIL_CONFIG);
             Content templateNode = configNode.getContent(MAIL_TEMPLATES_PATH);
@@ -32,10 +38,11 @@ public class MailModule extends AbstractModule {
             MgnlMailFactory.getInstance().register(smtpNode);
             log.info("Loading mail templates");
             MgnlMailFactory.getInstance().register(templateNode);
-            this.setInitialized(true);
-        }
-        catch (RepositoryException e) {
+        } catch (RepositoryException e) {
             log.error("Missing configuration for mail. Module is not properly initialized");
         }
+    }
+
+    public void stop(ModuleLifecycleContext moduleLifecycleContext) {
     }
 }
---
+++ magnolia-module-mail/src/main/java/info/magnolia/cms/mail/setup/MailModuleVersionHandler.java
@@ -0,0 +1,35 @@
+/**
+ *
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2006 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
+ */
+package info.magnolia.cms.mail.setup;
+
+import info.magnolia.module.DefaultModuleVersionHandler;
+import info.magnolia.module.delta.BasicDelta;
+import info.magnolia.module.delta.WebXmlConditionsUtil;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ */
+public class MailModuleVersionHandler extends DefaultModuleVersionHandler {
+
+    public MailModuleVersionHandler() {
+        final ArrayList conditions = new ArrayList();
+        final WebXmlConditionsUtil u = new WebXmlConditionsUtil(conditions);
+        u.servletIsNowWrapped("Mail");
+
+        register("3.1", BasicDelta.createBasicDelta("Mail Module 3.1", "", new ArrayList(), conditions));
+    }
+}
