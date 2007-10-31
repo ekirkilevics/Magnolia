1f3659db4f6c509a744117c86d8ba9840d8432d0
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2007-10-31 15:55:30 +0000

    added header (yes, outdated, we still need to change ALL of them) and class javadoc
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@12126 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/setup/for3_1/MoveMagnoliaUsersToRealmFolder.java
+++ magnolia-core/src/main/java/info/magnolia/setup/for3_1/MoveMagnoliaUsersToRealmFolder.java
@@ -1,3 +1,15 @@
+/**
+ *
+ * Magnolia and its source-code is licensed under the LGPL.
+ * You may copy, adapt, and redistribute this file for commercial or non-commercial use.
+ * When copying, adapting, or redistributing this document in keeping with the guidelines above,
+ * you are required to provide proper attribution to obinary.
+ * If you reproduce or distribute the document without making any substantive modifications to its content,
+ * please use the following attribution line:
+ *
+ * Copyright 1993-2006 obinary Ltd. (http://www.obinary.com) All rights reserved.
+ *
+ */
 package info.magnolia.setup.for3_1;
 
 import info.magnolia.cms.beans.config.ContentRepository;
@@ -8,16 +20,19 @@ import info.magnolia.cms.security.Realm;
 import info.magnolia.module.InstallContext;
 import info.magnolia.module.delta.AbstractRepositoryTask;
 import info.magnolia.module.delta.TaskExecutionException;
-
-import java.util.Collection;
-import java.util.Iterator;
-
-import javax.jcr.RepositoryException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jcr.RepositoryException;
+import java.util.Collection;
+import java.util.Iterator;
 
+/**
+ * A task which moves existing users to the /admin realm folder.
+ *
+ * @author philipp
+ * @version $Revision: $ ($Author: $)
+ */
 public class MoveMagnoliaUsersToRealmFolder extends AbstractRepositoryTask {
     private static final Logger log = LoggerFactory.getLogger(MoveMagnoliaUsersToRealmFolder.class);
 
@@ -32,7 +47,7 @@ public class MoveMagnoliaUsersToRealmFolder extends AbstractRepositoryTask {
         Collection users = usersHm.getRoot().getChildren(ItemType.USER);
 
         Iterator iter = users.iterator();
-        while(iter.hasNext()) {
+        while (iter.hasNext()) {
             Content node = (Content) iter.next();
             usersHm.getWorkspace().getSession().move(node.getHandle(), getAdminRealmFolder() + "/" + node.getName());
             log.info("Moved user " + node.getName() + " to " + getAdminRealmFolder() + "/" + node.getName());
