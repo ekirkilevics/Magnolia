064e0128e451fb3a8fd104bc997df37beda18ab0
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2007-11-01 13:04:36 +0000

    Please avoid commiting codestyle changes along with actual changes; thanks.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@12151 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/module/delta/WebXmlConditionsUtil.java
+++ magnolia-core/src/main/java/info/magnolia/module/delta/WebXmlConditionsUtil.java
@@ -16,17 +16,15 @@ import info.magnolia.cms.util.WebXmlUtil;
 
 import java.util.List;
 
-
 /**
- * A utility class for web.xml related conditions, which will add conditions to a given list of tasks based on some
- * conditions.
+ * A utility class for web.xml related conditions, which will add
+ * conditions to a given list of tasks based on some conditions.
+ *
  * @author gjoseph
  * @version $Revision: $ ($Author: $)
  */
 public class WebXmlConditionsUtil {
-
     private final WebXmlUtil webXmlUtil = new WebXmlUtil();
-
     private final List conditions;
 
     public WebXmlConditionsUtil(List conditions) {
@@ -35,34 +33,19 @@ public class WebXmlConditionsUtil {
 
     public void servletIsNowWrapped(final String servletName) {
         if (webXmlUtil.isServletOrMappingRegistered(servletName)) {
-            conditions
-                .add(new FalseCondition(
-                    "web.xml updates",
-                    "Since Magnolia 3.1, servlets are wrapped in ServletDispatchingFilter; please remove the <servlet> and <servlet-mapping> elements for "
-                        + servletName
-                        + " from your web.xml file."));
+            conditions.add(new FalseCondition("web.xml updates", "Since Magnolia 3.1, servlets are wrapped in ServletDispatchingFilter; please remove the <servlet> and <servlet-mapping> elements for " + servletName + " from your web.xml file."));
         }
     }
 
     public void servletIsDeprecated(final String servletName) {
         if (webXmlUtil.isServletOrMappingRegistered(servletName)) {
-            conditions
-                .add(new FalseCondition(
-                    "web.xml updates",
-                    "The "
-                        + servletName
-                        + " servlet is deprecated and not in use; please remove the corresponding <servlet> and <servlet-mapping> elements from your web.xml file."));
+            conditions.add(new FalseCondition("web.xml updates", "The " + servletName + " servlet is deprecated and not in use; please remove the corresponding <servlet> and <servlet-mapping> elements from your web.xml file."));
         }
     }
 
     public void servletIsRemoved(final String servletName) {
         if (webXmlUtil.isServletOrMappingRegistered(servletName)) {
-            conditions
-                .add(new FalseCondition(
-                    "web.xml updates",
-                    "The "
-                        + servletName
-                        + " servlet does not exist anymore; please remove the corresponding <servlet> and <servlet-mapping> elements from your web.xml file."));
+            conditions.add(new FalseCondition("web.xml updates", "The " + servletName + " servlet does not exist anymore; please remove the corresponding <servlet> and <servlet-mapping> elements from your web.xml file."));
         }
     }
 
