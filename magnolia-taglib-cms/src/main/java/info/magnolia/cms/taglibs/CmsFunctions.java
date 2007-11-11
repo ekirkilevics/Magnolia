9de6e2d00daf49ca2fc80168557631d8da1de20a
Fabrizio Giustina <fgiust@openmindonline.it>
2007-11-11 18:34:16 +0000

    a couple of small and useful functions
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@12407 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-taglib-cms/src/main/java/info/magnolia/cms/taglibs/CmsFunctions.java
+++ magnolia-taglib-cms/src/main/java/info/magnolia/cms/taglibs/CmsFunctions.java
@@ -10,6 +10,11 @@ import info.magnolia.context.MgnlContext;
 
 import java.util.Properties;
 
+import javax.jcr.RepositoryException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 /**
  * Useful EL functions that can be used in jsp 2.0 pages.
@@ -17,6 +22,11 @@ import java.util.Properties;
  * @version $Revision: $ ($Author: $)
  */
 public class CmsFunctions {
+
+    /**
+     * Logger.
+     */
+    private static Logger log = LoggerFactory.getLogger(CmsFunctions.class);
 
     /**
      * Returns the current active page (can be set using the loadPage tag).
@@ -95,6 +105,80 @@ public class CmsFunctions {
             && !Resource.showPreview()
             && activePage != null
             && activePage.isGranted(Permission.SET);
+    }
+
+    /**
+     * Find and load the first parent page containing a named collection of nodes. This function can be useful while
+     * building pages that should inherit columns from parent pages. Loaded page must be unloaded using the
+     * <code>&lt;cms:unloadPage /></code> tag. Sample use:
+     *
+     * <pre>
+     * &lt;c:if test="${cmsfn:firstPageWithCollection("node", 3)}">
+     *      content inherited from page ${cmsfn:currentPage().handle}.html
+     *   &lt;cms:includeTemplate contentNodeName="node" />
+     *   &lt;cms:unloadPage />
+     * &lt;/c:if>
+     * </pre>
+     *
+     * @param collectionName paragraph collection name
+     * @param minlevel level at which we will stop also if no page is found
+     * @return <code>true</code> if a page has been found and loaded, <code>false</code> otherwise
+     */
+    public static boolean firstPageWithCollection(String collectionName, int minlevel) {
+        Content actpage = Resource.getCurrentActivePage();
+        try {
+            while (actpage.getLevel() > minlevel) {
+                actpage = actpage.getParent();
+
+                if (actpage.hasContent(collectionName) && actpage.getContent(collectionName).hasChildren()) {
+                    Resource.setCurrentActivePage(actpage);
+                    return true;
+                }
+            }
+        }
+        catch (RepositoryException e) {
+            log.error("Error looking for collection " + collectionName + " in " + actpage.getHandle(), e);
+        }
+
+        return false;
+    }
+
+    /**
+     * Find and load the first parent page containing a named node. This function can be useful while building pages
+     * that should inherit a paragraph from parent pages. Loaded page must be unloaded using the
+     * <code>&lt;cms:unloadPage /></code> tag. Sample use:
+     *
+     * <pre>
+     * &lt;c:if test="${cmsfn:firstPageWithNode("column", 3)}">
+     *      content inherited from page ${cmsfn:currentPage().handle}.html
+     *   &lt;cms:contentNodeIterator contentNodeCollectionName="column">
+     *     &lt;cms:includeTemplate />
+     *   &lt;/cms:contentNodeIterator>
+     *   &lt;cms:unloadPage />
+     * &lt;/c:if>
+     * </pre>
+     *
+     * @param nodeName paragraph name
+     * @param minlevel level at which we will stop also if no page is found
+     * @return <code>true</code> if a page has been found and loaded, <code>false</code> otherwise
+     */
+    public static boolean firstPageWithNode(String nodeName, int minlevel) {
+        Content actpage = Resource.getCurrentActivePage();
+        try {
+            while (actpage.getLevel() > minlevel) {
+                actpage = actpage.getParent();
+
+                if (actpage.hasContent(nodeName)) {
+                    Resource.setCurrentActivePage(actpage);
+                    return true;
+                }
+            }
+        }
+        catch (RepositoryException e) {
+            log.error("Error looking for node " + nodeName + " in " + actpage.getHandle(), e);
+        }
+
+        return false;
     }
 
 }
