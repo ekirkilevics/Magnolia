45de52e9b06b3d33c63fe4f8fa780b0f75287d55
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2007-11-12 19:03:18 +0000

    MAGNOLIA-1165 / MAGNOLIA-1833 : we now have different bundles for 1.4 and 1.5
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@12443 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-bundle/src/release/bundle.sh
+++
@@ -1,5 +0,0 @@
-# bootstrapping needs extra memory
-export MAVEN_OPTS=-Xmx512M
-
-# create the bundle and then the assemblies
-mvn clean package info.magnolia:maven-bundle-plugin:bundle assembly:assembly -Pbundle
