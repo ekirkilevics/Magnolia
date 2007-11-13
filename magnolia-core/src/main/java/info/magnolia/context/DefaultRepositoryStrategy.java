e242f17cb4f43e899831cb1e3efda59a88bcaa10
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2007-11-13 19:16:11 +0000

    codestyle - all this whitespace was probably created by a wrongly configured editor, or by copying a file between os with different lineendings ?
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@12475 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/context/DefaultRepositoryStrategy.java
+++ magnolia-core/src/main/java/info/magnolia/context/DefaultRepositoryStrategy.java
@@ -33,107 +33,51 @@
  */
 package info.magnolia.context;
 
-
-
-import java.util.*;
-
-
-
-import javax.security.auth.Subject;
-
-
-
-import org.apache.log4j.spi.DefaultRepositorySelector;
-
+import info.magnolia.cms.security.AccessManager;
+import info.magnolia.cms.util.WorkspaceAccessUtil;
 import org.slf4j.Logger;
-
 import org.slf4j.LoggerFactory;
 
-
-
-import info.magnolia.cms.security.AccessManager;
-
-import info.magnolia.cms.security.Authenticator;
-
-import info.magnolia.cms.util.WorkspaceAccessUtil;
-
+import javax.security.auth.Subject;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class DefaultRepositoryStrategy extends AbstractRepositoryStrategy {
+    static final Logger log = LoggerFactory.getLogger(DefaultRepositoryStrategy.class);
 
-	static final Logger log = LoggerFactory.getLogger(DefaultRepositoryStrategy.class);
-
-
-
-	private static final long serialVersionUID = 222L;
-
-	
-
-	private Map accessManagers = new HashMap();
-
-	
-
-    protected UserContext context;	
-
-	
-
-	public DefaultRepositoryStrategy(UserContext context) {
-
-		this.context = context;
-
-	}
-
-	
-
-	public AccessManager getAccessManager(String repositoryId,
-
-			String workspaceId) {
-
-		AccessManager accessManager = null;
+    private static final long serialVersionUID = 222L;
 
 
+    private Map accessManagers = new HashMap();
+    protected UserContext context;
+
+    public DefaultRepositoryStrategy(UserContext context) {
+        this.context = context;
+    }
+
+
+    public AccessManager getAccessManager(String repositoryId, String workspaceId) {
+        AccessManager accessManager = null;
 
         final String amAttrName = repositoryId + "_" + workspaceId;
-
         accessManager = (AccessManager) accessManagers.get(amAttrName);
 
-        
-
-
-
         if (accessManager == null) {
-
-            accessManager = WorkspaceAccessUtil.getInstance().createAccessManager(
-
-                getSubject(), repositoryId, workspaceId);
-
-            accessManagers.put(amAttrName, accessManager);            
-
+            accessManager = WorkspaceAccessUtil.getInstance().createAccessManager(getSubject(), repositoryId, workspaceId);
+            accessManagers.put(amAttrName, accessManager);
         }
 
-
-
         return accessManager;
-
-	}
-
-
-
-	protected Subject getSubject() {
-
-        return this.context.getUser().getSubject();
-
     }
 
-
+    protected Subject getSubject() {
+        return this.context.getUser().getSubject();
+    }
 
     protected String getUserId() {
-
         return this.context.getUser().getName();
-
     }
-
-
 
 }
 
