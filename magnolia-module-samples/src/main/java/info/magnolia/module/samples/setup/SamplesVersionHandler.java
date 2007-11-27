fb82d773563bd215915672d6baa4a693311f4c3c
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2007-11-27 08:56:32 +0000

    MAGNOLIA-1890: set default uri based on a property
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@13132 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-samples/src/main/java/info/magnolia/module/samples/setup/SamplesVersionHandler.java
+++ magnolia-module-samples/src/main/java/info/magnolia/module/samples/setup/SamplesVersionHandler.java
@@ -1,5 +1,5 @@
 /**
- * This file Copyright (c) 2003-2007 Magnolia International
+ * This file Copyright (c) 2007 Magnolia International
  * Ltd.  (http://www.magnolia.info). All rights reserved.
  *
  *
@@ -34,15 +34,13 @@
 package info.magnolia.module.samples.setup;
 
 import info.magnolia.cms.beans.config.ContentRepository;
-import info.magnolia.module.DefaultModuleVersionHandler;
 import info.magnolia.module.InstallContext;
 import info.magnolia.module.admininterface.setup.AddSubMenuItemTask;
+import info.magnolia.module.admininterface.setup.SimpleContentVersionHandler;
 import info.magnolia.module.delta.Delta;
 import info.magnolia.module.delta.DeltaBuilder;
-import info.magnolia.module.delta.IsAuthorInstanceDelegateTask;
 import info.magnolia.module.delta.NodeExistsDelegateTask;
 import info.magnolia.module.delta.RemoveNodeTask;
-import info.magnolia.module.delta.SetPropertyTask;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,14 +51,14 @@ import java.util.List;
  * @version $Id$
  *
  */
-public class SamplesVersionHandler extends DefaultModuleVersionHandler {
+public class SamplesVersionHandler extends SimpleContentVersionHandler {
     private final List installOrUpdateTasks = new ArrayList();
-    
+
     public SamplesVersionHandler() {
         // - replace Templates menu item
         final AddSubMenuItemTask addTemplateMenuItem = new AddSubMenuItemTask("config", "sample-templates", "config.menu.config.templates.samples", "info.magnolia.module.samples.messages", "MgnlAdminCentral.showTree('config','/modules/samples/templates')", "/.resources/icons/16/dot.gif", "subscribers");
         final String templatesMenuItemPath = "/modules/adminInterface/config/menu/config/templates";
-        final NodeExistsDelegateTask removeOldTemplatesMenuItem = new NodeExistsDelegateTask("Menu", "Remove the Templates menu item if existent", ContentRepository.CONFIG, templatesMenuItemPath, 
+        final NodeExistsDelegateTask removeOldTemplatesMenuItem = new NodeExistsDelegateTask("Menu", "Remove the Templates menu item if existent", ContentRepository.CONFIG, templatesMenuItemPath,
             new RemoveNodeTask("Menu", "Removes the Template menu item as it will be replaced with a new configuration", ContentRepository.CONFIG, templatesMenuItemPath));
         installOrUpdateTasks.add(removeOldTemplatesMenuItem);
         installOrUpdateTasks.add(addTemplateMenuItem);
@@ -68,33 +66,29 @@ public class SamplesVersionHandler extends DefaultModuleVersionHandler {
         // - replace Paragraphs menu item
         final AddSubMenuItemTask addParagraphsMenuItem = new AddSubMenuItemTask("config", "sample-paragraphs", "config.menu.config.paragraphs.samples", "info.magnolia.module.samples.messages", "MgnlAdminCentral.showTree('config','/modules/samples/paragraphs')", "/.resources/icons/16/dot.gif", "subscribers");
         final String paragraphsMenuItemPath = "/modules/adminInterface/config/menu/config/paragraphs";
-        final NodeExistsDelegateTask removeOldParagraphsMenuItem = new NodeExistsDelegateTask("Menu", "Remove the Paragraphs menu item if existent", ContentRepository.CONFIG, paragraphsMenuItemPath, 
+        final NodeExistsDelegateTask removeOldParagraphsMenuItem = new NodeExistsDelegateTask("Menu", "Remove the Paragraphs menu item if existent", ContentRepository.CONFIG, paragraphsMenuItemPath,
             new RemoveNodeTask("Menu", "Removes the Paragraph menu item as it will be replaced with a new configuration", ContentRepository.CONFIG, paragraphsMenuItemPath));
         installOrUpdateTasks.add(removeOldParagraphsMenuItem);
         installOrUpdateTasks.add(addParagraphsMenuItem);
-        
+
         // - replace Dialogs menu item
         final AddSubMenuItemTask addDialogsMenuItem = new AddSubMenuItemTask("config", "sample-dialogs", "config.menu.config.dialogs.samples", "info.magnolia.module.samples.messages", "MgnlAdminCentral.showTree('config','/modules/samples/dialogs')", "/.resources/icons/16/dot.gif", "subscribers");
         final String dialogsMenuItemPath = "/modules/adminInterface/config/menu/config/dialogs";
-        final NodeExistsDelegateTask removeOldDialogsMenuItem = new NodeExistsDelegateTask("Menu", "Remove the Dialogs menu item if existent", ContentRepository.CONFIG, dialogsMenuItemPath, 
+        final NodeExistsDelegateTask removeOldDialogsMenuItem = new NodeExistsDelegateTask("Menu", "Remove the Dialogs menu item if existent", ContentRepository.CONFIG, dialogsMenuItemPath,
             new RemoveNodeTask("Menu", "Removes the Paragraph menu item as it will be replaced with a new configuration", ContentRepository.CONFIG, dialogsMenuItemPath));
         installOrUpdateTasks.add(removeOldDialogsMenuItem);
         installOrUpdateTasks.add(addDialogsMenuItem);
-        
+
         final Delta for35 = DeltaBuilder.update("3.5", "").addTasks(installOrUpdateTasks);
 
         register(for35);
     }
 
     protected List getExtraInstallTasks(InstallContext installContext) {
-        SetPropertyTask setDefaultURI = new SetPropertyTask(
-            ContentRepository.CONFIG, 
-            "/modules/adminInterface/virtualURIMapping/default", 
-            "toURI", 
-            "redirect:/features.html");
-        installOrUpdateTasks.add(new IsAuthorInstanceDelegateTask("Default URI", "Sets the value to the default page redirect:/features.html", null, setDefaultURI));
-        
-        return installOrUpdateTasks;
+        final List installTasks = new ArrayList(installOrUpdateTasks);
+        // add the default uri task
+        installTasks.add(getSetDefaultPublicURITask(installContext));
+        return installTasks;
     }
 
 }
