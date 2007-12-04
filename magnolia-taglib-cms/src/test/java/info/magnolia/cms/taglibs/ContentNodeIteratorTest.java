f993184dbcf35b74eabc9018242b07302fe7ea13
Fabrizio Giustina <fgiust@openmindonline.it>
2007-12-04 22:23:48 +0000

    formatting, no changes
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@13382 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-taglib-cms/src/test/java/info/magnolia/cms/taglibs/ContentNodeIteratorTest.java
+++ magnolia-taglib-cms/src/test/java/info/magnolia/cms/taglibs/ContentNodeIteratorTest.java
@@ -33,31 +33,32 @@
  */
 package info.magnolia.cms.taglibs;
 
+import info.magnolia.cms.util.FactoryUtil;
+import info.magnolia.cms.util.Resource;
+import info.magnolia.context.MgnlContext;
+import info.magnolia.context.SystemContext;
+import info.magnolia.test.MgnlTestCase;
+import info.magnolia.test.mock.MockContent;
+import info.magnolia.test.mock.MockWebContext;
+
 import java.util.ArrayList;
 
 import javax.servlet.jsp.jstl.core.LoopTagStatus;
 import javax.servlet.jsp.tagext.IterationTag;
 import javax.servlet.jsp.tagext.Tag;
 
-import info.magnolia.cms.util.FactoryUtil;
-import info.magnolia.cms.util.Resource;
-import info.magnolia.context.MgnlContext;
-import info.magnolia.context.SystemContext;
-import info.magnolia.context.WebContext;
-import info.magnolia.test.MgnlTestCase;
-import info.magnolia.test.mock.MockContent;
-import info.magnolia.test.mock.MockContext;
-import info.magnolia.test.mock.MockWebContext;
-
 import com.mockrunner.mock.web.MockHttpServletRequest;
 import com.mockrunner.mock.web.MockHttpServletResponse;
 import com.mockrunner.mock.web.MockPageContext;
 import com.mockrunner.mock.web.MockServletConfig;
 
+
 public class ContentNodeIteratorTest extends MgnlTestCase {
 
     ContentNodeIterator cni;
+
     MockPageContext pc;
+
     ArrayList items;
 
     protected void setUp() throws Exception {
@@ -66,7 +67,8 @@ public class ContentNodeIteratorTest extends MgnlTestCase {
         final MockWebContext ctx = new MockWebContext();
         MgnlContext.setInstance(ctx);
         // and system context as well
-        FactoryUtil.setInstanceFactory(SystemContext.class, new FactoryUtil.InstanceFactory(){
+        FactoryUtil.setInstanceFactory(SystemContext.class, new FactoryUtil.InstanceFactory() {
+
             public Object newInstance() {
                 return ctx;
             }
@@ -75,11 +77,10 @@ public class ContentNodeIteratorTest extends MgnlTestCase {
         // init tested tag
         cni = new ContentNodeIterator();
         MockHttpServletRequest req = new MockHttpServletRequest();
-        pc = new MockPageContext(new MockServletConfig(),
-                req, new MockHttpServletResponse());
+        pc = new MockPageContext(new MockServletConfig(), req, new MockHttpServletResponse());
         cni.setPageContext(pc);
         cni.setVarStatus("testStatus");
-//        cni.setContentNodeCollectionName(colName);
+        // cni.setContentNodeCollectionName(colName);
         items = new ArrayList();
         for (int i = 0; i < 10; i++) {
             items.add(new MockContent("mc" + i));
@@ -96,7 +97,7 @@ public class ContentNodeIteratorTest extends MgnlTestCase {
         assertEquals("Zero based index", 0, lts.getIndex());
 
         int count = 1;
-        while (count < items.size() ) {
+        while (count < items.size()) {
             assertEquals(IterationTag.EVAL_BODY_AGAIN, cni.doAfterBody());
             assertEquals(items.get(count), Resource.getLocalContentNode());
             count++;
@@ -116,7 +117,7 @@ public class ContentNodeIteratorTest extends MgnlTestCase {
         assertEquals("Zero based index", begin, lts.getIndex());
 
         int count = begin + 1;
-        while (count < items.size() ) {
+        while (count < items.size()) {
             assertEquals(IterationTag.EVAL_BODY_AGAIN, cni.doAfterBody());
             assertEquals(items.get(count), Resource.getLocalContentNode());
             count++;
@@ -135,8 +136,8 @@ public class ContentNodeIteratorTest extends MgnlTestCase {
         assertEquals("Count has to be 1 after call to doStartTag", 1, lts.getCount());
         assertEquals("Zero based index", 0, lts.getIndex());
 
-        int count =  1;
-        while (count < end + 1 ) {
+        int count = 1;
+        while (count < end + 1) {
             assertEquals(IterationTag.EVAL_BODY_AGAIN, cni.doAfterBody());
             assertEquals(items.get(count), Resource.getLocalContentNode());
             count++;
