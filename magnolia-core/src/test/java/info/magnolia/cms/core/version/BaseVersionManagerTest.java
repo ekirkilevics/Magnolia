f8bea3ad22461931f9072fc3ccafdac2aa849a13
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2008-04-14 14:02:54 +0000

    MAGNOLIA-2086: minimal testing
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@14930 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/test/java/info/magnolia/cms/core/version/BaseVersionManagerTest.java
@@ -0,0 +1,87 @@
+/**
+ * This file Copyright (c) 2008 Magnolia International
+ * Ltd.  (http://www.magnolia.info). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia.info/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.cms.core.version;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.version.Version;
+
+import info.magnolia.cms.beans.config.ContentRepository;
+import info.magnolia.cms.core.Content;
+import info.magnolia.cms.core.HierarchyManager;
+import info.magnolia.cms.core.ItemType;
+import info.magnolia.context.MgnlContext;
+import info.magnolia.test.RepositoryTestCase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @author philipp
+ * @version $Id$
+ *
+ */
+public class BaseVersionManagerTest extends RepositoryTestCase {
+
+    /**
+     * Logger.
+     */
+    private static Logger log = LoggerFactory.getLogger(BaseVersionManagerTest.class);
+
+    public void testCreateAndRestoreVersion() throws RepositoryException{
+        HierarchyManager hm = MgnlContext.getHierarchyManager(ContentRepository.WEBSITE);
+        Content node = hm.createContent("/", "page", ItemType.CONTENT.getSystemName());
+        node.createContent("paragraph", ItemType.CONTENTNODE.getSystemName());
+        hm.save();
+        Version version = node.addVersion();
+        assertFalse("Original node should not have mixin", node.isNodeType(ItemType.MIX_VERSIONABLE));
+
+        Content nodeInVersionWS = VersionManager.getInstance().getVersionedNode(node);
+        assertTrue("Node in mgnlVersion workspace must have mixin", nodeInVersionWS.isNodeType(ItemType.MIX_VERSIONABLE));
+
+        // assert that the the paragraph was versioned
+        Content versionedNode = node.getVersionedContent(version.getName());
+        assertTrue("Versioned content must include the paragraph", versionedNode.hasContent("paragraph"));
+
+        // now delete the paragraph
+        node.delete("paragraph");
+        node.save();
+        assertFalse("Paragraph should be deleted", node.hasContent("paragraph"));
+
+        // restore
+        node.restore(version.getName(), true);
+        assertTrue("Paragraph should be restored", node.hasContent("paragraph"));
+    }
+
+}
