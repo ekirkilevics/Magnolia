d4e45d94258cbcd2ea0b9166a4d096a2b932c429
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2008-05-06 17:21:34 +0000

    reverting r15234 for this file, had not intended to commit
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@15267 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/cms/filters/RenderingFilter.java
+++ magnolia-core/src/main/java/info/magnolia/cms/filters/RenderingFilter.java
@@ -81,7 +81,6 @@ public class RenderingFilter extends AbstractMgnlFilter {
      * @throws ServletException
      */
     public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException{
-        // TODO : this is completely useless ! cleanuppppp !!!
         if (ConfigLoader.isBootstrapping()) {
             // @todo a nice page, with the log content...
             response.getWriter().write("Magnolia bootstrapping has failed, check bootstrap.log in magnolia/logs"); //$NON-NLS-1$
@@ -103,7 +102,6 @@ public class RenderingFilter extends AbstractMgnlFilter {
                 // response.setStatus(HttpServletResponse.SC_OK);
                 renderer.renderTemplate(template, request, response);
 
-                /* TODO : why was this needed ?
                 try {
                     response.flushBuffer();
                 }
@@ -114,7 +112,6 @@ public class RenderingFilter extends AbstractMgnlFilter {
                         log.debug("Exception flushing response " + e.getClass().getName() + ": " + e.getMessage(), e); //$NON-NLS-1$ //$NON-NLS-2$
                     }
                 }
-                */
 
             }
             catch (IOException e) {
