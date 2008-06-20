5a5f0ddafce873523e2a6581c041e4cabc29109c
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2008-06-20 09:41:32 +0000

    a command which just logs (used for development)
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@16047 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/commands/impl/LogCommand.java
@@ -0,0 +1,84 @@
+/**
+ * This file Copyright (c) 2008 Magnolia International
+ * Ltd.  (http://www.magnolia.info). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia.info/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.commands.impl;
+
+import java.util.Date;
+
+import info.magnolia.commands.MgnlCommand;
+import info.magnolia.context.Context;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @author pbracher
+ * @version $Id$
+ */
+public class LogCommand extends MgnlCommand {
+
+    private String msg;
+
+    private String log = LogCommand.class.getName();
+
+    public boolean execute(Context ctx) throws Exception {
+        getLogger().info(getMsg());
+        return true;
+    }
+
+    public String getMsg() {
+        if(StringUtils.isEmpty(msg)){
+            return new Date().toString();
+        }
+        return this.msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getLog() {
+        return this.log;
+    }
+
+    public void setLog(String log) {
+        this.log = log;
+    }
+
+    protected Logger getLogger() {
+        return LoggerFactory.getLogger(getLog());
+    }
+
+}
