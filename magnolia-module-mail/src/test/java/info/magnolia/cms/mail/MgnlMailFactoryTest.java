190e5dd5e308ce74615082c28ba5ae130987d556
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2008-06-27 11:11:21 +0000

    moved javadoc after imports..
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@16287 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-mail/src/test/java/info/magnolia/cms/mail/MgnlMailFactoryTest.java
+++ magnolia-module-mail/src/test/java/info/magnolia/cms/mail/MgnlMailFactoryTest.java
@@ -33,19 +33,18 @@
  */
 package info.magnolia.cms.mail;
 
+
+import info.magnolia.cms.mail.templates.MgnlEmail;
+import org.subethamail.wiser.WiserMessage;
+
+import java.util.Iterator;
+
 /**
  *
  * Date: Mar 31, 2006
  * Time: 9:14:00 PM
  * @author <a href="mailto:niko@macnica.com">Nicolas Modrzyk</a>
  */
-
-import info.magnolia.cms.mail.templates.MgnlEmail;
-
-import java.util.Iterator;
-
-import org.subethamail.wiser.WiserMessage;
-
 public class MgnlMailFactoryTest extends AbstractMailTest {
 
     public void testSimpleMail() throws Exception {
