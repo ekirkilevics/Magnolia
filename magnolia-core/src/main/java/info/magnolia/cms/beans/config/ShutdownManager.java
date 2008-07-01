91ec9faf10193d221b21abe389af87dd7c63fbae
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2008-07-01 19:57:17 +0000

    clear core tasks as well
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@16410 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/cms/beans/config/ShutdownManager.java
+++ magnolia-core/src/main/java/info/magnolia/cms/beans/config/ShutdownManager.java
@@ -107,7 +107,7 @@ public class ShutdownManager extends ObservedManager implements ServletContextLi
      */
     public void contextInitialized(ServletContextEvent sce) {
         log.warn("\n***********\nThe use of ShutdownManager as ServletContextListener is deprecated in Magnolia 3.5, "
-            + "please update your web.xml and remove the listener but add a single listener with class name " 
+            + "please update your web.xml and remove the listener but add a single listener with class name "
             + "info.magnolia.cms.servlets.MgnlServletContextListener\n***********");
 
         // nothing to do
@@ -151,6 +151,11 @@ public class ShutdownManager extends ObservedManager implements ServletContextLi
             customTasks.add(0, mrc.getCommand((String) element)); // Last Registered First Executed
         }
 
+    }
+
+    public void clear() {
+        super.clear();
+        coreTasks.clear();
     }
 
     protected void onClear() {
