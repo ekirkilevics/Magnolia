c256bb37a77c2f65acf308aa1a7c5cfa3da8abd2
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2008-07-07 09:31:30 +0000

    renamed - fixing typo in classname
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/magnolia/trunk@16598 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/setup/CoreModuleVersionHandler.java
+++ magnolia-core/src/main/java/info/magnolia/setup/CoreModuleVersionHandler.java
@@ -35,14 +35,12 @@ package info.magnolia.setup;
 
 import info.magnolia.module.AbstractModuleVersionHandler;
 import info.magnolia.module.InstallContext;
-import info.magnolia.module.delta.ConditionalDelegateTask;
 import info.magnolia.module.delta.Delta;
 import info.magnolia.module.delta.DeltaBuilder;
-import info.magnolia.module.delta.WarnTask;
 import info.magnolia.module.delta.WebXmlConditionsUtil;
 import info.magnolia.module.delta.WorkspaceXmlConditionsUtil;
 import info.magnolia.setup.for3_5.GenericTasks;
-import info.magnolia.setup.for3_6.CheckMagoliaDevelopProperty;
+import info.magnolia.setup.for3_6.CheckMagnoliaDevelopProperty;
 import info.magnolia.setup.for3_6.CheckNodesForMixVersionable;
 import info.magnolia.setup.for3_6.CheckNodeTypesDefinition;
 
@@ -68,7 +66,7 @@ public class CoreModuleVersionHandler extends AbstractModuleVersionHandler {
         register(delta35);
         final Delta delta36 = DeltaBuilder.update("3.6", "")
             .addCondition(new CheckNodeTypesDefinition())
-            .addCondition(new CheckMagoliaDevelopProperty())
+            .addCondition(new CheckMagnoliaDevelopProperty())
             .addTask(new CheckNodesForMixVersionable());
         register(delta36);
     }
