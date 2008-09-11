44af0b502cdf8f0932208e46f22615ba8bc526d2
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2008-09-11 20:39:30 +0000

    MAGNOLIA-2115 : follow spec for mappings of servlets
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@17983 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/cms/filters/ServletDispatchingFilter.java
+++ magnolia-core/src/main/java/info/magnolia/cms/filters/ServletDispatchingFilter.java
@@ -51,6 +51,7 @@ import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
@@ -62,13 +63,15 @@ import org.slf4j.LoggerFactory;
 
 /**
  * A filter that dispatches requests to wrapped servlets.
- * 
+ *
  * @author vsteller
  * @version $Id$
  */
 public class ServletDispatchingFilter extends AbstractMgnlFilter {
 
     private static final Logger log = LoggerFactory.getLogger(ServletDispatchingFilter.class);
+
+    private static final String METACHARACTERS = "([\\^\\(\\)\\{\\}\\[\\]*$+])";
 
     private String servletName;
 
@@ -112,6 +115,18 @@ public class ServletDispatchingFilter extends AbstractMgnlFilter {
         return determineMatchingEnd(uri) < 0 || super.bypasses(request);
     }
 
+    protected Matcher findMatcher(String uri) {
+        for (Iterator iter = mappings.iterator(); iter.hasNext();) {
+            final Matcher matcher = ((Pattern) iter.next()).matcher(uri);
+
+            if (matcher.find()) {
+                return matcher;
+            }
+        }
+
+        return null;
+    }
+
     /**
      * Determines the index of the first pathInfo character. If the uri does not match any mapping this method returns
      * -1.
@@ -137,19 +152,12 @@ public class ServletDispatchingFilter extends AbstractMgnlFilter {
      * pathInfo.
      */
     public void doFilter(final HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-        throws IOException, ServletException {
+            throws IOException, ServletException {
 
         log.debug("Dispatching to servlet " + getServletClass());
-        servlet.service(new HttpServletRequestWrapper(request) {
-
-            public String getPathInfo() {
-                final String uri = StringUtils.substringAfter(request.getRequestURI(), request.getContextPath());
-                final String pathInfo = StringUtils.substring(uri, determineMatchingEnd(uri));
-
-                // according to the servlet spec the pathInfo should contain a leading slash
-                return (pathInfo.startsWith("/") ? pathInfo : "/" + pathInfo);
-            }
-        }, response);
+        final String uri = StringUtils.substringAfter(request.getRequestURI(), request.getContextPath());
+        final Matcher matcher = findMatcher(uri);
+        servlet.service(new WrappedRequest(request, matcher), response);
     }
 
     public String getServletName() {
@@ -176,22 +184,55 @@ public class ServletDispatchingFilter extends AbstractMgnlFilter {
         this.mappings = mappings;
     }
 
-    public void addMapping(String mapping) {
-        if (isPathMapping(mapping)) {
-            mapping = "^" + StringUtils.removeEnd(mapping, "*");
-        } else if (isExtensionMapping(mapping)) {
-            // TODO
-            throw new UnsupportedOperationException("Extension mappings are currently not supported");
-        } else if (isDefaultMapping(mapping)) {
-            mapping = "^" + mapping + "*";
-        } else {
-            mapping = "^" + mapping + "$";
-        }
-        final String encodedString = SimpleUrlPattern.getEncodedString(mapping);
+    /**
+     * See SRV.11.2 Specification of Mappings in the Servlet Specification
+     * for the syntax of mappings. Additionally, you can also use plain regular
+     * expressions to map your servlets, by prefix the mapping by "regex:". (in
+     * which case anything in the request url following the expression's match
+     * will be the pathInfo - if your pattern ends with a $, extra pathInfo won't
+     * match)
+     */
+    public void addMapping(final String mapping) {
+        final String pattern;
 
-        mappings.add(Pattern.compile(encodedString));
+        // we're building a Pattern with 3 groups: (1) servletPath (2) ignored (3) pathInfo
+
+        if (isDefaultMapping(mapping)) {
+            // the mapping is exactly '/*', the servlet path should be
+            // an empty string and everything else should be the path info
+            pattern = "^()(/)(" + SimpleUrlPattern.MULTIPLE_CHAR_PATTERN + ")";
+        } else if (isPathMapping(mapping)) {
+            // the pattern ends with /*, escape out metacharacters for
+            // use in a regex, and replace the ending * with MULTIPLE_CHAR_PATTERN
+            final String mappingWithoutSuffix = StringUtils.removeEnd(mapping, "/*");
+            pattern = "^(" + escapeMetaCharacters(mappingWithoutSuffix) + ")(/)(" + SimpleUrlPattern.MULTIPLE_CHAR_PATTERN + ")";
+        } else if (isExtensionMapping(mapping)) {
+            // something like '*.jsp', everything should be the servlet path
+            // and the path info should be null
+            final String regexedMapping = StringUtils.replace(mapping, "*.", SimpleUrlPattern.MULTIPLE_CHAR_PATTERN + "\\.");
+            pattern = "^(" + regexedMapping + ")$";
+        } else if (isRegexpMapping(mapping)) {
+            final String mappingWithoutPrefix = StringUtils.removeStart(mapping, "regex:");
+            pattern = "^(" + mappingWithoutPrefix + ")($|/)(" + SimpleUrlPattern.MULTIPLE_CHAR_PATTERN + ")";
+        } else {
+            // just literal text, ensure metacharacters are escaped, and that only
+            // the exact string is matched.
+            pattern = "^(" + escapeMetaCharacters(mapping) + ")$";
+        }
+        log.debug("Adding new mapping for {}", mapping);
+
+        mappings.add(Pattern.compile(pattern));
     }
 
+    static String escapeMetaCharacters(String str) {
+        return str.replaceAll(METACHARACTERS, "\\\\$1");
+    }
+
+    /**
+     * This is order specific, this method should not be called until
+     * after the isDefaultMapping() method else it will return true
+     * for a default mapping.
+     */
     private boolean isPathMapping(String mapping) {
         return mapping.startsWith("/") && mapping.endsWith("/*");
     }
@@ -199,9 +240,14 @@ public class ServletDispatchingFilter extends AbstractMgnlFilter {
     private boolean isExtensionMapping(String mapping) {
         return mapping.startsWith("*.");
     }
-    
+
     private boolean isDefaultMapping(String mapping) {
+        // TODO : default mapping per spec is "/" - do we really want to support this? is there a point ?
         return mapping.equals("/");
+    }
+
+    private boolean isRegexpMapping(String mapping) {
+        return mapping.startsWith("regex:");
     }
 
     public Map getParameters() {
@@ -259,6 +305,56 @@ public class ServletDispatchingFilter extends AbstractMgnlFilter {
 
         public String getServletName() {
             return servletName;
+        }
+
+    }
+
+    private class WrappedRequest extends HttpServletRequestWrapper {
+
+        private Matcher matcher;
+
+        /**
+         * This is set to true when the original request object passed
+         * in is changed by the setRequest() method.  This can indicate
+         * that a forward occurred and the values pulled from the
+         * matcher should no longer be used.
+         */
+        private boolean requestReplaced = false;
+
+        /**
+         * The given Matcher should be built from a Pattern containing two groups:
+         * (1) servletPath (2) ignored (3) pathInfo
+         */
+        public WrappedRequest(HttpServletRequest request, Matcher matcher) {
+            super(request);
+            this.matcher = matcher;
+        }
+
+        public String getPathInfo() {
+            if (requestReplaced) {
+                return super.getPathInfo();
+            }
+            if (matcher.groupCount() > 2) {
+                String pathInfo = matcher.group(3);
+                if (pathInfo.equals("")) {
+                    return null;
+                }
+                // according to the servlet spec the pathInfo should contain a leading slash
+                return (pathInfo.startsWith("/") ? pathInfo : "/" + pathInfo);
+            }
+            return null;
+        }
+
+        public String getServletPath() {
+            if (requestReplaced) {
+                return super.getServletPath();
+            }
+            return matcher.group(1);
+        }
+
+        public void setRequest(ServletRequest request) {
+            requestReplaced = true;
+            super.setRequest(request);
         }
 
     }
--- magnolia-core/src/test/java/info/magnolia/cms/filters/ServletDispatchingFilterTest.java
+++ magnolia-core/src/test/java/info/magnolia/cms/filters/ServletDispatchingFilterTest.java
@@ -48,58 +48,113 @@ import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Field;
 
 /**
- *
  * @author gjoseph
  * @version $Revision: $ ($Author: $)
  */
 public class ServletDispatchingFilterTest extends TestCase {
-    public void testShouldNotBypassWhenDefaultMappingMatches() throws Exception {
-        doTestBypassAndPathInfo(false, null, "/dms/some-doc.pdf", "/");
+
+    public void testEscapeMetaCharacters() {
+        assertEquals("a\\{b\\)c\\(d\\}e\\^f\\]g\\[h\\*i\\$j\\+kl",
+                ServletDispatchingFilter.escapeMetaCharacters("a{b)c(d}e^f]g[h*i$j+kl"));
     }
-    
+
+    public void testSupportsDefaultMapping() throws Exception {
+        shouldMatch("/dms/some-doc.pdf", "", "/dms/some-doc.pdf", "/");
+        shouldMatch(null, "", "/", "/");
+    }
+
+    public void testSupportsPathMapping() throws Exception {
+        shouldMatch("/hey/ho/lets/go", "/ramones", "/ramones/hey/ho/lets/go", "/ramones/*");
+        shouldMatch(null, "/ramones", "/ramones/", "/ramones/*");
+        shouldMatch("/wesh", "/yo", "/yo/wesh", "/yo/*");
+
+        // path mapping end with /*, and as such /my/path should not match for /my/path/*
+        shouldBypass("/ramones", "/ramones/*");
+    }
+
+    public void testSupportsExtensionMapping() throws Exception {
+        shouldMatch(null, "/dms/some-doc.pdf", "/dms/some-doc.pdf", "*.pdf");
+    }
+
+    public void testSupportsRegexMappings() throws Exception {
+        // the part of the request following the pattern match is the path info:
+        shouldMatch("/123/456", "/test", "/test/123/456", "regex:/[a-z]{4}");
+
+        // if pattern ends with a $, extra path info in the request uri does not match:
+        shouldMatch(null, "/test", "/test", "regex:^/[a-z]{4}$");
+        shouldBypass("/test/blah", "regex:^/[a-z]{4}$");
+        shouldBypass("/testx", "regex:^/[a-z]{4}$");
+
+        shouldMatch(null, "/foo/bar473", "/foo/bar473", "regex:^/foo/bar[0-9]{3}[A-Za-z]*");
+        shouldMatch(null, "/foo/bar473wEsh", "/foo/bar473wEsh", "regex:^/foo/bar[0-9]{3}[A-Za-z]*");
+
+        shouldMatch("/wEsh", "/foo/bar473", "/foo/bar473/wEsh", "regex:^/foo/bar[0-9]{3}");
+
+        //shouldMatch("/wEsh", "/foo/bar473", "/foo/bar473wEsh", "regex:^/foo/bar[0-9]{3}");
+        shouldBypass("/foo/bar473wEsh", "regex:^/foo/bar[0-9]{3}");
+
+        // to avoid the previous case:
+        shouldMatch(null, "/foo/bar473", "/foo/bar473", "regex:^/foo/bar[0-9]{3}");
+        shouldMatch("/wEsh", "/foo/bar473", "/foo/bar473/wEsh", "regex:^/foo/bar[0-9]{3}");
+    }
+
     public void testShouldNotBypassWhenPathMappingMatches() throws Exception {
-        doTestBypassAndPathInfo(false, "/some-doc.pdf", "/dms/some-doc.pdf", "/dms/*");
-        doTestBypassAndPathInfo(false, "/.html", "/nonSpecConformantMapping.html", "/nonSpecConformantMapping*");
+        shouldMatch("/some-doc.pdf", "/$d}^(m)+{s*", "/$d}^(m)+{s*/some-doc.pdf", "/$d}^(m)+{s*/*");
     }
 
     public void testShouldNotBypassWhenExactMappingMatches() throws Exception {
-        doTestBypassAndPathInfo(false, null, "/exactMatch", "/exactMatch");
+        shouldMatch(null, "/exactMatch", "/exactMatch", "/exactMatch");
+        shouldMatch(null, "/somepath*", "/somepath*", "/somepath*");
     }
-    
+
     public void testShouldBypassWhenMappingDoesNotMatch() throws Exception {
-        doTestBypassAndPathInfo(true, null, "/bleh/foo.bar", "/dms/*");
-        doTestBypassAndPathInfo(true, null, "/exactMatch/rightPathInfo", "/exactMatch");
+        shouldBypass("/bleh/foo.bar", "/dms/*");
+        shouldBypass("/exactMatch/rightPathInfo", "/exactMatch");
+        shouldBypass("/nonSpecConformantMapping.html", "/nonSpecConformantMapping*");
+        shouldBypass("/nonSpecConformantMapping*/somePage.html", "/nonSpecConformantMapping*");
     }
 
     public void testShouldBypassWhenMappingDoesNotMatchMAGNOLIA1984() throws Exception {
-        doTestBypassAndPathInfo(true, null, "/modules/dms/managingdocs.html", "/dms/*");
+        shouldBypass("/modules/dms/managingdocs.html", "/dms/*");
     }
 
-    /*
-    public void testPathInfoShouldStateWhateverIsAfterTheMapping() throws Exception {
-        // extension mappings not supported
-        doTestBypassAndPathInfo(false, "/test", "/some-doc.pdf/test", "*.pdf");
+    public void testPathInfoShouldAdhereToServletSpec() throws Exception {
+        shouldMatch("/pathInfo.html", "/servlet", "/servlet/pathInfo.html", "/servlet/*");
+        // The following doesn't work - spec isn't clear as to whether it should or not
+        //shouldMatch("/test", "/some-some.foo", "/some-some.foo/test", "*.foo");
     }
-    */
 
-    private void doTestBypassAndPathInfo(final boolean expectedBypass, final String expectedPathInfo, final String requestPath, String mapping) throws Exception {
+    public void testPathInfoShouldStateWhateverIsAfterTheRegexMapping() throws Exception {
+        shouldMatch("/test", "/some-doc.pdf", "/some-doc.pdf/test", "regex:.*\\.pdf");
+    }
+
+    private void shouldBypass(String requestPath, String mapping) throws Exception {
+        doTestBypassAndPathInfo(true, null, null, requestPath, mapping, false);
+    }
+
+    private void shouldMatch(final String expectedPathInfo, final String expectedServletPath, final String requestPath, String mapping) throws Exception {
+        doTestBypassAndPathInfo(false, expectedPathInfo, expectedServletPath, requestPath, mapping, true);
+    }
+
+    private void doTestBypassAndPathInfo(final boolean shouldBypass, final String expectedPathInfo, final String expectedServletPath, final String requestPath, String mapping, boolean shouldCheckPathInfoAndServletPath) throws Exception {
         FactoryUtil.setInstance(Voting.class, new DefaultVoting());
 
         final FilterChain chain = createNiceMock(FilterChain.class);
         final HttpServletResponse res = createNiceMock(HttpServletResponse.class);
         final HttpServletRequest req = createMock(HttpServletRequest.class);
-        expect(req.getServletPath()).andReturn(requestPath).anyTimes();
         expect(req.getRequestURI()).andReturn("/magnoliaAuthor" + requestPath).anyTimes();
         expect(req.getContextPath()).andReturn("/magnoliaAuthor").anyTimes();
 
         final Servlet servlet = createStrictMock(Servlet.class);
         servlet.service(isA(HttpServletRequestWrapper.class), same(res));
-        if (expectedPathInfo != null) {
+        if (!shouldBypass) {
             expectLastCall().andAnswer(new IAnswer<Object>() {
                 public Object answer() throws Throwable {
                     final HttpServletRequestWrapper requestWrapper = (HttpServletRequestWrapper) getCurrentArguments()[0];
                     final String pathInfo = requestWrapper.getPathInfo();
-                    assertEquals(expectedPathInfo, pathInfo);
+                    final String servletPath = requestWrapper.getServletPath();
+                    assertEquals("pathInfo does not match", expectedPathInfo, pathInfo);
+                    assertEquals("servletPath does not match", expectedServletPath, servletPath);
                     return null;
                 }
             });
@@ -114,7 +169,7 @@ public class ServletDispatchingFilterTest extends TestCase {
 
         filter.addMapping(mapping);
 
-        assertEquals(expectedBypass, filter.bypasses(req));
+        assertEquals("Should " + (shouldBypass ? "" : "not ") + "have bypassed", shouldBypass, filter.bypasses(req));
         filter.doFilter(req, res, chain);
 
         verify(chain, res, req, servlet);
