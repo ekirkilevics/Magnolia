aa2efb094c167f5f32423eadd23cee5a87d30e2e
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2008-10-08 19:36:54 +0000

    MAGNOLIA-1482 / MAGNOLIA-2170 : a FileTemplateLoader which can be configured with content2bean
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@18848 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-module-templating/src/main/java/info/magnolia/module/templating/LazyFileTemplateLoader.java
@@ -0,0 +1,98 @@
+/**
+ * This file Copyright (c) 2003-2008 Magnolia International
+ * Ltd.  (http://www.magnolia.info). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia.info/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.module.templating;
+
+import freemarker.cache.FileTemplateLoader;
+import freemarker.cache.TemplateLoader;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.File;
+
+/**
+ * A TemplateLoader wrapping freemarker's FileTemplateLoader, initializing it
+ * lazily, thus allowing setting its parameters with content2bean.
+ *
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ */
+public class LazyFileTemplateLoader implements TemplateLoader {
+    private String basePath;
+    private boolean allowLinking;
+    private FileTemplateLoader delegate;
+
+    public String getBasePath() {
+        return basePath;
+    }
+
+    public void setBasePath(String basePath) {
+        this.basePath = basePath;
+    }
+
+    public boolean isAllowLinking() {
+        return allowLinking;
+    }
+
+    public void setAllowLinking(boolean allowLinking) {
+        this.allowLinking = allowLinking;
+    }
+
+    public Object findTemplateSource(String name) throws IOException {
+        return getDelegate().findTemplateSource(name);
+    }
+
+    public long getLastModified(Object templateSource) {
+        return getDelegate().getLastModified(templateSource);
+    }
+
+    public Reader getReader(Object templateSource, String encoding) throws IOException {
+        return getDelegate().getReader(templateSource, encoding);
+    }
+
+    public void closeTemplateSource(Object templateSource) {
+        getDelegate().closeTemplateSource(templateSource);
+    }
+
+    // TODO : synchronized block around the FileTemplateLoader initialization ?
+    protected FileTemplateLoader getDelegate() {
+        if (delegate == null) {
+            try {
+                delegate = new FileTemplateLoader(new File(basePath), allowLinking);
+            } catch (IOException e) {
+                throw new IllegalArgumentException("Can't initialize FileTemplateLoader: " + e.getMessage());
+            }
+        }
+        return delegate;
+    }
+}
