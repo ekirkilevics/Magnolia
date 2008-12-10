8008833642a6c4d77c6ccde814efe6f4919c23e5
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2008-12-10 22:18:56 +0000

    imports
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@20443 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/cms/security/auth/login/BasicLogin.java
+++ magnolia-core/src/main/java/info/magnolia/cms/security/auth/login/BasicLogin.java
@@ -33,24 +33,20 @@
  */
 package info.magnolia.cms.security.auth.login;
 
-import info.magnolia.cms.security.auth.callback.CredentialsCallbackHandler;
 import info.magnolia.cms.security.auth.callback.Base64CallbackHandler;
-import info.magnolia.cms.security.Authenticator;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.security.auth.login.LoginException;
-
+import info.magnolia.cms.security.auth.callback.CredentialsCallbackHandler;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Sameer Charles
  * $Id$
  */
 public class BasicLogin extends LoginHandlerBase {
-
     private static final Logger log = LoggerFactory.getLogger(BasicLogin.class);
 
     public LoginResult handle(HttpServletRequest request, HttpServletResponse response) {
