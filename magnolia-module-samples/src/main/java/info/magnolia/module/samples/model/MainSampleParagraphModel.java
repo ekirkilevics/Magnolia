73c1356ba381ab4f0ec7c7370614251aad1c1d3e
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-01-14 19:34:33 +0000

    MAGNOLIA-2370 moved all templating-related classes from info.magnolia.cms.beans.config and info.magnolia.cms.beans.runtime to info.magnolia.module.templating
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@21341 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-samples/src/main/java/info/magnolia/module/samples/model/MainSampleParagraphModel.java
+++ magnolia-module-samples/src/main/java/info/magnolia/module/samples/model/MainSampleParagraphModel.java
@@ -37,12 +37,12 @@ import java.io.IOException;
 
 import org.apache.commons.lang.StringUtils;
 
-import info.magnolia.cms.beans.config.RenderableDefinition;
-import info.magnolia.cms.beans.config.RenderingModel;
-import info.magnolia.cms.beans.config.RenderingModelImpl;
 import info.magnolia.cms.core.Content;
 import info.magnolia.context.MgnlContext;
 import info.magnolia.context.WebContext;
+import info.magnolia.module.templating.RenderingModelImpl;
+import info.magnolia.module.templating.RenderableDefinition;
+import info.magnolia.module.templating.RenderingModel;
 
 /**
  *
