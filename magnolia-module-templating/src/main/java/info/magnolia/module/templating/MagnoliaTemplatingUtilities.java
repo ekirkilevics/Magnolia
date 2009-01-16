e78f0ccf08f98006f546d5a0a444cb3c8849a517
Jan Haderka <jan.haderka@magnolia-cms.com>
2009-01-16 12:03:51 +0000

    MAGNOLIA-2542 Moved common methods for link creation from STKUtil to MagnoliaTemplatingUtilities.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@21410 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-templating/src/main/java/info/magnolia/module/templating/MagnoliaTemplatingUtilities.java
+++ magnolia-module-templating/src/main/java/info/magnolia/module/templating/MagnoliaTemplatingUtilities.java
@@ -36,12 +36,19 @@ package info.magnolia.module.templating;
 import freemarker.core.Environment;
 import info.magnolia.cms.beans.config.ServerConfiguration;
 import info.magnolia.cms.core.Content;
+import info.magnolia.cms.core.NodeData;
 import info.magnolia.cms.i18n.I18nContentWrapper;
 import info.magnolia.cms.util.InheritanceContentWrapper;
 import info.magnolia.cms.util.SiblingsHelper;
 import info.magnolia.context.MgnlContext;
+import info.magnolia.link.LinkUtil;
 
 import javax.jcr.RepositoryException;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.Writer;
 
@@ -54,46 +61,70 @@ import java.io.Writer;
  */
 public class MagnoliaTemplatingUtilities {
 
+    private static final Logger log = LoggerFactory.getLogger(MagnoliaTemplatingUtilities.class);
+
     /**
      * Returns an instance of SiblingsHelper for the given node.
      */
-    public SiblingsHelper siblings(Content node) throws RepositoryException {
+    public static SiblingsHelper siblings(Content node) throws RepositoryException {
         return SiblingsHelper.of(node);
     }
 
     /**
      * FreeMarker only.
      */
-    public void renderParagraph(Content paragraphNode) throws RenderException, IOException {
+    public static void renderParagraph(Content paragraphNode) throws RenderException, IOException {
         final Environment env = Environment.getCurrentEnvironment();
         final Writer out = env.getOut();
         // TODO - set and unset AggState.currentContent
         ParagraphRenderingFacade.getInstance().render(paragraphNode, out);
     }
 
-    public Content inherit(Content node) {
+    public static Content inherit(Content node) {
         return new InheritanceContentWrapper(node);
     }
 
-    public Content i18n(Content node) {
+    public static Content i18n(Content node) {
         return new I18nContentWrapper(node);
     }
 
-    public boolean isEditMode(){
+    public static boolean isEditMode(){
         // TODO : see CmsFunctions.isEditMode, which checks a couple of other properties.
         return isAuthorInstance() && !isPreviewMode();
     }
 
-    public boolean isPreviewMode(){
+    public static boolean isPreviewMode(){
         return MgnlContext.getAggregationState().isPreviewMode();
     }
 
-    public boolean isAuthorInstance(){
+    public static boolean isAuthorInstance(){
         return ServerConfiguration.getInstance().isAdmin();
     }
 
-    public boolean isPublicInstance(){
+    public static boolean isPublicInstance(){
         return !isAuthorInstance();
+    }
+
+    public static String createLink(NodeData nd) {
+        try {
+            return LinkUtil.createAbsoluteLink(nd);
+        } catch (RepositoryException e) {
+            log.error("Can't resolve link defined in node {} because of {}.", nd.getHandle(), ExceptionUtils.getRootCauseMessage(e));
+            return null;
+        }
+    }
+
+    public static String createLink(String repositoryId, String uuid) {
+        try {
+            return LinkUtil.createLink(repositoryId, uuid);
+        } catch (RepositoryException e) {
+            log.error("Can't resolve link with UUID {} because of {}.", uuid , ExceptionUtils.getRootCauseMessage(e));
+            return null;
+        }
+    }
+
+    public static String createLink(Content node) {
+        return LinkUtil.createAbsoluteLink(node);
     }
 
 }
