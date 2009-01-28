62ec8255a80758b7302a28511334a5ee74d562d7
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-01-28 20:00:34 +0000

    MAGNOLIA-2597 MAGNOLIA-2595 We now have an abstract QueryTask
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@21939 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/module/delta/QueryTask.java
@@ -0,0 +1,71 @@
+/**
+ * This file Copyright (c) 2009 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.module.delta;
+
+import info.magnolia.cms.core.Content;
+import info.magnolia.cms.core.search.Query;
+import info.magnolia.cms.util.QueryUtil;
+import info.magnolia.module.InstallContext;
+
+import javax.jcr.RepositoryException;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * An abstract task to perform operations on nodes returned by a given query.
+ *
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ */
+public abstract class QueryTask extends AbstractRepositoryTask {
+    private final String repositoryName;
+    private final String query;
+
+    public QueryTask(String name, String description, String repositoryName, String query) {
+        super(name, description);
+        this.repositoryName = repositoryName;
+        this.query = query;
+    }
+
+    protected void doExecute(InstallContext installContext) throws RepositoryException, TaskExecutionException {
+        final Collection nodes = QueryUtil.query(repositoryName, query, Query.SQL);
+        final Iterator it = nodes.iterator();
+        while (it.hasNext()) {
+            final Content paragraphNode = (Content) it.next();
+            operateOnNode(installContext, paragraphNode);
+        }
+    }
+
+    protected abstract void operateOnNode(InstallContext installContext, Content node);
+}
