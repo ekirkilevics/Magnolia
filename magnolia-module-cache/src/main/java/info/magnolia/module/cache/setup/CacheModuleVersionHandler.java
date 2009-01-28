bff21c5a267e1ef46cd2afa11e7a57c3d4184469
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-01-28 19:16:54 +0000

    cleanup and removed redundant task from DMSModuleVersionHandler, which is taken care of by CacheModuleVersionHandler
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@21938 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-cache/src/main/java/info/magnolia/module/cache/setup/CacheModuleVersionHandler.java
+++ magnolia-module-cache/src/main/java/info/magnolia/module/cache/setup/CacheModuleVersionHandler.java
@@ -37,7 +37,6 @@ import info.magnolia.cms.beans.config.ContentRepository;
 import info.magnolia.cms.core.Content;
 import info.magnolia.cms.core.HierarchyManager;
 import info.magnolia.cms.core.ItemType;
-import info.magnolia.cms.core.NodeData;
 import info.magnolia.module.DefaultModuleVersionHandler;
 import info.magnolia.module.InstallContext;
 import info.magnolia.module.cache.RegisterWorkspaceForCacheFlushingTask;
@@ -47,19 +46,16 @@ import info.magnolia.module.delta.BackupTask;
 import info.magnolia.module.delta.BootstrapResourcesTask;
 import info.magnolia.module.delta.BootstrapSingleResource;
 import info.magnolia.module.delta.CheckAndModifyPropertyValueTask;
-import info.magnolia.module.delta.ConditionalDelegateTask;
 import info.magnolia.module.delta.DeltaBuilder;
 import info.magnolia.module.delta.FilterOrderingTask;
 import info.magnolia.module.delta.NewPropertyTask;
 import info.magnolia.module.delta.NodeExistsDelegateTask;
 import info.magnolia.module.delta.RemoveNodeTask;
-import info.magnolia.module.delta.Task;
 import info.magnolia.module.delta.TaskExecutionException;
 import info.magnolia.module.delta.WarnTask;
 import info.magnolia.module.delta.WebXmlConditionsUtil;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
@@ -85,11 +81,10 @@ public class CacheModuleVersionHandler extends DefaultModuleVersionHandler {
 
         final NodeExistsDelegateTask addFilterIfNotExisting = new NodeExistsDelegateTask("Check cache filter", "A bug in previous 3.5 releases made it possible for the cache filter to be removed. This task readds it if necessary.",
                 ContentRepository.CONFIG, "/server/filters/cache", null, new ArrayDelegateTask("",
-                new BootstrapSingleResource("", "", "/mgnl-bootstrap/cache/config.server.filters.cache.xml"),
-                new FilterOrderingTask("cache", new String[]{"i18n"}),
-                new WarnTask("", "The cache filter was not found, and was just added. If you removed it on purpose, you should consider disabling it instead.")
-        )
-        );
+                        new BootstrapSingleResource("", "", "/mgnl-bootstrap/cache/config.server.filters.cache.xml"),
+                        new FilterOrderingTask("cache", new String[]{"i18n"}),
+                        new WarnTask("", "The cache filter was not found, and was just added. If you removed it on purpose, you should consider disabling it instead.")));
+
         register(DeltaBuilder.update("3.5.9", "")
                 .addTask(addFilterIfNotExisting)
         );
@@ -114,6 +109,7 @@ public class CacheModuleVersionHandler extends DefaultModuleVersionHandler {
                         new FilterOrderingTask("gzip", new String[]{"cache"})
                 ))
         );
+
         register(DeltaBuilder.update("3.6.2", "Updated executors and filter configuration.")
                 .addTask(new BootstrapResourcesTask("Updated configuration", "Bootstraps new default cache configuration.") {
                     protected String[] getResourcesToBootstrap(final InstallContext installContext) {
@@ -139,12 +135,13 @@ public class CacheModuleVersionHandler extends DefaultModuleVersionHandler {
                 return new String[]{"/mgnl-bootstrap/cache/config.modules.cache.config.compression.xml"};
             }
         });
-        // remove previously used compressible content types configuration 
-        list.add(
-                new DefaultCompressibleContentTypesCondition("Cache cleanup", "Removes obsolete cache compression list in favor of new global configuration.", 
+
+        // remove previously used compressible content types configuration
+        list.add(new DefaultCompressibleContentTypesCondition("Cache cleanup", "Removes obsolete cache compression list in favor of new global configuration.",
                         new RemoveNodeTask("Remove obsolete compression list configuration.", "Removes cache executor specific compression list configuration in favor of using one global list for both cache and gzip.", ContentRepository.CONFIG, "/modules/cache/config/configurations/default/executors/store/cacheContent/compressible"), 
                         new WarnTask("Warning", "The compression list configuration have been relocated to /modules/cache/compression/voters/contentType, since you have modified the default configuration, please make sure your customization is applied also to new configuration."),
                         "/modules/cache/config/configurations/default/executors/store/cacheContent/compressible"));
+
         // remove bypass for compressible content types from gzip filter
         list.add(new DefaultCompressibleContentTypesCondition("GZip cleanup", "Removes obsolete gzip bypass in favor of new global configuration.", 
                         new RemoveNodeTask("Remove obsolete bypass.", "Removes content type bypass from gzip filter.", ContentRepository.CONFIG, "/server/filters/gzip/bypasses/contentType"), 
@@ -160,6 +157,7 @@ public class CacheModuleVersionHandler extends DefaultModuleVersionHandler {
                 content.createNodeData("delegatePath","/modules/cache/config/compression/voters");
             }
         });
+        
         list.add(new AbstractRepositoryTask("Cache Flushing", "Migrate old cache flushing configuration to new location." ) {
             protected void doExecute(InstallContext installContext) throws RepositoryException, TaskExecutionException {
                 final String reposPath= "/modules/cache/config/repositories";
@@ -221,35 +219,4 @@ public class CacheModuleVersionHandler extends DefaultModuleVersionHandler {
     }
     */
 
-    class DefaultCompressibleContentTypesCondition extends ConditionalDelegateTask {
-
-        private String checkedNodeName;
-
-        public DefaultCompressibleContentTypesCondition(String taskName, String taskDescription, Task ifTrue, Task ifFalse, String checkedNodeName) {
-            super(taskName, taskDescription, ifTrue, ifFalse);
-            this.checkedNodeName = checkedNodeName;
-        }
-        
-        protected boolean condition(InstallContext installContext) throws TaskExecutionException {
-            try {
-                List vals = new ArrayList();
-                HierarchyManager hm = installContext.getConfigHierarchyManager();
-                if (!hm.isExist(checkedNodeName)) {
-                    // compression configuration doesn't exist, just return false
-                    return false;
-                }
-                Collection compressionList = hm.getContent(checkedNodeName).getNodeDataCollection();
-                for (Iterator iterator = compressionList.iterator(); iterator.hasNext();) {
-                    NodeData data = (NodeData) iterator.next();
-                    vals.add(data.getString());
-                }
-                return vals.remove("text/html") && vals.remove("text/css") && vals.remove("application/x-javascript") && vals.isEmpty();
-                
-            } catch (RepositoryException e) {
-                log.error(e.getMessage(), e);
-            }
-            // the node doesn't have any children or their values can't be retried. Return false so the warning will be displayed.
-            return false;
-        }
-    }
 }
---
+++ magnolia-module-cache/src/main/java/info/magnolia/module/cache/setup/DefaultCompressibleContentTypesCondition.java
@@ -0,0 +1,83 @@
+/**
+ * This file Copyright (c) 2009 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.module.cache.setup;
+
+import info.magnolia.cms.core.HierarchyManager;
+import info.magnolia.cms.core.NodeData;
+import info.magnolia.module.InstallContext;
+import info.magnolia.module.delta.ConditionalDelegateTask;
+import info.magnolia.module.delta.Task;
+import info.magnolia.module.delta.TaskExecutionException;
+
+import javax.jcr.RepositoryException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @version $Revision: $ ($Author: $)
+ */
+public class DefaultCompressibleContentTypesCondition extends ConditionalDelegateTask {
+
+    private final String checkedNodeName;
+
+    public DefaultCompressibleContentTypesCondition(String taskName, String taskDescription, Task ifTrue, Task ifFalse, String checkedNodeName) {
+        super(taskName, taskDescription, ifTrue, ifFalse);
+        this.checkedNodeName = checkedNodeName;
+    }
+
+    protected boolean condition(InstallContext installContext) throws TaskExecutionException {
+        try {
+            List vals = new ArrayList();
+            HierarchyManager hm = installContext.getConfigHierarchyManager();
+            if (!hm.isExist(checkedNodeName)) {
+                // compression configuration doesn't exist, just return false
+                return false;
+            }
+            Collection compressionList = hm.getContent(checkedNodeName).getNodeDataCollection();
+            for (Iterator iterator = compressionList.iterator(); iterator.hasNext();) {
+                NodeData data = (NodeData) iterator.next();
+                vals.add(data.getString());
+            }
+            return vals.remove("text/html") && vals.remove("text/css") && vals.remove("application/x-javascript") && vals.isEmpty();
+
+        } catch (RepositoryException e) {
+            log.error(e.getMessage(), e);
+        }
+        // the node doesn't have any children or their values can't be retried. Return false so the warning will be displayed.
+        return false;
+    }
+}
