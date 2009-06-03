f6f5a295a9750161de0eb9cf54cfb1865acc8935
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-06-03 11:57:04 +0000

    MAGNOLIA-2653 reverting r25674, postponed until 4.1.1
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@25683 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/importexport/filters/MagnoliaV2Filter.java
+++ magnolia-core/src/main/java/info/magnolia/importexport/filters/MagnoliaV2Filter.java
@@ -54,7 +54,6 @@ public class MagnoliaV2Filter extends XMLFilterImpl {
 
     private boolean skipProperty;
 
-    private boolean skipWs;
     /**
      * Instantiates a new version filter.
      * @param parent wrapped XMLReader
@@ -62,6 +61,7 @@ public class MagnoliaV2Filter extends XMLFilterImpl {
     public MagnoliaV2Filter(XMLReader parent) {
         super(parent);
     }
+
     /**
      * @see org.xml.sax.helpers.XMLFilterImpl#endElement(String, String, String)
      */
@@ -73,14 +73,12 @@ public class MagnoliaV2Filter extends XMLFilterImpl {
 
         if (skipNode && "sv:node".equals(qName)) {
             skipNode = false;
-            skipWs = true; // skip additional whitespace after skipped tag
             return;
         }
 
         if (skipProperty) {
             if ("sv:property".equals(qName)) {
                 skipProperty = false;
-                skipWs = true; // skip additional whitespace after skipped tag
             }
             return;
         }
@@ -92,18 +90,10 @@ public class MagnoliaV2Filter extends XMLFilterImpl {
      * @see org.xml.sax.helpers.XMLFilterImpl#characters(char[], int, int)
      */
     public void characters(char[] ch, int start, int length) throws SAXException {
-        // do not emit text inside of skipped elements
-        if (!(skipNode || skipProperty)) {
-            while ( skipWs && length>0
-                    && Character.isWhitespace(ch[start]) ){
-                start ++;
-                length--;
-            }
-            if ( length>0){
-                super.characters(ch, start, length);
-            }
-        }
-        skipWs = false;
+        // filter content
+        // if (inMetadataElement == 0) {
+        super.characters(ch, start, length);
+        // }
     }
 
     /**
@@ -122,11 +112,6 @@ public class MagnoliaV2Filter extends XMLFilterImpl {
         }
 
         if (inMetadataElement > 0) {
-            // MAGNOILA-2653 skip nested elements (sv:value)
-            // os skipped property
-            if ( skipProperty ){
-                return;
-            }
             // remove
             // <sv:node sv:name="jcr:content">
             if ("sv:node".equals(qName) && "jcr:content".equals(svname)) {
