8f774968719b1bd3e14d8ed6292b864e926b0b32
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-06-08 19:37:43 +0000

    fixed wording of an update task and added warning comment for 4.0.2 vs 4.1 tasks
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@26010 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/setup/CoreModuleVersionHandler.java
+++ magnolia-core/src/main/java/info/magnolia/setup/CoreModuleVersionHandler.java
@@ -118,7 +118,10 @@ public class CoreModuleVersionHandler extends AbstractModuleVersionHandler {
         );
         register(DeltaBuilder.update("4.0.2", "")
                 .addTask(new UpdateUsers())
-                .addTask(new CheckAndModifyPropertyValueTask("Anonymous user rights", "Update anonymous user access rights.", ContentRepository.USERS, "/system/anonymous/acl_users/0", "path", "/anonymous/*", "/system/anonymous/*")));
+                .addTask(new CheckAndModifyPropertyValueTask("Fix for anonymous user permissions", "Fix previously incorrect path for anonymous user permissions.", ContentRepository.USERS, "/system/anonymous/acl_users/0", "path", "/anonymous/*", "/system/anonymous/*")));
+                // warning - if adding update tasks for the 4.0.2 release below this point,
+                // make sure they'd also be applied correctly for the 4.1 branch in the various possible update scenarios
+                // since 4.1 will be released before 4.0.2 (4.0.2 -> 4.1 -> 4.1.1; 4.0.1 -> 4.1 -> 4.1.1) 
     }
 
     protected List getBasicInstallTasks(InstallContext ctx) {
