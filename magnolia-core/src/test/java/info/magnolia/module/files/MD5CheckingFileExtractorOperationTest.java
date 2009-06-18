3cd3f1ef3327a55103b4913de325d1d72dff92aa
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-06-18 15:27:56 +0000

    fixing incomplete test for MD5CheckingFileExtractorOperation
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@26301 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/test/java/info/magnolia/module/files/MD5CheckingFileExtractorOperationTest.java
+++ magnolia-core/src/test/java/info/magnolia/module/files/MD5CheckingFileExtractorOperationTest.java
@@ -33,9 +33,15 @@
  */
 package info.magnolia.module.files;
 
-import junit.framework.TestCase;
-import static org.easymock.EasyMock.*;
+import info.magnolia.cms.core.Content;
 import info.magnolia.cms.core.HierarchyManager;
+import info.magnolia.cms.core.NodeData;
+import junit.framework.TestCase;
+import org.apache.commons.io.IOUtils;
+import static org.easymock.EasyMock.*;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 /**
  *
@@ -44,17 +50,39 @@ import info.magnolia.cms.core.HierarchyManager;
  */
 public class MD5CheckingFileExtractorOperationTest extends TestCase {
     public void testAbsentFilesAreRecreated() throws Exception {
+        final String resourcePath = "/info/magnolia/test/mock/testcontent.properties";
+        final String fileInfoNodePath = "/server/install" + resourcePath;
+
+        assertNotNull(getClass().getResourceAsStream(resourcePath));
+        final File testOut = new File(System.getProperty("java.io.tmpdir"), "MD5CheckingFileExtractorOperationTest-testAbsentFilesAreRecreated.tmp");
+        testOut.deleteOnExit();
+
+        // the file is gone from the file system
+        assertEquals(false, testOut.exists());
+
         final HierarchyManager hm = createStrictMock(HierarchyManager.class);
+        final Content fileInfoNode = createStrictMock(Content.class);
+        final NodeData md5 = createStrictMock(NodeData.class);
+        // the file had already been extracted:
+        expect(hm.isExist(fileInfoNodePath)).andReturn(true);
+        expect(hm.getContent(fileInfoNodePath)).andReturn(fileInfoNode);
+        expect(fileInfoNode.hasNodeData("md5")).andReturn(true);
+        expect(fileInfoNode.getNodeData("md5")).andReturn(md5);
+        md5.setValue(isA(String.class));
+
         final MD5CheckingFileExtractorOperation op = new MD5CheckingFileExtractorOperation(new FileExtractionLogger() {
             public void error(String message) {
                 fail(message);
             }
-        }, hm, "/yop", "/tmp");
+        }, hm, resourcePath, testOut.getAbsolutePath());
 
-        replay(hm);
+        replay(hm, fileInfoNode, md5);
 
         op.extract();
 
-        verify(hm);
+        verify(hm, fileInfoNode, md5);
+        
+        assertEquals("File should have been re-extracted", true, testOut.exists());
+        assertEquals(IOUtils.toString(new FileInputStream(testOut)), IOUtils.toString(getClass().getResourceAsStream(resourcePath)));
     }
 }
