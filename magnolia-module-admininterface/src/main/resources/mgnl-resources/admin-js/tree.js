08f5668a7ce3b5a9fae416e8a425d0424b2d2187
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-06-22 14:30:03 +0000

    MAGNOLIA-2654 i was a little too eager in adding units - and for once, IE6 is the one that was stricter and more correct about this
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@26378 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-admininterface/src/main/resources/mgnl-resources/admin-js/tree.js
+++ magnolia-module-admininterface/src/main/resources/mgnl-resources/admin-js/tree.js
@@ -352,7 +352,7 @@
                 if (columnResizerLine){
                     if (!columnNumber || elem==columnNumber){
                         columnResizerLine.style.left=left + 'px';
-                        columnResizerLine.style.height=this.divMain.style.height + 'px';
+                        columnResizerLine.style.height=this.divMain.style.height;
                      }
                 }
 
