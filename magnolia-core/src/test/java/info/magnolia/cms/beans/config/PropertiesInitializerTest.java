ccd35948f00d9ec9ce0fbe50004a9d03c0bbc144
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-08-03 15:35:32 +0000

    test cleanup
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@26965 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/test/java/info/magnolia/cms/beans/config/PropertiesInitializerTest.java
+++ magnolia-core/src/test/java/info/magnolia/cms/beans/config/PropertiesInitializerTest.java
@@ -34,12 +34,10 @@
 package info.magnolia.cms.beans.config;
 
 import info.magnolia.cms.core.SystemProperty;
+import junit.framework.TestCase;
 
 import java.io.File;
 import java.net.URL;
-
-import junit.framework.TestCase;
-
 
 /**
  * @author fgiust
@@ -50,61 +48,39 @@ public class PropertiesInitializerTest extends TestCase {
         super.setUp();
         // shunt log4j
         org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
-    }
 
-    public void testSimpleProperty() throws Exception {
+        // load test properties
         PropertiesInitializer pi = PropertiesInitializer.getInstance();
 
         URL propertyUrl = this.getClass().getResource("/test-magnolia.properties");
         String propertyDir = new File(propertyUrl.toURI().getRawPath()).getParent();
 
         pi.loadAllProperties("test-magnolia.properties", propertyDir);
+    }
 
+    protected void tearDown() throws Exception {
+        SystemProperty.getPropertyList().clear();
+        super.tearDown();
+    }
+
+    public void testSimpleProperty() throws Exception {
         assertEquals("property", SystemProperty.getProperty("test.one"));
     }
 
     public void testNestedProperty() throws Exception {
-        PropertiesInitializer pi = PropertiesInitializer.getInstance();
-
-        URL propertyUrl = this.getClass().getResource("/test-magnolia.properties");
-        String propertyDir = new File(propertyUrl.toURI().getRawPath()).getParent();
-
-        pi.loadAllProperties("test-magnolia.properties", propertyDir);
-
         assertEquals("nested property", SystemProperty.getProperty("test.two"));
     }
 
     public void testNestedPropertyMoreLevels() throws Exception {
-        PropertiesInitializer pi = PropertiesInitializer.getInstance();
-
-        URL propertyUrl = this.getClass().getResource("/test-magnolia.properties");
-        String propertyDir = new File(propertyUrl.toURI().getRawPath()).getParent();
-
-        pi.loadAllProperties("test-magnolia.properties", propertyDir);
-
         assertEquals("another nested property", SystemProperty.getProperty("test.three"));
     }
 
     public void testCircularProperty() throws Exception {
-        PropertiesInitializer pi = PropertiesInitializer.getInstance();
-
-        URL propertyUrl = this.getClass().getResource("/test-magnolia.properties");
-        String propertyDir = new File(propertyUrl.toURI().getRawPath()).getParent();
-
-        pi.loadAllProperties("test-magnolia.properties", propertyDir);
-
         assertEquals("${test.circular2}", SystemProperty.getProperty("test.circular1"));
         assertEquals("${test.circular1}", SystemProperty.getProperty("test.circular2"));
     }
 
     public void testSelfReferencingProperty() throws Exception {
-        PropertiesInitializer pi = PropertiesInitializer.getInstance();
-
-        URL propertyUrl = this.getClass().getResource("/test-magnolia.properties");
-        String propertyDir = new File(propertyUrl.toURI().getRawPath()).getParent();
-
-        pi.loadAllProperties("test-magnolia.properties", propertyDir);
-
         assertEquals("${test.circular3}", SystemProperty.getProperty("test.circular3"));
     }
 }
