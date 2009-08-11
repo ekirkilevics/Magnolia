62e149e8e3b8eed881b6e549d354aa909bed3dff
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-08-11 17:43:17 +0000

    MAGNOLIA-2816 Factored out info.magnolia.module.ModuleVersionHandlerTestCase, so it can hopefully be reused to test other ModuleVersionHandlers
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@27120 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/test/java/info/magnolia/module/ModuleVersionHandlerTestCase.java
@@ -0,0 +1,134 @@
+/**
+ * This file Copyright (c) 2009 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.module;
+
+import info.magnolia.cms.core.Content;
+import info.magnolia.cms.core.HierarchyManager;
+import info.magnolia.cms.util.ContentUtil;
+import info.magnolia.cms.util.FactoryUtil;
+import info.magnolia.cms.util.NodeDataUtil;
+import info.magnolia.context.MgnlContext;
+import info.magnolia.logging.AuditLoggingManager;
+import info.magnolia.module.delta.Delta;
+import info.magnolia.module.model.ModuleDefinition;
+import info.magnolia.module.model.Version;
+import info.magnolia.module.model.reader.BetwixtModuleDefinitionReader;
+import info.magnolia.module.model.reader.ModuleDefinitionReader;
+import info.magnolia.test.RepositoryTestCase;
+
+import javax.jcr.RepositoryException;
+import java.util.Collections;
+import java.util.List;
+
+import static org.easymock.EasyMock.*;
+
+/**
+ * A base class for testing implementations of ModuleVersionHandler.
+ *
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ */
+public abstract class ModuleVersionHandlerTestCase extends RepositoryTestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        // this should disable observation for audit logging (mgnl-beans.properties registers a path for this componenent)
+        FactoryUtil.setInstance(AuditLoggingManager.class, new AuditLoggingManager());
+    }
+
+    /**
+     * A helper method to quickly set up a few properties to simulate a given environment.
+     * Could be advantageously replaced by a dsl-like api, see MAGNOLIA-
+     */
+    protected void setupProperty(final String workspace, String path, String propertyName, String value) throws RepositoryException {
+        final HierarchyManager hm = MgnlContext.getHierarchyManager(workspace);
+        final Content content = ContentUtil.createPath(hm, path);
+        NodeDataUtil.getOrCreateAndSet(content, propertyName, value);
+        hm.save();
+    }
+
+    /**
+     * This essentially fakes calls to ModuleManager and ensures only the ModuleVersionHandler under test
+     * is in use.
+     * It's likely that this will need improvements when we want to test ModuleVersionHandler which
+     * use IsModuleInstalledOrRegistered tasks, for example.
+     */
+    protected void executeUpdatesAsIfTheCurrentlyInstalledVersionWas(final Version currentlyInstalledVersion) throws ModuleManagementException {
+        final BetwixtModuleDefinitionReader reader = new BetwixtModuleDefinitionReader();
+        final ModuleDefinition moduleDefinition = reader.readFromResource(getModuleDescriptorPath());
+
+        final ModuleDefinitionReader readerMock = createStrictMock(ModuleDefinitionReader.class);
+        expect(readerMock.readAll()).andReturn(Collections.singletonMap(moduleDefinition.getName(), moduleDefinition));
+        replay(readerMock);
+
+        final ModuleVersionHandler versionHandlerUnderTest = newModuleVersionHandlerForTests();
+        final InstallContextImpl ctx = new InstallContextImpl();
+        final ModuleRegistryImpl moduleRegistry = new ModuleRegistryImpl();
+        final ModuleManagerImpl mm = new ModuleManagerImpl(ctx, readerMock, moduleRegistry) {
+            protected ModuleVersionHandler newVersionHandler(ModuleDefinition module) {
+                assertEquals("this test doesn't behave as expected", moduleDefinition, module);
+                // wrap and delegate so that we control getCurrentlyInstalled()
+                return new ModuleVersionHandler() {
+                    public Version getCurrentlyInstalled(InstallContext ctx) {
+                        return currentlyInstalledVersion;
+                    }
+
+                    public List getDeltas(InstallContext installContext, Version from) {
+                        return versionHandlerUnderTest.getDeltas(installContext, from);
+                    }
+
+                    public Delta getStartupDelta(InstallContext installContext) {
+                        return versionHandlerUnderTest.getStartupDelta(installContext);
+                    }
+                };
+            }
+        };
+
+        final List<ModuleDefinition> defs = mm.loadDefinitions();
+        assertEquals(1, defs.size());
+        assertEquals(moduleDefinition, defs.get(0));
+
+        mm.checkForInstallOrUpdates();
+        assertTrue(mm.getStatus().needsUpdateOrInstall());
+
+        mm.performInstallOrUpdate();
+        assertEquals(InstallStatus.installDone, mm.getInstallContext().getStatus());
+
+        verify(readerMock);
+    }
+
+    protected abstract String getModuleDescriptorPath();
+
+    protected abstract ModuleVersionHandler newModuleVersionHandlerForTests();
+}
--- magnolia-core/src/test/java/info/magnolia/setup/CoreModuleVersionHandlerTest.java
+++ magnolia-core/src/test/java/info/magnolia/setup/CoreModuleVersionHandlerTest.java
@@ -34,26 +34,11 @@
 package info.magnolia.setup;
 
 import info.magnolia.cms.beans.config.ContentRepository;
-import info.magnolia.cms.core.Content;
-import info.magnolia.cms.core.HierarchyManager;
-import info.magnolia.cms.util.ContentUtil;
-import info.magnolia.cms.util.FactoryUtil;
-import info.magnolia.cms.util.NodeDataUtil;
 import info.magnolia.context.MgnlContext;
-import info.magnolia.logging.AuditLoggingManager;
-import info.magnolia.module.InstallContext;
-import info.magnolia.module.InstallContextImpl;
-import info.magnolia.module.InstallStatus;
 import info.magnolia.module.ModuleManagementException;
-import info.magnolia.module.ModuleManagerImpl;
-import info.magnolia.module.ModuleRegistryImpl;
 import info.magnolia.module.ModuleVersionHandler;
-import info.magnolia.module.model.ModuleDefinition;
+import info.magnolia.module.ModuleVersionHandlerTestCase;
 import info.magnolia.module.model.Version;
-import info.magnolia.module.model.reader.BetwixtModuleDefinitionReader;
-import info.magnolia.module.model.reader.ModuleDefinitionReader;
-import info.magnolia.test.RepositoryTestCase;
-import static org.easymock.EasyMock.*;
 
 import javax.jcr.RepositoryException;
 import java.util.Collections;
@@ -64,15 +49,22 @@ import java.util.List;
  * @author gjoseph
  * @version $Revision: $ ($Author: $)
  */
-public class CoreModuleVersionHandlerTest extends RepositoryTestCase {
-    protected void setUp() throws Exception {
-        super.setUp();
-        // this should disable observation for audit logging (mgnl-beans.properties registers a path for this componenent)
-        FactoryUtil.setInstance(AuditLoggingManager.class, new AuditLoggingManager());
+public class CoreModuleVersionHandlerTest extends ModuleVersionHandlerTestCase {
+    protected String getModuleDescriptorPath() {
+        return "/META-INF/magnolia/core.xml";
+    }
+
+    protected ModuleVersionHandler newModuleVersionHandlerForTests() {
+        return new CoreModuleVersionHandler() {
+            // cheat - one of the conditions needs web.xml. Can't be bothered to fake that here
+            protected List getInstallConditions() {
+                return Collections.emptyList();
+            }
+        };
     }
 
     public void testPngSwfMimeTypesOnInstall() throws ModuleManagementException, RepositoryException {
-        doUpdatesOnInstallAsIfTheCurrentlyInstalledVersionOfCoreWas(null);
+        executeUpdatesAsIfTheCurrentlyInstalledVersionWas(null);
 
         assertEquals("image/png", getMimeTypePropertyValue("png"));
         assertEquals("application/x-shockwave-flash", getMimeTypePropertyValue("swf"));
@@ -89,7 +81,7 @@ public class CoreModuleVersionHandlerTest extends RepositoryTestCase {
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/png", "mime-type", "application/octet-stream");
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/swf", "mime-type", "application/octet-stream");
 
-        doUpdatesOnInstallAsIfTheCurrentlyInstalledVersionOfCoreWas(Version.parseVersion("3.5"));
+        executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("3.5"));
 
         assertEquals("image/png", getMimeTypePropertyValue("png"));
         assertEquals("application/x-shockwave-flash", getMimeTypePropertyValue("swf"));
@@ -106,7 +98,7 @@ public class CoreModuleVersionHandlerTest extends RepositoryTestCase {
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/png", "mime-type", "custom-type-for-png");
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/swf", "mime-type", "custom-type-for-swf");
 
-        doUpdatesOnInstallAsIfTheCurrentlyInstalledVersionOfCoreWas(Version.parseVersion("3.5"));
+        executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("3.5"));
 
         assertEquals("custom-type-for-png", getMimeTypePropertyValue("png"));
         assertEquals("custom-type-for-swf", getMimeTypePropertyValue("swf"));
@@ -125,7 +117,7 @@ public class CoreModuleVersionHandlerTest extends RepositoryTestCase {
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/png", "mime-type", "image/png;");
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/swf", "mime-type", "application/x-shockwave-flash;");
 
-        doUpdatesOnInstallAsIfTheCurrentlyInstalledVersionOfCoreWas(Version.parseVersion("3.6.4"));
+        executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("3.6.4"));
 
         assertEquals("image/png", getMimeTypePropertyValue("png"));
         assertEquals("application/x-shockwave-flash", getMimeTypePropertyValue("swf"));
@@ -150,7 +142,7 @@ public class CoreModuleVersionHandlerTest extends RepositoryTestCase {
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/png", "mime-type", "custom-type-for-png");
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/swf", "mime-type", "custom-type-for-swf");
 
-        doUpdatesOnInstallAsIfTheCurrentlyInstalledVersionOfCoreWas(Version.parseVersion("3.6.4"));
+        executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("3.6.4"));
 
         assertEquals("custom-type-for-png", getMimeTypePropertyValue("png"));
         assertEquals("custom-type-for-swf", getMimeTypePropertyValue("swf"));
@@ -170,7 +162,7 @@ public class CoreModuleVersionHandlerTest extends RepositoryTestCase {
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/png", "mime-type", "image/png;");
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/swf", "mime-type", "application/x-shockwave-flash;");
 
-        doUpdatesOnInstallAsIfTheCurrentlyInstalledVersionOfCoreWas(Version.parseVersion("4.0.1"));
+        executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("4.0.1"));
 
         assertEquals("image/png", getMimeTypePropertyValue("png"));
         assertEquals("application/x-shockwave-flash", getMimeTypePropertyValue("swf"));
@@ -196,7 +188,7 @@ public class CoreModuleVersionHandlerTest extends RepositoryTestCase {
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/png", "mime-type", "custom-type-for-png");
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/swf", "mime-type", "custom-type-for-swf");
 
-        doUpdatesOnInstallAsIfTheCurrentlyInstalledVersionOfCoreWas(Version.parseVersion("4.0.1"));
+        executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("4.0.1"));
 
         assertEquals("custom-type-for-png", getMimeTypePropertyValue("png"));
         assertEquals("custom-type-for-swf", getMimeTypePropertyValue("swf"));
@@ -216,7 +208,7 @@ public class CoreModuleVersionHandlerTest extends RepositoryTestCase {
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/png", "mime-type", "image/png;");
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/swf", "mime-type", "application/x-shockwave-flash;");
 
-        doUpdatesOnInstallAsIfTheCurrentlyInstalledVersionOfCoreWas(Version.parseVersion("4.1"));
+        executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("4.1"));
 
         assertEquals("image/png", getMimeTypePropertyValue("png"));
         assertEquals("application/x-shockwave-flash", getMimeTypePropertyValue("swf"));
@@ -242,59 +234,14 @@ public class CoreModuleVersionHandlerTest extends RepositoryTestCase {
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/png", "mime-type", "custom-type-for-png");
         setupProperty(ContentRepository.CONFIG, "/server/MIMEMapping/swf", "mime-type", "custom-type-for-swf");
 
-        doUpdatesOnInstallAsIfTheCurrentlyInstalledVersionOfCoreWas(Version.parseVersion("4.1"));
+        executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("4.1"));
 
         assertEquals("custom-type-for-png", getMimeTypePropertyValue("png"));
         assertEquals("custom-type-for-swf", getMimeTypePropertyValue("swf"));
-    }
-
-    private void setupProperty(final String workspace, String path, String propertyName, String value) throws RepositoryException {
-        final HierarchyManager hm = MgnlContext.getHierarchyManager(workspace);
-        final Content content = ContentUtil.createPath(hm, path);
-        NodeDataUtil.getOrCreateAndSet(content, propertyName, value);
-        hm.save();
     }
 
     private String getMimeTypePropertyValue(String typeName) throws RepositoryException {
         return MgnlContext.getHierarchyManager("config").getContent("/server/MIMEMapping/" + typeName).getNodeData("mime-type").getString();
     }
 
-    private void doUpdatesOnInstallAsIfTheCurrentlyInstalledVersionOfCoreWas(final Version currentVersion) throws ModuleManagementException {
-        final BetwixtModuleDefinitionReader reader = new BetwixtModuleDefinitionReader();
-        final ModuleDefinition core = reader.readFromResource("/META-INF/magnolia/core.xml");
-
-        final ModuleDefinitionReader readerMock = createStrictMock(ModuleDefinitionReader.class);
-        expect(readerMock.readAll()).andReturn(Collections.singletonMap("core", core));
-        replay(readerMock);
-
-        final InstallContextImpl ctx = new InstallContextImpl();
-        final ModuleRegistryImpl moduleRegistry = new ModuleRegistryImpl();
-        final ModuleManagerImpl mm = new ModuleManagerImpl(ctx, readerMock, moduleRegistry) {
-            protected ModuleVersionHandler newVersionHandler(ModuleDefinition module) {
-                assertEquals("this test doesn't behave as expected", core, module);
-                return new CoreModuleVersionHandler() {
-                    public Version getCurrentlyInstalled(InstallContext ctx) {
-                        return currentVersion;
-                    }
-
-                    // cheat - one of the conditions needs web.xml. Can't be bothered to fake that here
-                    protected List getInstallConditions() {
-                        return Collections.emptyList();
-                    }
-                };
-            }
-        };
-
-        final List<ModuleDefinition> defs = mm.loadDefinitions();
-        assertEquals(1, defs.size());
-        assertEquals(core, defs.get(0));
-
-        mm.checkForInstallOrUpdates();
-        assertTrue(mm.getStatus().needsUpdateOrInstall());
-
-        mm.performInstallOrUpdate();
-        assertEquals(InstallStatus.installDone, mm.getInstallContext().getStatus());
-
-        verify(readerMock);
-    }
 }
