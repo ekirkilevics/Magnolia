73a4a505a280b91605ff2e6a13d9cf387aed8398
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-11-05 18:05:26 +0000

    MAGNOLIA-2929 introducing a wrapper/helper for unicode normalization
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@29241 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/cms/util/UnicodeNormalizer.java
@@ -0,0 +1,152 @@
+/**
+ * This file Copyright (c) 2009 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.cms.util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * A wrapper around java.text.Normalizer and com.ibm.icu.text.Normalizer; uses the former if present, or none
+ * if none is present.
+ *
+ * <strong>note:</strong> if not running under Java >=6, and without ICU, this does nothing.
+ * If needed, one can use their own implementation, by setting the info.magnolia.cms.util.UnicodeNormalizer$Normalizer
+ * system property.
+ *
+ * @see java.text.Normalizer
+ * @see com.ibm.icu.text.Normalizer
+ * @see <a href="http://www.icu-project.org/">http://www.icu-project.org/</a> to get the ICU4J library.
+ * @see <a href="http://en.wikipedia.org/wiki/Unicode_equivalence#Normal_forms">http://en.wikipedia.org/wiki/Unicode_equivalence#Normal_forms</a> for more information.
+ *
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ */
+public class UnicodeNormalizer {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(UnicodeNormalizer.class);
+
+    private static final String JAVA6_NORMALIZER_CLASS = "java.text.Normalizer";
+    private static final String JAVA6_FORMPARAM_CLASS = "java.text.Normalizer$Form";
+    private static final String ICU_NORMALIZER_CLASS = "com.ibm.icu.text.Normalizer";
+
+    private static final Normalizer normalizer = (Normalizer) FactoryUtil.newInstance(Normalizer.class);
+
+    /**
+     * Normalizes the given String to the NFC form.
+     */
+    public static String normalizeNFC(String in) {
+        return normalizer.normalizeNFC(in);
+    }
+
+    public interface Normalizer {
+        String normalizeNFC(String in);
+    }
+
+    /**
+     * This uses reflection, since we're still compiling with Java 5.
+     * This implementation could be externalized to a "java 6 only" module if needed.
+     */
+    public static final class Java6ReflectionNormalizer implements Normalizer {
+        private final Method normalize;
+        private final Object nfc;
+
+        public Java6ReflectionNormalizer() {
+            try {
+                final Class<?> normalizer = Class.forName(JAVA6_NORMALIZER_CLASS);
+                final Class<?> form = Class.forName(JAVA6_FORMPARAM_CLASS);
+                normalize = normalizer.getMethod("normalize", CharSequence.class, form);
+                nfc = form.getField("NFC").get(null);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchFieldException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+        public String normalizeNFC(String in) {
+            try {
+                return (String) normalize.invoke(null, in, nfc);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static final class ICUNormalizer implements UnicodeNormalizer.Normalizer {
+        public String normalizeNFC(String in) {
+            return com.ibm.icu.text.Normalizer.normalize(in, com.ibm.icu.text.Normalizer.NFC);
+        }
+    }
+
+    public static final class NonNormalizer implements UnicodeNormalizer.Normalizer {
+        public String normalizeNFC(String in) {
+            return in;
+        }
+    }
+
+    public static final class AutoDetectNormalizer implements Normalizer {
+        private final Normalizer delegate;
+
+        public AutoDetectNormalizer() {
+            Normalizer candidate;
+            try {
+                Class.forName(JAVA6_NORMALIZER_CLASS);
+                candidate = new Java6ReflectionNormalizer();
+                log.info("Running on Java 6, using {} for unicode form normalization.", candidate.getClass());
+            } catch (ClassNotFoundException e) {
+                log.warn("Not running on Java 6 ({} not found). Attempting to locate the ICU4J library.", JAVA6_NORMALIZER_CLASS);
+                try {
+                    Class.forName(ICU_NORMALIZER_CLASS);
+                    candidate = new ICUNormalizer();
+                    log.info("ICU4J found, using {} for Unicode form normalization.", candidate.getClass());
+                } catch (ClassNotFoundException e2) {
+                    log.warn("ICU4J not found ({} not found), Unicode will not be 100% supported; no Unicode form normalization available. If Java 6 is not an option, you can get the ICU4J library from http://www.icu-project.org/.", ICU_NORMALIZER_CLASS);
+                    candidate = new NonNormalizer();
+                }
+            }
+            this.delegate = candidate;
+        }
+
+        public String normalizeNFC(String in) {
+            return delegate.normalizeNFC(in);
+        }
+    }
+
+}
---
+++ magnolia-core/src/test/java/info/magnolia/cms/util/UnicodeNormalizerTest.java
@@ -0,0 +1,111 @@
+/**
+ * This file Copyright (c) 2009 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.cms.util;
+
+import info.magnolia.cms.core.SystemProperty;
+import junit.framework.TestCase;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ *
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ */
+public class UnicodeNormalizerTest extends TestCase {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(UnicodeNormalizerTest.class);
+
+    private static final byte[] MACROMAN_BYTES = new byte[]{47, 121, -118, -118, -118, -118, -118};
+    private static final byte[] NFC_BYTES = new byte[]{47, 121, -61, -92, -61, -92, -61, -92, -61, -92, -61, -92};
+    private static final byte[] NFD_BYTES = new byte[]{47, 121, 97, -52, -120, 97, -52, -120, 97, -52, -120, 97, -52, -120, 97, -52, -120};
+    private static final String MACROMAN;
+    private static final String NFC;
+    private static final String NFD;
+
+    static {
+        try {
+            MACROMAN = new String(MACROMAN_BYTES, "MacRoman");
+            NFC = new String(NFC_BYTES, "UTF-8");
+            NFD = new String(NFD_BYTES, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        SystemProperty.setProperty("info.magnolia.cms.util.UnicodeNormalizer$Normalizer", "info.magnolia.cms.util.UnicodeNormalizer$AutoDetectNormalizer");
+    }
+
+    public void testAsciiStringsShouldPassThroughWithAutoDetect() {
+        assertEquals("hello", UnicodeNormalizer.normalizeNFC("hello"));
+    }
+
+    public void testNFCStringIsStillEqualsAfterNormalizeCallWithAutoDetect() {
+        assertEquals(NFC, UnicodeNormalizer.normalizeNFC(NFC));
+    }
+
+    public void testNormalizingNFDStringMakesItEqualsToNFCStringWithAutoDetect() {
+        assertEquals(NFC, UnicodeNormalizer.normalizeNFC(NFD));
+    }
+
+    public void testNormalizationAlsoWorksForStringsThatWereOriginallyNotUTF8WithAutoDetect() {
+        assertEquals(MACROMAN, NFC);
+        assertEquals(NFC, UnicodeNormalizer.normalizeNFC(MACROMAN));
+    }
+
+    public void testICUNormalizer() {
+        final UnicodeNormalizer.Normalizer n = new UnicodeNormalizer.ICUNormalizer();
+        assertEquals(NFC, n.normalizeNFC(NFD));
+        assertEquals(NFC, n.normalizeNFC(MACROMAN));
+        assertEquals("hello", n.normalizeNFC("hello"));
+    }
+
+    public void testNonNormalizer() {
+        final UnicodeNormalizer.Normalizer n = new UnicodeNormalizer.NonNormalizer();
+        assertEquals(NFD, n.normalizeNFC(NFD));
+        assertEquals(NFC, n.normalizeNFC(MACROMAN));
+        assertEquals("hello", n.normalizeNFC("hello"));
+    }
+
+    public void testJava6ReflectionNormalizer() {
+        try {
+            Class.forName("java.text.Normalizer");
+            final UnicodeNormalizer.Normalizer n = new UnicodeNormalizer.Java6ReflectionNormalizer();
+            assertEquals(NFC, n.normalizeNFC(NFD));
+            assertEquals(NFC, n.normalizeNFC(MACROMAN));
+            assertEquals("hello", n.normalizeNFC("hello"));
+        } catch (ClassNotFoundException e) {
+            log.info("This test can only be run with Java 6.");
+        }
+
+    }
+}
