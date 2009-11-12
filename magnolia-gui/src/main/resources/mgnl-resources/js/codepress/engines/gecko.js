9e65f21370bf67819155d901c00b51e4c4828a54
Federico Grilli <federico.grilli@magnolia-cms.com>
2009-11-12 14:14:57 +0000

    MAGNOLIA-2937: Replaced original implementation of snippets function with one found on http://sourceforge.net/projects/codepress/forums/forum/654507/topic/2108862 which fixes the problem.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@29540 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-gui/src/main/resources/mgnl-resources/js/codepress/engines/gecko.js
+++ magnolia-gui/src/main/resources/mgnl-resources/js/codepress/engines/gecko.js
@@ -21,131 +21,174 @@ CodePress = {
 
 	// set initial vars and start sh
 	initialize : function() {
-		if(typeof(editor)=='undefined' && !arguments[0]) return;
+		if (typeof (editor) == 'undefined' && !arguments[0])
+			return;
 		body = document.getElementsByTagName('body')[0];
-		body.innerHTML = body.innerHTML.replace(/\n/g,"");
+		body.innerHTML = body.innerHTML.replace(/\n/g, "");
 		chars = '|32|46|62|8|'; // charcodes that trigger syntax highlighting
 		cc = '\u2009'; // carret char
 		editor = document.getElementsByTagName('pre')[0];
 		document.designMode = 'on';
 		document.addEventListener('keypress', this.keyHandler, true);
-		window.addEventListener('scroll', function() { if(!CodePress.scrolling) CodePress.syntaxHighlight('scroll') }, false);
+		window.addEventListener('scroll', function() {
+			if (!CodePress.scrolling)
+				CodePress.syntaxHighlight('scroll')
+		}, false);
 		completeChars = this.getCompleteChars();
-		completeEndingChars =  this.getCompleteEndingChars();
+		completeEndingChars = this.getCompleteEndingChars();
 	},
 
 	// treat key bindings
 	keyHandler : function(evt) {
-    	keyCode = evt.keyCode;	
+		keyCode = evt.keyCode;
 		charCode = evt.charCode;
 		fromChar = String.fromCharCode(charCode);
 
-		if((evt.ctrlKey || evt.metaKey) && evt.shiftKey && charCode!=90)  { // shortcuts = ctrl||appleKey+shift+key!=z(undo) 
-			CodePress.shortcuts(charCode?charCode:keyCode);
-		}
-		else if( (completeEndingChars.indexOf('|'+fromChar+'|')!= -1 || completeChars.indexOf('|'+fromChar+'|')!=-1) && CodePress.autocomplete) { // auto complete
-			if(!CodePress.completeEnding(fromChar))
-			     CodePress.complete(fromChar);
-		}
-	    else if(chars.indexOf('|'+charCode+'|')!=-1||keyCode==13) { // syntax highlighting
-			top.setTimeout(function(){CodePress.syntaxHighlight('generic');},100);
-		}
-		else if(keyCode==9 || evt.tabKey) {  // snippets activation (tab)
+		if ((evt.ctrlKey || evt.metaKey) && evt.shiftKey && charCode != 90) { // shortcuts
+			// =
+			// ctrl||appleKey+shift+key!=z(undo)
+			CodePress.shortcuts(charCode ? charCode : keyCode);
+		} else if ((completeEndingChars.indexOf('|' + fromChar + '|') != -1 || completeChars
+				.indexOf('|' + fromChar + '|') != -1)
+				&& CodePress.autocomplete) { // auto complete
+			if (!CodePress.completeEnding(fromChar))
+				CodePress.complete(fromChar);
+		} else if (chars.indexOf('|' + charCode + '|') != -1 || keyCode == 13) { // syntax
+			// highlighting
+			top.setTimeout(function() {
+				CodePress.syntaxHighlight('generic');
+			}, 100);
+		} else if (keyCode == 9 || evt.tabKey) { // snippets activation (tab)
 			CodePress.snippets(evt);
-		}
-		else if(keyCode==46||keyCode==8) { // save to history when delete or backspace pressed
-		 	CodePress.actions.history[CodePress.actions.next()] = editor.innerHTML;
-		}
-		else if((charCode==122||charCode==121||charCode==90) && evt.ctrlKey) { // undo and redo
-			(charCode==121||evt.shiftKey) ? CodePress.actions.redo() :  CodePress.actions.undo(); 
+		} else if (keyCode == 46 || keyCode == 8) { // save to history when
+			// delete or backspace
+			// pressed
+			CodePress.actions.history[CodePress.actions.next()] = editor.innerHTML;
+		} else if ((charCode == 122 || charCode == 121 || charCode == 90)
+				&& evt.ctrlKey) { // undo and redo
+			(charCode == 121 || evt.shiftKey) ? CodePress.actions.redo()
+					: CodePress.actions.undo();
 			evt.preventDefault();
-		}
-		else if(charCode==118 && evt.ctrlKey)  { // handle paste
-		 	top.setTimeout(function(){CodePress.syntaxHighlight('generic');},100);
-		}
-		else if(charCode==99 && evt.ctrlKey)  { // handle cut
-		 	//alert(window.getSelection().getRangeAt(0).toString().replace(/\t/g,'FFF'));
+		} else if (charCode == 118 && evt.ctrlKey) { // handle paste
+			top.setTimeout(function() {
+				CodePress.syntaxHighlight('generic');
+			}, 100);
+		} else if (charCode == 99 && evt.ctrlKey) { // handle cut
+			// alert(window.getSelection().getRangeAt(0).toString().replace(/\t/g,'FFF'));
 		}
 
 	},
 
 	// put cursor back to its original position after every parsing
 	findString : function() {
-		if(self.find(cc))
+		if (self.find(cc))
 			window.getSelection().getRangeAt(0).deleteContents();
 	},
-	
+
 	// split big files, highlighting parts of it
-	split : function(code,flag) {
-		if(flag=='scroll') {
+	split : function(code, flag) {
+		if (flag == 'scroll') {
 			this.scrolling = true;
 			return code;
-		}
-		else {
+		} else {
 			this.scrolling = false;
 			mid = code.indexOf(cc);
-			if(mid-2000<0) {ini=0;end=4000;}
-			else if(mid+2000>code.length) {ini=code.length-4000;end=code.length;}
-			else {ini=mid-2000;end=mid+2000;}
-			code = code.substring(ini,end);
+			if (mid - 2000 < 0) {
+				ini = 0;
+				end = 4000;
+			} else if (mid + 2000 > code.length) {
+				ini = code.length - 4000;
+				end = code.length;
+			} else {
+				ini = mid - 2000;
+				end = mid + 2000;
+			}
+			code = code.substring(ini, end);
 			return code;
 		}
 	},
-	
+
 	getEditor : function() {
-		if(!document.getElementsByTagName('pre')[0]) {
+		if (!document.getElementsByTagName('pre')[0]) {
 			body = document.getElementsByTagName('body')[0];
-			if(!body.innerHTML) return body;
-			if(body.innerHTML=="<br>") body.innerHTML = "<pre> </pre>";
-			else body.innerHTML = "<pre>"+body.innerHTML+"</pre>";
+			if (!body.innerHTML)
+				return body;
+			if (body.innerHTML == "<br>")
+				body.innerHTML = "<pre> </pre>";
+			else
+				body.innerHTML = "<pre>" + body.innerHTML + "</pre>";
 		}
 		return document.getElementsByTagName('pre')[0];
 	},
-	
+
 	// syntax highlighting parser
 	syntaxHighlight : function(flag) {
-		//if(document.designMode=='off') document.designMode='on'
-		if(flag != 'init') { window.getSelection().getRangeAt(0).insertNode(document.createTextNode(cc));}
+		// if(document.designMode=='off') document.designMode='on'
+		if (flag != 'init') {
+			window.getSelection().getRangeAt(0).insertNode(
+					document.createTextNode(cc));
+		}
 		editor = CodePress.getEditor();
 		o = editor.innerHTML;
-		o = o.replace(/<br>/g,'\n');
-		o = o.replace(/<.*?>/g,'');
-		x = z = this.split(o,flag);
-		x = x.replace(/\n/g,'<br>');
+		o = o.replace(/<br>/g, '\n');
+		o = o.replace(/<.*?>/g, '');
+		x = z = this.split(o, flag);
+		x = x.replace(/\n/g, '<br>');
 
-		if(arguments[1]&&arguments[2]) x = x.replace(arguments[1],arguments[2]);
-	
-		for(i=0;i<Language.syntax.length;i++) 
-			x = x.replace(Language.syntax[i].input,Language.syntax[i].output);
+		if (arguments[1] && arguments[2])
+			x = x.replace(arguments[1], arguments[2]);
 
-		editor.innerHTML = this.actions.history[this.actions.next()] = (flag=='scroll') ? x : o.split(z).join(x);
-		if(flag!='init') this.findString();
+		for (i = 0; i < Language.syntax.length; i++)
+			x = x.replace(Language.syntax[i].input, Language.syntax[i].output);
+
+		editor.innerHTML = this.actions.history[this.actions.next()] = (flag == 'scroll') ? x
+				: o.split(z).join(x);
+		if (flag != 'init')
+			this.findString();
 	},
-	
+
 	getLastWord : function() {
 		var rangeAndCaret = CodePress.getRangeAndCaret();
-		words = rangeAndCaret[0].substring(rangeAndCaret[1]-40,rangeAndCaret[1]);
-		words = words.replace(/[\s\n\r\);\W]/g,'\n').split('\n');
-		return words[words.length-1].replace(/[\W]/gi,'').toLowerCase();
+		words = rangeAndCaret[0].substring(rangeAndCaret[1] - 40,
+				rangeAndCaret[1]);
+		words = words.replace(/[\s\n\r\);\W]/g, '\n').split('\n');
+		return words[words.length - 1].replace(/[\W]/gi, '').toLowerCase();
 	},
-	
+
 	snippets : function(evt) {
-		var snippets = Language.snippets;	
+		/*
+		 * On firefox 3.x TAB indentation is broken. R original implementation
+		 * with this one found on
+		 * http://sourceforge.net/projects/codepress/forums/forum/654507/topic/2108862
+		 * which fixes the problem.
+		 */
+		var snippets = Language.snippets;
 		var trigger = this.getLastWord();
-		for (var i=0; i<snippets.length; i++) {
-			if(snippets[i].input == trigger) {
-				var content = snippets[i].output.replace(/</g,'&lt;');
-				content = content.replace(/>/g,'&gt;');
-				if(content.indexOf('$0')<0) content += cc;
-				else content = content.replace(/\$0/,cc);
-				content = content.replace(/\n/g,'<br>');
-				var pattern = new RegExp(trigger+cc,'gi');
+		var tab = true;
+		for ( var i = 0; i < snippets.length; i++) {
+			if (snippets[i].input == trigger) {
+				tab = false;
+				var content = snippets[i].output.replace(/</g, '&lt;');
+				content = content.replace(/>/g, '&gt;');
+				if (content.indexOf('$0') < 0)
+					content += cc;
+				else
+					content = content.replace(/\$0/, cc);
+				content = content.replace(/\n/g, '<br>');
+				var pattern = new RegExp(trigger + cc, 'gi');
 				evt.preventDefault(); // prevent the tab key from being added
-				this.syntaxHighlight('snippets',pattern,content);
+				this.syntaxHighlight('snippets', pattern, content);
 			}
 		}
+		if (tab) {
+			var range = window.getSelection().getRangeAt(0);
+			var newTextNode = document.createTextNode('\t');
+			range.insertNode(newTextNode);
+			range.setStartAfter(newTextNode)
+			evt.preventDefault(); // prevent the normal tab action
+		}
 	},
-	
+
 	readOnly : function() {
 		document.designMode = (arguments[0]) ? 'off' : 'on';
 	},
@@ -153,103 +196,109 @@ CodePress = {
 	complete : function(trigger) {
 		window.getSelection().getRangeAt(0).deleteContents();
 		var complete = Language.complete;
-		for (var i=0; i<complete.length; i++) {
-			if(complete[i].input == trigger) {
-				var pattern = new RegExp('\\'+trigger+cc);
-				var content = complete[i].output.replace(/\$0/g,cc);
-				parent.setTimeout(function () { CodePress.syntaxHighlight('complete',pattern,content)},0); // wait for char to appear on screen
+		for ( var i = 0; i < complete.length; i++) {
+			if (complete[i].input == trigger) {
+				var pattern = new RegExp('\\' + trigger + cc);
+				var content = complete[i].output.replace(/\$0/g, cc);
+				parent.setTimeout(function() {
+					CodePress.syntaxHighlight('complete', pattern, content)
+				}, 0); // wait for char to appear on screen
 			}
 		}
 	},
 
 	getCompleteChars : function() {
 		var cChars = '';
-		for(var i=0;i<Language.complete.length;i++)
-			cChars += '|'+Language.complete[i].input;
-		return cChars+'|';
+		for ( var i = 0; i < Language.complete.length; i++)
+			cChars += '|' + Language.complete[i].input;
+		return cChars + '|';
 	},
-	
+
 	getCompleteEndingChars : function() {
 		var cChars = '';
-		for(var i=0;i<Language.complete.length;i++)
-			cChars += '|'+Language.complete[i].output.charAt(Language.complete[i].output.length-1);
-		return cChars+'|';
+		for ( var i = 0; i < Language.complete.length; i++)
+			cChars += '|' + Language.complete[i].output
+					.charAt(Language.complete[i].output.length - 1);
+		return cChars + '|';
 	},
-	
+
 	completeEnding : function(trigger) {
 		var range = window.getSelection().getRangeAt(0);
 		try {
-			range.setEnd(range.endContainer, range.endOffset+1)
-		}
-		catch(e) {
+			range.setEnd(range.endContainer, range.endOffset + 1)
+		} catch (e) {
 			return false;
 		}
 		var next_character = range.toString()
-		range.setEnd(range.endContainer, range.endOffset-1)
-		if(next_character != trigger) return false;
+		range.setEnd(range.endContainer, range.endOffset - 1)
+		if (next_character != trigger)
+			return false;
 		else {
-			range.setEnd(range.endContainer, range.endOffset+1)
+			range.setEnd(range.endContainer, range.endOffset + 1)
 			range.deleteContents();
 			return true;
 		}
 	},
-	
+
 	shortcuts : function() {
 		var cCode = arguments[0];
-		if(cCode==13) cCode = '[enter]';
-		else if(cCode==32) cCode = '[space]';
-		else cCode = '['+String.fromCharCode(charCode).toLowerCase()+']';
-		for(var i=0;i<Language.shortcuts.length;i++)
-			if(Language.shortcuts[i].input == cCode)
-				this.insertCode(Language.shortcuts[i].output,false);
+		if (cCode == 13)
+			cCode = '[enter]';
+		else if (cCode == 32)
+			cCode = '[space]';
+		else
+			cCode = '[' + String.fromCharCode(charCode).toLowerCase() + ']';
+		for ( var i = 0; i < Language.shortcuts.length; i++)
+			if (Language.shortcuts[i].input == cCode)
+				this.insertCode(Language.shortcuts[i].output, false);
 	},
-	
-	getRangeAndCaret : function() {	
+
+	getRangeAndCaret : function() {
 		var range = window.getSelection().getRangeAt(0);
 		var range2 = range.cloneRange();
-		var node = range.endContainer;			
+		var node = range.endContainer;
 		var caret = range.endOffset;
-		range2.selectNode(node);	
-		return [range2.toString(),caret];
+		range2.selectNode(node);
+		return [ range2.toString(), caret ];
 	},
-	
-	insertCode : function(code,replaceCursorBefore) {
+
+	insertCode : function(code, replaceCursorBefore) {
 		var range = window.getSelection().getRangeAt(0);
 		var node = window.document.createTextNode(code);
 		var selct = window.getSelection();
 		var range2 = range.cloneRange();
 		// Insert text at cursor position
-		selct.removeAllRanges();
-		range.deleteContents();
-		range.insertNode(node);
-		// Move the cursor to the end of text
-		range2.selectNode(node);		
-		range2.collapse(replaceCursorBefore);
-		selct.removeAllRanges();
-		selct.addRange(range2);
-	},
-	
+	selct.removeAllRanges();
+	range.deleteContents();
+	range.insertNode(node);
+	// Move the cursor to the end of text
+	range2.selectNode(node);
+	range2.collapse(replaceCursorBefore);
+	selct.removeAllRanges();
+	selct.addRange(range2);
+},
+
 	// get code from editor
 	getCode : function() {
-		if(!document.getElementsByTagName('pre')[0] || editor.innerHTML == '')
+		if (!document.getElementsByTagName('pre')[0] || editor.innerHTML == '')
 			editor = CodePress.getEditor();
 		var code = editor.innerHTML;
-		code = code.replace(/<br>/g,'\n');
-		code = code.replace(/\u2009/g,'');
-		code = code.replace(/<.*?>/g,'');
-		code = code.replace(/&lt;/g,'<');
-		code = code.replace(/&gt;/g,'>');
-		code = code.replace(/&amp;/gi,'&');
+		code = code.replace(/<br>/g, '\n');
+		code = code.replace(/\u2009/g, '');
+		code = code.replace(/<.*?>/g, '');
+		code = code.replace(/&lt;/g, '<');
+		code = code.replace(/&gt;/g, '>');
+		code = code.replace(/&amp;/gi, '&');
 		return code;
 	},
 
 	// put code inside editor
 	setCode : function() {
 		var code = arguments[0];
-		code = code.replace(/\u2009/gi,'');
-		code = code.replace(/&/gi,'&amp;');
-		code = code.replace(/</g,'&lt;');
-		code = code.replace(/>/g,'&gt;');
+		code = code.replace(/\u2009/gi, '');
+		code = code.replace(/&/gi, '&amp;');
+		code = code.replace(/</g, '&lt;');
+		code = code.replace(/>/g, '&gt;');
 		editor.innerHTML = code;
 		if (code == '')
 			document.getElementsByTagName('body')[0].innerHTML = '';
@@ -259,35 +308,42 @@ CodePress = {
 	actions : {
 		pos : -1, // actual history position
 		history : [], // history vector
-		
+
 		undo : function() {
 			editor = CodePress.getEditor();
-			if(editor.innerHTML.indexOf(cc)==-1){
-				if(editor.innerHTML != " ")
-					window.getSelection().getRangeAt(0).insertNode(document.createTextNode(cc));
+			if (editor.innerHTML.indexOf(cc) == -1) {
+				if (editor.innerHTML != " ")
+					window.getSelection().getRangeAt(0).insertNode(
+							document.createTextNode(cc));
 				this.history[this.pos] = editor.innerHTML;
 			}
-			this.pos --;
-			if(typeof(this.history[this.pos])=='undefined') this.pos ++;
+			this.pos--;
+			if (typeof (this.history[this.pos]) == 'undefined')
+				this.pos++;
 			editor.innerHTML = this.history[this.pos];
-			if(editor.innerHTML.indexOf(cc)>-1) editor.innerHTML+=cc;
+			if (editor.innerHTML.indexOf(cc) > -1)
+				editor.innerHTML += cc;
 			CodePress.findString();
 		},
-		
+
 		redo : function() {
 			// editor = CodePress.getEditor();
-			this.pos++;
-			if(typeof(this.history[this.pos])=='undefined') this.pos--;
-			editor.innerHTML = this.history[this.pos];
-			CodePress.findString();
-		},
-		
-		next : function() { // get next vector position and clean old ones
-			if(this.pos>20) this.history[this.pos-21] = undefined;
+		this.pos++;
+		if (typeof (this.history[this.pos]) == 'undefined')
+			this.pos--;
+		editor.innerHTML = this.history[this.pos];
+		CodePress.findString();
+	},
+
+	next : function() { // get next vector position and clean old ones
+			if (this.pos > 20)
+				this.history[this.pos - 21] = undefined;
 			return ++this.pos;
 		}
 	}
 }
 
-Language={};
-window.addEventListener('load', function() { CodePress.initialize('new'); }, true);
+Language = {};
+window.addEventListener('load', function() {
+	CodePress.initialize('new');
+}, true);
--- magnolia-gui/src/main/resources/mgnl-resources/js/codepress/readme.txt
+++ magnolia-gui/src/main/resources/mgnl-resources/js/codepress/readme.txt
@@ -1,3 +1,6 @@
 This CodePress distribution has been patched to support chrome+safari. 
 See http://sourceforge.net/tracker/index.php?func=detail&aid=2209244&group_id=186981&atid=919469
-It also disables Opera support (falls back to plain textarea) which is problematic at least in version 10. 
+It also disables Opera support (falls back to plain textarea) which is problematic at least in version 10.
+On firefox 3.x TAB indentation is broken. Replaced original implementation of snippets function
+with one found on http://sourceforge.net/projects/codepress/forums/forum/654507/topic/2108862
+which fixes the problem. 
