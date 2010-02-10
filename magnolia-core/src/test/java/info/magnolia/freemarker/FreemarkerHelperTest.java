2568b17b7cb85ce4ed27fd22a3d4233b42abf82f
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2010-02-10 17:31:39 +0000

    comment
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@31747 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/test/java/info/magnolia/freemarker/FreemarkerHelperTest.java
+++ magnolia-core/src/test/java/info/magnolia/freemarker/FreemarkerHelperTest.java
@@ -603,7 +603,7 @@ public class FreemarkerHelperTest extends AbstractFreemarkerTestCase {
         return hm;
     }
 
-    // we use this method, since FreemarkerHelp needs to add objects to our root map, Collections.singletonMap won't do.
+    // we use this method, since FreemarkerHelper adds objects to our root map (Collections.singletonMap returns an immutable map).
     private Map createSingleValueMap(Object key, Object value) {
         final HashMap map = new HashMap();
         map.put(key, value);
