5bf7973741742c4d74735b4c38f4147d6c87c067
Jan Haderka <jan.haderka@magnolia-cms.com>
2010-03-30 12:44:59 +0000

    MAGNOLIA-3167 Initial test to expose some scenarios known to cause issues.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@33366 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-module-cache/src/test/java/info/magnolia/module/cache/ehcache/EhCacheFactoryTest.java
@@ -0,0 +1,228 @@
+/**
+ * This file Copyright (c) 2010 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.module.cache.ehcache;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.sf.ehcache.config.CacheConfiguration;
+
+import info.magnolia.cms.core.SystemProperty;
+import info.magnolia.module.cache.Cache;
+import junit.framework.TestCase;
+
+/**
+ * Test to assert whether there are still any issues with cache failing to un-block on access from multiple threads.
+ * @author had
+ * @version $Id:$
+ */
+public class EhCacheFactoryTest extends TestCase {
+
+    private static final Logger log = LoggerFactory.getLogger(EhCacheFactoryTest.class);
+    private EhCacheFactory factory;
+
+
+    @Override
+    public void setUp() throws Exception {
+        SystemProperty.setProperty(SystemProperty.MAGNOLIA_CACHE_STARTDIR, "cacheTest");
+        SystemProperty.setProperty(SystemProperty.MAGNOLIA_APP_ROOTDIR, ".");
+
+        factory = new EhCacheFactory();
+        // init with ehCache 1.5
+        CacheConfiguration config = new CacheConfiguration();
+        // init with ehCache 2.0
+        //CacheConfiguration config = new CacheConfiguration("test", 0);
+        config.setDiskPersistent(false);
+        config.setOverflowToDisk(false);
+        config.setTimeToIdleSeconds(0);
+        config.setTimeToLiveSeconds(0);
+        // comment out the line below and blocking test will fail with timeout exception (as of ehCache 1.5, works fine on 2.0)
+        // ... if you don't believe it will block forever feel free to increase the timeout
+        config.setMaxElementsInMemory(1);
+        factory.setDefaultCacheConfiguration(config);
+        factory.start();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+        // TODO: clear the SystemProperty ... oops, we do not expose remove() and clear() ops
+    }
+
+    /**
+     * Simple test for serial access to the cache
+     */
+    public void testSerialAccess() throws Exception {
+        final Cache ehCache = factory.getCache("test");
+        assertNull(ehCache.get("foo"));
+        ehCache.put("foo", "xxx");
+        assertEquals("xxx", ehCache.get("foo"));
+    }
+
+    /**
+     * Simple test of item eviction
+     */
+    public void testAddMoreThenSize() throws Exception {
+        // make sure there's only one item allowed
+        assertEquals(1, factory.getDefaultCacheConfiguration().getMaxElementsInMemory());
+
+        final Cache ehCache = factory.getCache("test");
+        assertNull(ehCache.get("foo"));
+        // add first
+        ehCache.put("foo", "xxx");
+        // and check first is there
+        assertEquals("xxx", ehCache.get("foo"));
+        // add second (and evict first
+        ehCache.put("boo", "xxx");
+        // first is gone
+        assertEquals(null, ehCache.get("foo"));
+        // second is there
+        assertEquals("xxx", ehCache.get("boo"));
+
+    }
+
+    /**
+     * Ensure that cache unblocks all threads waiting for item to be cached.
+     */
+    public void testBlocking() throws Exception {
+        final Cache ehCache = factory.getCache("test");
+        // 1st call
+        Object entry = ehCache.get("blah");
+        assertNull(entry);
+        Executor ex = Executors.newFixedThreadPool(2);
+
+        // 2nd call
+        FutureTask<Object> task = new FutureTask<Object>(new Callable<Object>() {
+            public Object call() throws Exception {
+                log.debug("2nd get called");
+                Object res = ehCache.get("blah");
+                log.debug("2nd unblocked");
+                return res;
+            }});
+        ex.execute(task);
+
+        // 3rd call
+        FutureTask<Object> task2 = new FutureTask<Object>(new Callable<Object>() {
+            public Object call() throws Exception {
+                log.debug("3rd get called");
+                Object res = ehCache.get("blah");
+                log.debug("3rd unblocked");
+                return res;
+            }});
+        ex.execute(task2);
+
+        log.debug("Put");
+        // add something in the main thread
+        ehCache.put("blah", "boo");
+        // try to read what you just put in (if the cache is misconfigured, you get the first block here, and since that would block whole test we skip it and rely on block to be exercised also in the tasks that will time out without blocking whole execution
+        //assertEquals("boo", ehCache.get("blah"));
+
+        log.debug("verify");
+        // thread2
+        Object result = task.get(2, TimeUnit.SECONDS);
+        log.debug("" + result);
+
+        // thread3
+        Object result2 = task2.get(2, TimeUnit.SECONDS);
+        log.debug("" + result2);
+
+        assertNotNull(result);
+        assertNotNull(result2);
+
+    }
+
+    /**
+     * Ensure cache unblocks and returns proper item to all the threads waiting for the item even if such is soon after evicted from the cache.
+     */
+    public void testBlockingMoreThenMaxSize() throws Exception {
+        // make sure there's only one item allowed
+        assertEquals(1, factory.getDefaultCacheConfiguration().getMaxElementsInMemory());
+
+        final Cache ehCache = factory.getCache("test");
+        // 1st call
+        Object entry = ehCache.get("blah");
+        assertNull(entry);
+        Executor ex = Executors.newFixedThreadPool(2);
+
+        // 2nd call
+        FutureTask<Object> task = new FutureTask<Object>(new Callable<Object>() {
+
+            public Object call() throws Exception {
+                log.debug("2nd get called");
+                Object res = ehCache.get("blah");
+                log.debug("2nd unblocked");
+                return res;
+            }});
+        ex.execute(task);
+        // 3rd call
+        FutureTask<Object> task2 = new FutureTask<Object>(new Callable<Object>() {
+
+            public Object call() throws Exception {
+                log.debug("3rd get called");
+                Object res = ehCache.get("blah");
+                log.debug("3rd unblocked");
+                return res;
+            }});
+        ex.execute(task2);
+
+        log.debug("Put");
+        // add something in the main thread
+        ehCache.put("blah", "boo");
+        // try to read what you just put in (if the cache is misconfigured, you get the first block here, and since that would block whole test we skip it and rely on block to be exercised also in the tasks that will time out without blocking whole execution
+        //assertEquals("boo", ehCache.get("blah"));
+        // put new item to evict old
+        ehCache.put("foo", "xxx");
+        // try to read evicted item:
+        entry = ehCache.get("blah");
+        assertNull(entry);
+
+        log.debug("verify");
+        // thread2
+        Object result = task.get(2, TimeUnit.SECONDS);
+        log.debug("" + result);
+
+        // thread3
+        Object result2 = task2.get(2, TimeUnit.SECONDS);
+        log.debug("" + result2);
+        assertNotNull(result);
+        assertNotNull(result2);
+
+    }
+}
