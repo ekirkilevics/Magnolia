24c19802fb765981dab8a0ba4c079adc744bacd8
Federico Grilli <federico.grilli@magnolia-cms.com>
2010-05-21 14:04:25 +0000

    MAGNOLIA-3210: implementation and tests
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@34612 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/cms/i18n/HierarchyBasedI18nContentSupport.java
@@ -0,0 +1,132 @@
+/**
+ * This file Copyright (c) 2003-2010 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.cms.i18n;
+
+import info.magnolia.cms.core.Content;
+import info.magnolia.cms.core.NodeData;
+import info.magnolia.context.MgnlContext;
+
+import java.util.Locale;
+
+import javax.jcr.RepositoryException;
+
+import org.apache.commons.lang.LocaleUtils;
+
+/**
+ * Serves i18n content found in a per locale hierarchy structure.
+ * E.g.
+ * <pre>
+ * +my-website
+ *  + <strong>en</strong>
+ *   + page-1
+ *   + page-2
+ *   + page-n
+ *  + <strong>de</strong>
+ *   + page-1
+ *   + page-2
+ *   + page-n
+ *  + <strong>de_CH</strong>
+ *   + page-1
+ *   + page-2
+ *   + page-n
+ *  + <strong>it</strong>
+ *   + page-1
+ *   + page-2
+ *   + page-n
+ *</pre>
+ * The locale is inferred by analyzing the URI and checking whether it contains a valid Java locale code (for a definition of valid <a href="http://java.sun.com/j2se/1.5.0/docs/api/java/util/Locale.html">Java locale</a> code).
+ * This code can be at whatever position in the URI, not necessarily the first one. For example, given the following URI <em>/my-website/node-1/node-2/de/home-page.html</em>, the locale <em>de</em>
+ * will be detected and content served from here:
+ * <pre>
+ * + my-website
+ *  + node-1
+ *   + node-2
+ *    + <strong>de</strong>
+ *     + home-page
+ * </pre>
+ * If no locale is found in the URI, the default one is assumed.
+ * @author fgrilli
+ *
+ */
+public class HierarchyBasedI18nContentSupport extends AbstractI18nContentSupport {
+    @Override
+    protected Locale onDetermineLocale() {
+        Locale locale = null;
+        Locale validUnsupportedLocale = null;
+        final String i18nURI = MgnlContext.getAggregationState().getCurrentURI();
+        log.debug("URI to check for locales is {}", i18nURI);
+        final String[] splitURI = i18nURI.split("/");
+        for(String uriToken: splitURI){
+            locale = determineLocalFromString(uriToken);
+            if(LocaleUtils.isAvailableLocale(locale)){
+                log.debug("found a valid Locale code {}", uriToken);
+                if(isLocaleSupported(locale)){
+                    break;
+                } else {
+                    //the URI contains a valid Locale code but it is not supported by the current I18n configuration.
+                    //We store it anyway and eventually return it if no exact match will be found at the end of this loop.
+                    validUnsupportedLocale = locale;
+                }
+            }
+            locale = null;
+        }
+        return locale != null ? locale : validUnsupportedLocale;
+    }
+
+    /*
+     * Just return the uri, no need to add the locale to it
+     *  (non-Javadoc)
+     * @see info.magnolia.cms.i18n.AbstractI18nContentSupport#toI18NURI(java.lang.String, java.util.Locale)
+     */
+    @Override
+    protected String toI18NURI(String uri, Locale locale) {
+        return uri;
+    }
+
+    /*
+     * Just return the uri, no need to remove the locale from it
+     *  (non-Javadoc)
+     * @see info.magnolia.cms.i18n.AbstractI18nContentSupport#toRawURI(java.lang.String, java.util.Locale)
+     */
+    @Override
+    protected String toRawURI(String i18nURI, Locale locale) {
+        return i18nURI;
+    }
+
+    public NodeData getNodeData(Content node, String name, Locale locale) throws RepositoryException {
+        if (node.hasNodeData(name) && isLocaleSupported(locale)) {
+            return node.getNodeData(name);
+        }
+        return null;
+    }
+}
--- magnolia-core/src/test/java/info/magnolia/cms/i18n/HierarchyBasedI18nContentSupportTest.java
+++ magnolia-core/src/test/java/info/magnolia/cms/i18n/HierarchyBasedI18nContentSupportTest.java
@@ -15,7 +15,6 @@
  * or MNA you select, is prohibited.
  *
  * 1. For the GPL license (GPL), you can redistribute and/or
- * modify this file under the terms of the GNU General
  * Public License, Version 3, as published by the Free Software
  * Foundation.  You should have received a copy of the GNU
  * General Public License, Version 3 along with this program;
@@ -33,41 +32,20 @@
  */
 package info.magnolia.cms.i18n;
 
-import static org.easymock.EasyMock.expect;
-import static org.easymock.classextension.EasyMock.createMock;
-import static org.easymock.classextension.EasyMock.replay;
-import info.magnolia.cms.core.AggregationState;
 import info.magnolia.cms.core.NodeData;
-import info.magnolia.context.MgnlContext;
-import info.magnolia.context.WebContext;
-import info.magnolia.test.MgnlTestCase;
 import info.magnolia.test.mock.MockContent;
 import info.magnolia.test.mock.MockHierarchyManager;
 
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 /**
- * @author philipp
- * @version $Id$
+ * @author fgrilli
  *
  */
-public class DefaultI18NContentSupportTest extends MgnlTestCase {
-
-    /**
-     *
-     */
-    protected static final Locale DEFAULT_LOCALE = new Locale("en");
-    /**
-     * Logger.
-     */
-    protected static Logger log = LoggerFactory.getLogger(DefaultI18NContentSupportTest.class);
-
-    public void testDetermineLocale(){
-        DefaultI18nContentSupport defSupport = new DefaultI18nContentSupport();
+public class HierarchyBasedI18nContentSupportTest extends DefaultI18NContentSupportTest {
+	public void testDetermineLocale(){
+		HierarchyBasedI18nContentSupport defSupport = new HierarchyBasedI18nContentSupport();
         defSupport.setFallbackLocale(DEFAULT_LOCALE);
         defSupport.addLocale(new LocaleDefinition("de", null, true));
         defSupport.addLocale(new LocaleDefinition("de", "CH", true));
@@ -78,27 +56,35 @@ public class DefaultI18NContentSupportTest extends MgnlTestCase {
         Locale  locale = defSupport.determineLocale();
         assertEquals(DEFAULT_LOCALE, locale);
 
-        setCurrentURI("/de/home.html");
+        setCurrentURI("/de/foo/bar/home.html");
         locale = defSupport.determineLocale();
         assertEquals(new Locale("de"), locale);
 
-        setCurrentURI("/de_ch/home.html");
+        setCurrentURI("/foo/bar/de/home.html");
+        locale = defSupport.determineLocale();
+        assertEquals(new Locale("de"), locale);
+
+        setCurrentURI("/de_ch/foo/bar/home.html");
+        locale = defSupport.determineLocale();
+        assertEquals(new Locale("de", "ch"), locale);
+
+        setCurrentURI("/foo/de_ch/bar/home.html");
         locale = defSupport.determineLocale();
         assertEquals(new Locale("de", "ch"), locale);
 
         // not supported language
-        setCurrentURI("/fr/home.html");
+        setCurrentURI("/fr/foo/bar/home.html");
         locale = defSupport.determineLocale();
         assertEquals(DEFAULT_LOCALE, locale);
 
         // disabled language
-        setCurrentURI("/it/home.html");
+        setCurrentURI("/it/foo/bar/home.html");
         locale = defSupport.determineLocale();
         assertEquals(DEFAULT_LOCALE, locale);
     }
 
     public void testDetermineLocaleEndlessLoop () {
-        DefaultI18nContentSupport defSupport = new DefaultI18nContentSupport();
+    	HierarchyBasedI18nContentSupport defSupport = new HierarchyBasedI18nContentSupport();
         defSupport.setFallbackLocale(DEFAULT_LOCALE);
         defSupport.addLocale(new LocaleDefinition("de", "CH", true));
         defSupport.addLocale(new LocaleDefinition("it", null, true));
@@ -109,38 +95,38 @@ public class DefaultI18NContentSupportTest extends MgnlTestCase {
         assertEquals(DEFAULT_LOCALE, locale);
 
         // exact match on the lang/country
-        setCurrentURI("/de_ch/home.html");
+        setCurrentURI("/foo/de_ch/bar/home.html");
         locale = defSupport.determineLocale();
         assertEquals(new Locale("de", "ch"), locale);
 
         // supported lang, but not country - should default to the same lang but with diff country
-        setCurrentURI("/de_at/home.html");
+        setCurrentURI("/de_at/foo/bar/home.html");
         locale = defSupport.determineLocale();
         assertEquals(new Locale("de", "ch"), locale);
 
         // supported lang, but not w/o country code - should default to the same lang but with different country code
-        setCurrentURI("/de/home.html");
+        setCurrentURI("/de/foo/bar/home.html");
         locale = defSupport.determineLocale();
         assertEquals(new Locale("de", "ch"), locale);
 
         // not supported language
-        setCurrentURI("/fr/home.html");
+        setCurrentURI("/fr/foo/bar/home.html");
         locale = defSupport.determineLocale();
         assertEquals(DEFAULT_LOCALE, locale);
 
-        setCurrentURI("/it/home.html");
+        setCurrentURI("/it/foo/bar/home.html");
         locale = defSupport.determineLocale();
         assertEquals(new Locale("it"), locale);
 
     }
 
     public void testGetNodeDataEndlessLoop() throws Exception {
-        DefaultI18nContentSupport defSupport = new DefaultI18nContentSupport();
+    	HierarchyBasedI18nContentSupport defSupport = new HierarchyBasedI18nContentSupport();
         defSupport.setEnabled(true);
         defSupport.setFallbackLocale(DEFAULT_LOCALE);
         defSupport.addLocale(new LocaleDefinition("de", "CH", true));
         defSupport.addLocale(new LocaleDefinition("it", null, false));
-        MockContent content = new MockContent("boo");
+        MockContent content = new MockContent("/foo/de/bar/");
         content.setHierarchyManager(new MockHierarchyManager());
 
         // no language
@@ -155,7 +141,9 @@ public class DefaultI18NContentSupportTest extends MgnlTestCase {
         assertEquals(defaultFoo, localized);
 
         // exact match on the lang/country
-        NodeData swissBlah = content.setNodeData("blah_de_CH", "val_de_ch_blah");
+        content = new MockContent("/de_CH/foo/bar/");
+        content.setHierarchyManager(new MockHierarchyManager());
+        NodeData swissBlah = content.setNodeData("blah", "val_de_ch_blah");
         defSupport.setLocale(new Locale("de", "CH"));
         localized = defSupport.getNodeData(content, "blah");
         assertEquals(swissBlah, localized);
@@ -170,24 +158,20 @@ public class DefaultI18NContentSupportTest extends MgnlTestCase {
         localized = defSupport.getNodeData(content, "blah");
         assertEquals(swissBlah, localized);
 
+        //TODO what we should do when a Locale is not supported or disabled but the language tree exists anyway in the content repo?
+        //return null? return the fallback locale content? throw an exception?
+        /*
         // not supported language
+        content = new MockContent("/fr/foo/bar/");
+        content.setHierarchyManager(new MockHierarchyManager());
         defSupport.setLocale(new Locale("fr"));
+        NodeData frenchBlah = content.setNodeData("blah", "val_fr_blah");
         localized = defSupport.getNodeData(content, "blah");
-        assertEquals(defaultblah, localized);
-
+        assertNull("Expected null, got " + localized + " instead", localized);
         // disabled language
         defSupport.setLocale(new Locale("it"));
         localized = defSupport.getNodeData(content, "blah");
-        assertEquals(defaultblah, localized);
-
+        assrtEquals(defaultblah, localized);*/
     }
 
-    protected void setCurrentURI(String uri) {
-        WebContext ctx = createMock(WebContext.class);
-        AggregationState state = createMock(AggregationState.class);
-        expect(state.getCurrentURI()).andReturn(uri);
-        expect(ctx.getAggregationState()).andReturn(state);
-        replay(ctx, state);
-        MgnlContext.setInstance(ctx);
-    }
 }
