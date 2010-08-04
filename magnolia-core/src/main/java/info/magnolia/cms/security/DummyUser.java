bb85f17b478a175b6450141be15b036a175bca1b
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2010-08-04 14:29:24 +0000

    javadoc
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/branches/magnolia-4.3@36204 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/cms/security/DummyUser.java
+++ magnolia-core/src/main/java/info/magnolia/cms/security/DummyUser.java
@@ -43,49 +43,38 @@ import org.slf4j.LoggerFactory;
 
 
 /**
- * Date: Oct 26, 2005 Time: 5:02:46 PM
+ * Dummy user implementation returned when an actual user instance can't be instantiated.
+ *
  * @author Sameer Charles
  * @version $Revision:9391 $ ($Author:scharles $)
  */
 public class DummyUser extends AbstractUser {
 
-    /**
-     * Logger
-     */
-    private static Logger log = LoggerFactory.getLogger(DummyUser.class);
+    private static final Logger log = LoggerFactory.getLogger(DummyUser.class);
 
-    /**
-     * Default language
-     */
     private static final String DEFAULT_LANGUAGE = "en";
 
-    /**
-     * package private constructor
-     */
     DummyUser() {
         log.info("Initializing dummy user - Anonymous");
         log.info("This area and/or instance is not secured");
     }
 
     /**
-     * has full access
-     * @param roleName the name of the role
-     * @return true if in role
+     * Dummy user has full access, always returns true.
      */
     public boolean hasRole(String roleName) {
         return true;
     }
 
     /**
-     * Simply log that its a dummy user
-     * @param roleName
+     * Simply log that its a dummy user.
      */
     public void removeRole(String roleName) throws UnsupportedOperationException {
         log.debug("User [ Anonymous ] has no roles");
     }
 
     /**
-     * Simply log that its a dummy user
+     * Simply log that its a dummy user.
      * @param roleName the name of the role
      */
     public void addRole(String roleName) throws UnsupportedOperationException {
@@ -93,26 +82,16 @@ public class DummyUser extends AbstractUser {
     }
 
     /**
-     * Is this user in a specified group?
-     * @param groupName
-     * @return true if in group
+     * Always returns true.
      */
     public boolean inGroup(String groupName) {
         return true;
     }
 
-    /**
-     * Remove a group. Implementation is optional
-     * @param groupName
-     */
     public void removeGroup(String groupName) throws UnsupportedOperationException {
         log.debug("User [ Anonymous ] has no groups");
     }
 
-    /**
-     * Adds this user to a group. Implementation is optional
-     * @param groupName
-     */
     public void addGroup(String groupName) throws UnsupportedOperationException {
         log.debug("No groups can be attached to user [ Anonymous ]");
     }
@@ -124,19 +103,10 @@ public class DummyUser extends AbstractUser {
     public void setEnabled(boolean enabled) {
     }
 
-    /**
-     * get user language
-     * @return language string
-     */
-
     public String getLanguage() {
         return DEFAULT_LANGUAGE;
     }
 
-    /**
-     * get user name
-     * @return name string
-     */
     public String getName() {
         return UserManager.ANONYMOUS_USER;
     }
@@ -153,9 +123,6 @@ public class DummyUser extends AbstractUser {
         log.debug("Can not set properties on dummy user (name: {}, value: {})", propertyName, value);
     }
 
-    /**
-     * @see info.magnolia.cms.security.User#getGroups()
-     */
     public Collection<String> getGroups() {
         return Collections.emptyList();
     }
@@ -164,9 +131,6 @@ public class DummyUser extends AbstractUser {
         return new ArrayList<String>();
     }
 
-    /**
-     * @see info.magnolia.cms.security.User#getRoles()
-     */
     public Collection<String> getRoles() {
         return Collections.emptyList();
     }
