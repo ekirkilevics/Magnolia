7008f23de556efbf8ba1908932fc293fbefe17df
Federico Grilli <federico.grilli@magnolia-cms.com>
2010-08-12 16:43:34 +0000

    refactoring: new MenuItem class extending Button widget. MenuItem(s) now have icons now retrieved from the jcr config.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@36372 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-admincentral-vaadin/src/main/java/info/magnolia/module/admincentral/Navigation.java
+++ magnolia-module-admincentral-vaadin/src/main/java/info/magnolia/module/admincentral/Navigation.java
@@ -11,10 +11,15 @@ import java.util.Iterator;
 
 import javax.jcr.RepositoryException;
 
+import com.vaadin.terminal.ClassResource;
+import com.vaadin.ui.AbstractComponent;
 import com.vaadin.ui.Accordion;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
+import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.TabSheet.SelectedTabChangeEvent;
+import com.vaadin.ui.TabSheet.Tab;
 import com.vaadin.ui.themes.BaseTheme;
 
 
@@ -27,13 +32,21 @@ import com.vaadin.ui.themes.BaseTheme;
 public class Navigation extends Accordion{
 
     private static final long serialVersionUID = 1L;
+    private Content node = null;
 
     /**
      * @param path the path to the menu
      */
     public Navigation(final String path) throws RepositoryException{
         // get it with system permission
-        final Content node = MgnlContext.getSystemContext().getHierarchyManager(ContentRepository.CONFIG).getContent(path);
+        node = MgnlContext.getSystemContext().getHierarchyManager(ContentRepository.CONFIG).getContent(path);
+    }
+    /**
+     * See {@link AbstractComponent#getApplication()} javadoc as to why we need to do most of the initialization here and not in the constructor.
+     */
+    @Override
+    public void attach() {
+        super.attach();
         // loop over the menu items
         for (Iterator<Content> iter = node.getChildren(ItemType.CONTENTNODE).iterator(); iter.hasNext();) {
             Content menuItem = iter.next();
@@ -41,39 +54,67 @@ public class Navigation extends Accordion{
             panel.setStyleName(BaseTheme.PANEL_LIGHT);
             // check permission
             if (isMenuItemRenderable(menuItem)) {
-                // sub menu items (2 level only)
+                // sub menu items (2 levels only)
                 for (Iterator<Content> iterator = menuItem.getChildren(ItemType.CONTENTNODE).iterator(); iterator.hasNext();) {
                     Content sub = iterator.next();
                     if (isMenuItemRenderable(sub)) {
-                        Button button = new Button(getLabel(sub));
-                        button.setStyleName(BaseTheme.BUTTON_LINK);
-                        button.setHeight(30f, Button.UNITS_PIXELS);
-                        panel.addComponent(button);
+                        panel.addComponent(new MenuItem(sub));
                     }
                 }
             }
             if(panel.getComponentIterator().hasNext()){
-                addTab(panel, getLabel(menuItem), null);
+                addTab(panel, getLabel(menuItem), new ClassResource(getIconPath(menuItem), getApplication()));
             } else {
                 //TODO empty tabs (e.g. website) should not be openable(sp?) but I could not figure out a way to do it with the Tab API so far
-                addTab(new Label(), getLabel(menuItem), null);
+                addTab(new Label(), getLabel(menuItem), new ClassResource(getIconPath(menuItem), getApplication()));
             }
         }
-    }
-
-    /**
-     * @param mp
-     * @return
-     */
-    protected String getLabel(Content mp) {
-        return NodeDataUtil.getI18NString(mp, "label");
+        //TODO for testing only. To be removed.
+        addTab(new Label("For testing dialogs"), "Dialogs", null);
     }
 
     /**
      * @param menuItem
      * @return
      */
+    protected String getLabel(Content menuItem) {
+        return NodeDataUtil.getI18NString(menuItem, "label");
+    }
+
+    protected String getIconPath(Content menuItem){
+        String iconPath = NodeDataUtil.getString(menuItem, "icon").trim();
+        return iconPath.replaceFirst(".resources/", "mgnl-resources/");
+    }
+
+    /**
+     * @param menuItem
+     * @return <code>true</code> if the the current user is granted access to this menu item, <code>false</code> otherwise
+     */
     protected boolean isMenuItemRenderable(Content menuItem) {
         return MgnlContext.getAccessManager(ContentRepository.CONFIG).isGranted(menuItem.getHandle(), Permission.READ);
     }
+    //TODO extract this as a top level class?
+    public class MenuItem extends Button{
+        private static final long serialVersionUID = 1L;
+        private Content content;
+
+        public MenuItem(final Content content) {
+            this.content = content;
+        }
+
+        /**
+         * See {@link AbstractComponent#getApplication()} javadoc as to why we need to do most of the initialization here and not in the constructor.
+         */
+        @Override
+        public void attach() {
+            super.attach();
+            setCaption(getLabel(content));
+            setStyleName(BaseTheme.BUTTON_LINK);
+            setHeight(30f, Button.UNITS_PIXELS);
+            setIcon(new ClassResource(getIconPath(content), getApplication()));
+            String onClickAction = NodeDataUtil.getString(content, "onclick").trim();
+        }
+    }
 }
+
+
