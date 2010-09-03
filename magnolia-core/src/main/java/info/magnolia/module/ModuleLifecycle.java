e859cf2ec72c64cd440bd905453fb1194d17f514
Jan Haderka <jan.haderka@magnolia-cms.com>
2010-09-03 12:55:55 +0000

    And javadoc again ... svn diff is playing funny games it seems.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@36930 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/module/ModuleLifecycle.java
+++ magnolia-core/src/main/java/info/magnolia/module/ModuleLifecycle.java
@@ -34,7 +34,7 @@
 package info.magnolia.module;
 
 /**
- * Manages the  of an installed instance of a Magnolia Module. Implementing this interface is optional.
+ * Manages the lifecycle of an installed instance of a Magnolia Module. Implementing this interface is optional.
  * @author gjoseph
  * @version $Revision: $ ($Author: $)
  */
