7e04be3d9f1277994db2ea20b10fe45dec45acde
Jan Haderka <jan.haderka@magnolia-cms.com>
2010-10-08 06:58:10 +0000

    MAGNOLIA-3261 refresh sessions manualy on updates to ensure visibility of data in case it is not propagated by the repo automatically on read but only on write
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/branches/magnolia-4.4@37815 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/context/SystemRepositoryStrategy.java
+++ magnolia-core/src/main/java/info/magnolia/context/SystemRepositoryStrategy.java
@@ -33,21 +33,41 @@
  */
 package info.magnolia.context;
 
+import info.magnolia.cms.core.HierarchyManager;
 import info.magnolia.cms.security.AccessManager;
 import info.magnolia.cms.security.Permission;
 import info.magnolia.cms.security.PermissionImpl;
 import info.magnolia.cms.security.SystemUserManager;
+import info.magnolia.cms.util.ObservationUtil;
 import info.magnolia.cms.util.UrlPattern;
 import info.magnolia.cms.util.WorkspaceAccessUtil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.UnsupportedRepositoryOperationException;
+import javax.jcr.observation.Event;
+import javax.jcr.observation.EventIterator;
+import javax.jcr.observation.EventListener;
+import javax.jcr.observation.ObservationManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Uses a single full access AccessManager. JCR sessions are only released if no event listener were registered.
  */
 public class SystemRepositoryStrategy extends AbstractRepositoryStrategy {
+
+    private static final Logger log = LoggerFactory.getLogger(SystemRepositoryStrategy.class);
+
     private AccessManager accessManager;
+  
+    private Map<String, EventListener> observedHMs = new HashMap<String, EventListener>();
+ 
 
     public SystemRepositoryStrategy(SystemContext context) {
     }
@@ -75,7 +95,50 @@ public class SystemRepositoryStrategy extends AbstractRepositoryStrategy {
     }
 
     public void release() {
+        if (!observedHMs.isEmpty()) {
+            for (Map.Entry<String, EventListener> entry : observedHMs.entrySet()) {
+                final String[] key = entry.getKey().split("_");
+                final HierarchyManager hm = super.getHierarchyManager(key[0], key[1]);
+                try {
+                    hm.getWorkspace().getObservationManager().removeEventListener(entry.getValue());
+                } catch (UnsupportedRepositoryOperationException e) {
+                    log.error("Failed to remove listener from short living session. Session doesn't support listener removal.");
+                } catch (RepositoryException e) {
+                    log.error("Failed to remove listener from short living session. Will not be able to release this session.");
+                }
+            }
+            // release reference to listener so it can be GCed
+            observedHMs.clear();
+        }
         super.release(true);
     }
 
+    @Override
+    public HierarchyManager getHierarchyManager(String repositoryId, String workspaceId) {
+        final HierarchyManager hm = super.getHierarchyManager(repositoryId, workspaceId);
+        final String key = repositoryId + "_" + workspaceId;
+        if (!observedHMs.keySet().contains(key)) {
+            final EventListener listener = new EventListener() {
+
+                public void onEvent(EventIterator iterator) {
+                    // reload everything
+                    try {
+                        hm.refresh(true);
+                    } catch (RepositoryException e) {
+                        log.error("Failed to refresh short living session after update. Session will not be able to see content changes if repository uses update-on-read strategy.");
+                    }
+                }
+            };
+            try {
+                hm.getWorkspace().getObservationManager().addEventListener(listener, Event.NODE_ADDED | Event.NODE_REMOVED | Event.PROPERTY_ADDED | Event.PROPERTY_CHANGED | Event.PROPERTY_REMOVED, "/", true, null, null, false);
+                observedHMs.put(key, listener);
+            } catch (UnsupportedRepositoryOperationException e) {
+                log.warn("Repository doesn't support observation. Observers will not be notified of changes in repository.");
+            } catch (RepositoryException e) {
+                log.error("Failed to register observer for repository updates.");
+            }
+            //ObservationUtil.registerChangeListener(hm.getName(), "/", listener );
+        }
+        return hm;
+    }
 }
