59f92027e66c573b06239ff87fe1688ed378b882
Christian Ringele <christian.ringele@magnolia-cms.com>
2010-11-05 09:59:12 +0000

    MAGNOLIA-3366: ItemType of created path is definable.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@38832 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/module/delta/CreateNodePathTask.java
+++ magnolia-core/src/main/java/info/magnolia/module/delta/CreateNodePathTask.java
@@ -1,19 +1,19 @@
 /**
  * This file Copyright (c) 2007-2010 Magnolia International
  * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
- * 
- * 
+ *
+ *
  * This file is dual-licensed under both the Magnolia
- * Network Agreement and the GNU General Public License. 
+ * Network Agreement and the GNU General Public License.
  * You may elect to use one or the other of these licenses.
- * 
+ *
  * This file is distributed in the hope that it will be
  * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
  * Redistribution, except as permitted by whichever of the GPL
  * or MNA you select, is prohibited.
- * 
+ *
  * 1. For the GPL license (GPL), you can redistribute and/or
  * modify this file under the terms of the GNU General
  * Public License, Version 3, as published by the Free Software
@@ -21,19 +21,20 @@
  * General Public License, Version 3 along with this program;
  * if not, write to the Free Software Foundation, Inc., 51
  * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- * 
+ *
  * 2. For the Magnolia Network Agreement (MNA), this file
  * and the accompanying materials are made available under the
  * terms of the MNA which accompanies this distribution, and
  * is available at http://www.magnolia-cms.com/mna.html
- * 
+ *
  * Any modifications to this file must keep this entire header
  * intact.
- * 
+ *
  */
 package info.magnolia.module.delta;
 
 import info.magnolia.cms.core.HierarchyManager;
+import info.magnolia.cms.core.ItemType;
 import info.magnolia.cms.util.ContentUtil;
 import info.magnolia.module.InstallContext;
 
@@ -50,15 +51,21 @@ public class CreateNodePathTask extends AbstractRepositoryTask {
 
     private final String workspace;
     private final String path;
+    private final ItemType type;
 
     public CreateNodePathTask(String name, String description, String workspace, String path) {
+        this(name, description, workspace, path, ItemType.CONTENT);
+    }
+
+    public CreateNodePathTask(String name, String description, String workspace, String path, ItemType type) {
         super(name, description);
         this.workspace = workspace;
         this.path = path;
+        this.type = type;
     }
 
     protected void doExecute(InstallContext installContext) throws TaskExecutionException, RepositoryException {
         final HierarchyManager hm = installContext.getHierarchyManager(workspace);
-        ContentUtil.createPath(hm, path, false);
+        ContentUtil.createPath(hm, path, type, false);
     }
 }
