7a28ea06f8d8afd287d3cbf23fce40dab87780b0
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2010-12-07 19:10:30 +0000

    MAGNOLIA-3426: align 5.0 trunk
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@40406 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-cache/src/main/java/info/magnolia/module/cache/browsercachepolicy/FixedDuration.java
+++ magnolia-module-cache/src/main/java/info/magnolia/module/cache/browsercachepolicy/FixedDuration.java
@@ -35,53 +35,23 @@ package info.magnolia.module.cache.browsercachepolicy;
 
 import info.magnolia.module.cache.BrowserCachePolicyResult;
 import info.magnolia.module.cache.CachePolicyResult;
-import info.magnolia.voting.Voter;
-import info.magnolia.voting.voters.FalseVoter;
-import info.magnolia.voting.voters.TrueVoter;
-
 
 /**
- * Uses a fix expiration time (in minutes) if {@link #voter} votes positive or sets a far future
- * expiration - more than a year - if {@link #farFutureVoter} votes true. Default to 30 minutes.
+ * Uses a fix expiration time (in minutes). Default to 30 minutes.
  *
- * @author pbracher
  * @version $Revision$ ($Author$)
  */
 public class FixedDuration extends AbstractVoterBased {
 
-
-    /**
-     * Far future expiration is a year.
-     */
-    private static final long ONE_YEAR_IN_MILLISECONDS = 365L * 24 * 60 * 60 *  1000;
-
     private static final int MINUTE_IN_MILLIS = 60 * 1000;
-
-    /**
-     * Used to test if the request should be cached by the browser. Default {@link TrueVoter}.
-     */
-    private Voter voter = new TrueVoter();
-
-    /**
-     * Used to test if the request should be cache for ever. Default {@link FalseVoter}.
-     */
-    private Voter farFutureVoter = new FalseVoter();
 
     private int expirationMinutes = 30;
 
     @Override
     protected BrowserCachePolicyResult getPositiveVoteResult(CachePolicyResult cachePolicyResult) {
-        if(farFutureVoter.vote(cachePolicyResult)>0){
-            return new BrowserCachePolicyResult(System.currentTimeMillis() + ONE_YEAR_IN_MILLISECONDS);
-        }
-        else if(voter.vote(cachePolicyResult)>0){
-            // cast to long as the operation might exceed the int range
-            long expirationInMilliseconds = (long) this.getExpirationMinutes() * MINUTE_IN_MILLIS;
-            return new BrowserCachePolicyResult(System.currentTimeMillis() + expirationInMilliseconds);
-        }
-        else{
-            return BrowserCachePolicyResult.NO_CACHE;
-        }
+        // cast to long as the operation might exceed the int range
+        long expirationInMilliseconds = (long) this.getExpirationMinutes() * MINUTE_IN_MILLIS;
+        return new BrowserCachePolicyResult(System.currentTimeMillis() + expirationInMilliseconds);
     }
 
     public int getExpirationMinutes() {
@@ -90,22 +60,6 @@ public class FixedDuration extends AbstractVoterBased {
 
     public void setExpirationMinutes(int expirationMinutes) {
         this.expirationMinutes = expirationMinutes;
-    }
-
-    public Voter getVoter() {
-        return voter;
-    }
-
-    public void setVoter(Voter voter) {
-        this.voter = voter;
-    }
-
-    public Voter getFarFutureVoter() {
-        return farFutureVoter;
-    }
-
-    public void setFarFutureVoter(Voter farFutureVoter) {
-        this.farFutureVoter = farFutureVoter;
     }
 
 }
