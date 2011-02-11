c898bf84e259c6a68127caf4a52eea983dd97f9d
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2011-02-11 11:37:34 +0000

    MAGNOLIA-3549: - base framework - use blackboard as event bus
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@41954 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-module-vaadin/src/main/java/info/magnolia/module/vaadin/activity/ActivityManager.java
@@ -0,0 +1,91 @@
+/**
+ * This file Copyright (c) 2011 Magnolia International
+ * Ltd.  (http://www.magnolia.info). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia.info/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.module.vaadin.activity;
+
+import info.magnolia.module.vaadin.event.EventBus;
+import info.magnolia.module.vaadin.place.Place;
+import info.magnolia.module.vaadin.place.PlaceChangeEvent;
+import info.magnolia.module.vaadin.place.PlaceChangeListener;
+import info.magnolia.module.vaadin.region.Region;
+
+/**
+ * Manages {@link Activity} objects that should be kicked off in response to
+ * {@link PlaceChangeEvent} events. Each activity provides a widget to be shown when it's ready to run.
+ *
+ * Inspired by {@link com.google.gwt.activity.shared.ActivityManager}.
+ */
+public class ActivityManager implements PlaceChangeListener {
+
+    private static final Activity NULL_ACTIVITY = new Activity() {
+        public void start(Region region, EventBus eventBus) {
+            region.setComponent(null);
+        }
+    };
+
+    private final ActivityMapper mapper;
+    private final EventBus eventBus;
+    private Region region;
+    private Activity currentActivity = NULL_ACTIVITY;
+
+    public ActivityManager(ActivityMapper mapper, EventBus eventBus) {
+        this.mapper = mapper;
+        this.eventBus = eventBus;
+
+        eventBus.addListener(this);
+    }
+
+    public void onPlaceChange(PlaceChangeEvent event) {
+        Place newPlace = event.getNewPlace();
+        Activity nextActivity = mapper.getActivity(newPlace);
+
+
+        if (currentActivity.equals(nextActivity)) {
+            return;
+        }
+
+        if(nextActivity == null){
+            nextActivity = NULL_ACTIVITY;
+        }
+
+        currentActivity = nextActivity;
+
+        currentActivity.start(region, eventBus);
+
+   }
+
+    public void setDisplay(Region region){
+        this.region = region;
+    }
+
+}
