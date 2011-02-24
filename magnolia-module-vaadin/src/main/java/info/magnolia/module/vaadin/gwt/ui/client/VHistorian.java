7a58915c42c8495d658549f986b2d4580fd4815c
Federico Grilli <federico.grilli@magnolia-cms.com>
2011-02-24 16:41:51 +0000

    MAGNOLIA-3546: need to override UriFragmentUtility client side implementation to overcome problem related to initial uri fragment not written into browser location bar.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@42371 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-module-vaadin/src/main/java/info/magnolia/module/vaadin/gwt/ui/client/VHistorian.java
@@ -0,0 +1,93 @@
+/**
+ * This file Copyright (c) 2011 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.module.vaadin.gwt.ui.client;
+
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.user.client.History;
+import com.vaadin.terminal.gwt.client.ApplicationConnection;
+import com.vaadin.terminal.gwt.client.UIDL;
+import com.vaadin.terminal.gwt.client.ui.VUriFragmentUtility;
+
+/**
+ * Client side implementation for UriFragmentUtility. Uses GWT's History object
+ * as an implementation.
+ *
+ */
+public class VHistorian extends VUriFragmentUtility {
+
+    private String fragment;
+    private ApplicationConnection client;
+    private String paintableId;
+    private boolean immediate;
+
+    public VHistorian() {
+        super();
+
+    }
+
+    public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
+        if (client.updateComponent(this, uidl, false)) {
+            return;
+        }
+        String uidlFragment = uidl.getStringVariable("fragment");
+        immediate = uidl.getBooleanAttribute("immediate");
+        if (this.client == null) {
+            // initial paint has some special logic
+            this.client = client;
+            paintableId = uidl.getId();
+            if (!fragment.equals(uidlFragment)) {
+                // initial server side fragment (from link/bookmark/typed) does
+                // not equal the one on
+                // server, send initial fragment to server
+                //History.fireCurrentHistoryState();
+                History.newItem(uidlFragment, false);
+            }
+        } else {
+            if (uidlFragment != null && !uidlFragment.equals(fragment)) {
+                fragment = uidlFragment;
+                // normal fragment change from server, add new history item
+                History.newItem(uidlFragment, false);
+            }
+        }
+    }
+
+    public void onValueChange(ValueChangeEvent<String> event) {
+        String historyToken = event.getValue();
+        fragment = historyToken;
+        if (client != null) {
+            client.updateVariable(paintableId, "fragment", fragment, immediate);
+        }
+    }
+
+}
