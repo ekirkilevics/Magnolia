34e9c9b593d74fd1f179f4e65098a99b2392c1bd
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2011-02-25 09:26:55 +0000

    fixed test - setting component via ComponentsTestUtil
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@42386 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/test/java/info/magnolia/cms/util/UnicodeNormalizerTest.java
+++ magnolia-core/src/test/java/info/magnolia/cms/util/UnicodeNormalizerTest.java
@@ -67,7 +67,7 @@ public class UnicodeNormalizerTest extends TestCase {
         super.setUp();
         SystemProperty.clear();
         ComponentsTestUtil.clear();
-        SystemProperty.setProperty("info.magnolia.cms.util.UnicodeNormalizer$Normalizer", "info.magnolia.cms.util.UnicodeNormalizer$AutoDetectNormalizer");
+        ComponentsTestUtil.setImplementation(info.magnolia.cms.util.UnicodeNormalizer.Normalizer.class, "info.magnolia.cms.util.UnicodeNormalizer$AutoDetectNormalizer");
         SystemProperty.setProperty("magnolia.utf8.enabled", "true");
     }
 
