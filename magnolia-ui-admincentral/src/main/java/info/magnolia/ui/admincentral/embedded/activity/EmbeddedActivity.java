ae022c9ec4b90f88736bafb70c7fddd42cecc206
Federico Grilli <federico.grilli@magnolia-cms.com>
2011-03-30 08:15:48 +0000

    MAGNOLIA-3546: implement equals() and hashCode()
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@43496 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-ui-admincentral/src/main/java/info/magnolia/ui/admincentral/embedded/activity/EmbeddedActivity.java
+++ magnolia-ui-admincentral/src/main/java/info/magnolia/ui/admincentral/embedded/activity/EmbeddedActivity.java
@@ -69,4 +69,34 @@ public class EmbeddedActivity extends AbstractActivity {
         viewPort.setView(view);
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((place == null) ? 0 : place.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        EmbeddedActivity other = (EmbeddedActivity) obj;
+        if (place == null) {
+            if (other.place != null) {
+                return false;
+            }
+        } else if (!place.getUrl().equals(other.place.getUrl())) {
+            return false;
+        }
+        return true;
+    }
+
 }
