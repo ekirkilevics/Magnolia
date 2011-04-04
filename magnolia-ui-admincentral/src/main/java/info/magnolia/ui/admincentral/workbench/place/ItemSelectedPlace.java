99ca123904632fb26089240f134facb90ac7f24f
Daniel Lipp <daniel.lipp@magnolia-cms.com>
2011-04-04 09:27:18 +0000

    Generated equals + hashcode: PlaceController#goTo tried to optimize by not reacting when where equals newPlace - did only work on identical Places before!
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@43617 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-ui-admincentral/src/main/java/info/magnolia/ui/admincentral/workbench/place/ItemSelectedPlace.java
+++ magnolia-ui-admincentral/src/main/java/info/magnolia/ui/admincentral/workbench/place/ItemSelectedPlace.java
@@ -86,5 +86,36 @@ public class ItemSelectedPlace extends Place {
         return ToStringBuilder.reflectionToString(this);
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((path == null) ? 0 : path.hashCode());
+        result = prime * result + ((workspace == null) ? 0 : workspace.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ItemSelectedPlace other = (ItemSelectedPlace) obj;
+        if (path == null) {
+            if (other.path != null)
+                return false;
+        } else if (!path.equals(other.path))
+            return false;
+        if (workspace == null) {
+            if (other.workspace != null)
+                return false;
+        } else if (!workspace.equals(other.workspace))
+            return false;
+        return true;
+    }
+
 
 }
