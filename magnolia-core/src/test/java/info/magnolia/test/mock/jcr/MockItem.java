d88cfbacc99d2417330d799166367de854f89de9
Daniel Lipp <daniel.lipp@magnolia-cms.com>
2011-05-24 07:56:53 +0000

    MAGNOLIA-3694: MockSession can now have a ValueFactory
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@45426 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/test/java/info/magnolia/test/mock/jcr/MockItem.java
+++ magnolia-core/src/test/java/info/magnolia/test/mock/jcr/MockItem.java
@@ -107,7 +107,7 @@ public abstract class MockItem implements Item {
         return session;
     }
 
-    protected void setSession(MockSession session) {
+    public void setSession(MockSession session) {
         this.session = session;
     }
 
@@ -144,7 +144,7 @@ public abstract class MockItem implements Item {
         throw new UnsupportedOperationException("Not implemented. This is a fake class.");
     }
 
-    protected void setParent(MockNode parent) throws RepositoryException {
+    public void setParent(MockNode parent) {
         this.parent = parent;
         setSession((MockSession) parent.getSession());
     }
