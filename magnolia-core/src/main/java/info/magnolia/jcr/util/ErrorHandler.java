689deaace9d0f7aadce522cca2ab405bd2652302
Daniel Lipp <daniel.lipp@magnolia-cms.com>
2011-05-24 09:34:13 +0000

    MAGNOLIA-3694, SCRUM-103: first draft of JCR-based node builder
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@45434 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/jcr/util/ErrorHandler.java
@@ -0,0 +1,66 @@
+/**
+ * This file Copyright (c) 2009-2011 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.jcr.util;
+
+import info.magnolia.nodebuilder.NodeOperationException;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+/**
+ * ErrorHandler implementations can decide what to do with certain conditions.
+ * Specifically, they'll usually log or throw exceptions. They can only throw
+ * NodeOperationException (or other RuntimeExceptions, obviously), which
+ * the client code is expected to handle.
+ *
+ * @version $Id$
+ */
+public interface ErrorHandler {
+
+    /**
+     * The operation calling this method is expected to pass a fully formed message;
+     * it should ideally contain some context information about the operation that
+     * caused an issue. The ErrorHandler implementation will decide what to do
+     * with it. (log, throw a NodeOperationException, ...)
+     */
+    void report(String message) throws NodeOperationException, RepositoryException;
+
+    /**
+     * The operation calling this method isn't expected to do anything here;
+     * the ErrorHandler implementation will attempt to build a fully formed message,
+     * then decide what to do with it. (log, throw a NodeOperationException, ...)
+     */
+    void handle(RepositoryException e, Node context) throws NodeOperationException, RepositoryException;
+
+}
---
+++ magnolia-core/src/main/java/info/magnolia/jcr/util/NodeOperation.java
@@ -0,0 +1,53 @@
+/**
+ * This file Copyright (c) 2009-2011 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.jcr.util;
+
+import javax.jcr.Node;
+
+/**
+ * A NodeOperation instance describes an "atomic" operation to apply to a node ("context").
+ * Instances of this are executed via {@link NodeBuilder}, can be nested (via {@link #then(NodeOperation...)}).
+ *
+ * @version $Id$
+ */
+public interface NodeOperation {
+    // return this
+    // TODO - differentiate between operations that can be chained or not - i.e setProperty shouldn't be chainable
+    NodeOperation then(NodeOperation... childrenOps);
+
+    // TODO exec should not appear in public interface
+    void exec(Node context, ErrorHandler errorHandler);
+
+    // TODO - description ... ?
+}
