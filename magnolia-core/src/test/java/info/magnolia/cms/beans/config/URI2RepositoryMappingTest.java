92f46035354acf4ca32c9b361e23f78d6bce3809
Daniel Lipp <daniel.lipp@magnolia-cms.com>
2011-06-10 07:39:25 +0000

    QA - conversion: EasyMock -> Mockito, Junit3 -> 4
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@46121 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/test/java/info/magnolia/cms/beans/config/URI2RepositoryMappingTest.java
+++ magnolia-core/src/test/java/info/magnolia/cms/beans/config/URI2RepositoryMappingTest.java
@@ -33,46 +33,48 @@
  */
 package info.magnolia.cms.beans.config;
 
-import static org.easymock.EasyMock.*;
-
-import javax.jcr.PropertyType;
-
-import info.magnolia.cms.core.SystemProperty;
-import info.magnolia.objectfactory.Components;
-import info.magnolia.test.TestMagnoliaConfigurationProperties;
-import junit.framework.TestCase;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import info.magnolia.cms.core.Content;
 import info.magnolia.cms.core.HierarchyManager;
 import info.magnolia.cms.core.NodeData;
-import info.magnolia.test.ComponentsTestUtil;
+import info.magnolia.cms.core.SystemProperty;
 import info.magnolia.context.Context;
 import info.magnolia.context.MgnlContext;
 import info.magnolia.context.WebContext;
+import info.magnolia.objectfactory.Components;
+import info.magnolia.test.ComponentsTestUtil;
+import info.magnolia.test.TestMagnoliaConfigurationProperties;
 import info.magnolia.test.mock.MockRepositoryAcquiringStrategy;
 
-/**
- * @author had
- * @version $Id:$
- */
-public class URI2RepositoryMappingTest extends TestCase {
+import javax.jcr.PropertyType;
 
-    @Override
-    protected void tearDown() throws Exception {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @version $Id$
+ */
+public class URI2RepositoryMappingTest {
+
+    @After
+    public void tearDown() throws Exception {
         MgnlContext.setInstance(null);
         ComponentsTestUtil.clear();
-        super.tearDown();
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    @Before
+    public void setUp() throws Exception {
         // some tests do not cleanup properly so we need to cleanup here before starting to be sure.
         MgnlContext.setInstance(null);
         ComponentsTestUtil.clear();
         SystemProperty.setMagnoliaConfigurationProperties(new TestMagnoliaConfigurationProperties());
     }
 
+    @Test
     public void testGetUri() throws Exception {
         ComponentsTestUtil.setInstance(ServerConfiguration.class, new ServerConfiguration());
         ServerConfiguration.getInstance().setDefaultExtension("bla");
@@ -80,46 +82,30 @@ public class URI2RepositoryMappingTest extends TestCase {
         mapping.setRepository("dummy-repo");
         mapping.setURIPrefix("/blabla/");
         // instance is set only in constructor ...
-        final Context context = createStrictMock(Context.class);
-        final HierarchyManager hm = createStrictMock(HierarchyManager.class);
-        final Content cnt = createStrictMock(Content.class);
-        final Content root = createStrictMock(Content.class);
-        final NodeData docu = createStrictMock(NodeData.class);
-        expect(context.getHierarchyManager("dummy-repo")).andReturn(hm);
-        expect(hm.isExist("/Test/image")).andReturn(Boolean.TRUE);
-        expect(hm.isNodeData("/Test/image")).andReturn(Boolean.TRUE);
-        expect(hm.isNodeData("/Test")).andReturn(Boolean.FALSE);
-        expect(hm.isNodeData("/Test/image")).andReturn(Boolean.TRUE);
-        expect(hm.getContent("/Test")).andReturn(cnt);
-        expect(cnt.getNodeData("image")).andReturn(docu);
-        expect(hm.getName()).andReturn("dummy-repo");
-        expect(cnt.getHierarchyManager()).andReturn(hm);
-        expect(cnt.isNodeType("mix:referenceable")).andReturn(true);
-        expect(cnt.getUUID()).andReturn("uu-something-real");
+        final Context context = mock(Context.class);
+        final HierarchyManager hm = mock(HierarchyManager.class);
+        final Content cnt = mock(Content.class);
+        final NodeData docu = mock(NodeData.class);
+        when(context.getHierarchyManager("dummy-repo")).thenReturn(hm);
+        when(hm.isNodeData("/Test")).thenReturn(Boolean.FALSE);
+        when(hm.isNodeData("/Test/image")).thenReturn(Boolean.TRUE);
+        when(hm.getContent("/Test")).thenReturn(cnt);
+        when(cnt.getNodeData("image")).thenReturn(docu);
+        when(cnt.getHierarchyManager()).thenReturn(hm);
 
-        expect(docu.getType()).andReturn(PropertyType.BINARY);
-        expect(docu.getAttribute("nodeDataTemplate")).andReturn("");
-        expect(docu.getAttribute("extension")).andReturn("jpg");
-        expect(docu.getAttribute("fileName")).andReturn("blah");
-        expect(docu.getAttribute("contentType")).andReturn("mgnl:resource");
-        expect(docu.getAttribute("size")).andReturn("1234");
-        expect(docu.getType()).andReturn(PropertyType.BINARY);
-        expect(docu.getAttribute("nodeDataTemplate")).andReturn("");
-        expect(docu.getAttribute("extension")).andReturn("jpg");
-        expect(docu.getAttribute("fileName")).andReturn("blah");
-        expect(docu.getAttribute("contentType")).andReturn("mgnl:resource");
-        expect(docu.getAttribute("size")).andReturn("1234");
-        replay(context, hm, cnt, root, docu);
+        when(docu.getType()).thenReturn(PropertyType.BINARY);
+        when(docu.getAttribute("extension")).thenReturn("jpg");
+        when(docu.getAttribute("fileName")).thenReturn("blah");
         MgnlContext.setInstance(context);
         String uri = mapping.getURI("/Test/image");
         assertEquals("Detected double slash in generated link path.",-1, uri.indexOf("//"));
         assertTrue("Incorrect file name generated.",uri.endsWith("/blah.jpg"));
-        verify(context, hm, cnt, root, docu);
     }
 
+    @Test
     public void testGetHandleStripsExtensionInclTheDot() throws Exception {
-        WebContext context = createNiceMock(WebContext.class);
-        HierarchyManager hm = createNiceMock(HierarchyManager.class);
+        WebContext context = mock(WebContext.class);
+        HierarchyManager hm = mock(HierarchyManager.class);
         MgnlContext.setInstance(context);
         MockRepositoryAcquiringStrategy strategy = Components.getSingleton(MockRepositoryAcquiringStrategy.class);
         strategy.addHierarchyManager("config", hm);
@@ -128,13 +114,11 @@ public class URI2RepositoryMappingTest extends TestCase {
         ServerConfiguration.getInstance().setDefaultExtension("ext");
         ComponentsTestUtil.setInstance(URI2RepositoryManager.class, new URI2RepositoryManager());
         Object[] objs = new Object[] {context, hm};
-        replay(objs);
         String handle = URI2RepositoryManager.getInstance().getHandle("/blah.ext");
         assertEquals("/blah", handle);
         handle = URI2RepositoryManager.getInstance().getHandle("/b.l/ah.ext");
         assertEquals("/b.l/ah", handle);
         handle = URI2RepositoryManager.getInstance().getHandle("/bl.ah.ext");
         assertEquals("/bl.ah", handle);
-        verify(objs);
     }
 }
