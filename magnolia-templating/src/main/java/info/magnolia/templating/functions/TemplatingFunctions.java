f43db712f4b34caccc8dc3d2e9a097ce4e85c345
Christian Ringele <christian.ringele@magnolia-cms.com>
2011-06-29 15:52:38 +0000

    SCRUM-138 : added asContent(), uuid() and all link() methods
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@46747 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-templating/src/main/java/info/magnolia/templating/functions/TemplatingFunctions.java
+++ magnolia-templating/src/main/java/info/magnolia/templating/functions/TemplatingFunctions.java
@@ -34,7 +34,9 @@
 package info.magnolia.templating.functions;
 
 import info.magnolia.cms.core.Content;
+import info.magnolia.cms.core.DefaultContent;
 import info.magnolia.jcr.util.ContentMap;
+import info.magnolia.link.LinkUtil;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
@@ -50,6 +52,13 @@ import org.slf4j.LoggerFactory;
 public class TemplatingFunctions {
 
     private static final Logger log = LoggerFactory.getLogger(TemplatingFunctions.class);
+
+
+    //TODO cringele : test missing
+    //TODO cringele : check with PH, if it can be done without using deprecated objects
+    public Content asContent(Node node) throws RepositoryException {
+        return node == null ? null : new DefaultContent(node, null);
+    }
 
     public Node asJCRNode(Content content) {
         return content == null ? null : content.getJCRNode();
@@ -69,6 +78,61 @@ public class TemplatingFunctions {
         }
         Node parentContent = this.parent(contentMap.getJCRNode());
         return new ContentMap(parentContent);
+    }
+
+    //TODO cringele : test missing
+    public String uuid(Node content) throws RepositoryException {
+        return content == null ? null : content.getIdentifier();
+    }
+
+    //TODO cringele : test missing
+    public String uuid(ContentMap contentMap) throws RepositoryException {
+        return contentMap == null ? null : this.uuid(asJCRNode(contentMap));
+    }
+
+    //TODO cringele : test missing
+    public String link(Node content) throws RepositoryException{
+        if(content == null) {
+            return null;
+        }
+        //TODO cringele : LinkUtil should accept Node and not Content
+        Content asContent = this.asContent(content);
+        return LinkUtil.createLink(asContent);
+    }
+
+    //TODO cringele : test missing
+    public String link(ContentMap contentMap) throws RepositoryException{
+        return contentMap == null ? null : this.link(asJCRNode(contentMap));
+    }
+
+    //TODO cringele : test missing
+    public String linkExteral(Node content) throws RepositoryException{
+        if(content == null) {
+            return null;
+        }
+        //TODO cringele : LinkUtil should accept Node and not Content
+        Content asContent = this.asContent(content);
+        return LinkUtil.createExternalLink(asContent);
+    }
+
+    //TODO cringele : test missing
+    public String linkExternal(ContentMap contentMap) throws RepositoryException{
+        return contentMap == null ? null : this.linkExteral(asJCRNode(contentMap));
+    }
+
+    //TODO cringele : test missing
+    public String linkAbsolute(Node content) throws RepositoryException{
+        if(content == null) {
+            return null;
+        }
+        //TODO cringele : LinkUtil should accept Node and not Content
+        Content asContent = this.asContent(content);
+        return LinkUtil.createAbsoluteLink(asContent);
+    }
+
+    //TODO cringele : test missing
+    public String linkAbsolute(ContentMap contentMap) throws RepositoryException{
+        return contentMap == null ? null : this.linkAbsolute(asJCRNode(contentMap));
     }
 
 }
