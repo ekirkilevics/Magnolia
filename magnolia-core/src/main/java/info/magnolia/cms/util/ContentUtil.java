e04d5e577bd8e46a77ef266d61950b266c93af8e
Federico Grilli <federico.grilli@magnolia-cms.com>
2011-08-17 10:38:27 +0000

    if time is to be  in ms then do not divide by 1000
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@48399 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-core/src/main/java/info/magnolia/cms/util/ContentUtil.java
+++ magnolia-core/src/main/java/info/magnolia/cms/util/ContentUtil.java
@@ -622,7 +622,7 @@ public class ContentUtil {
                 log.debug("{} won't be replaced", node.getHandle());
                 return;
             }
-            log.debug("find&replace operations took {}ms" + (System.currentTimeMillis() - start) / 1000);
+            log.debug("find&replace operations took {}ms" + (System.currentTimeMillis() - start));
 
             FileUtils.writeStringToFile(file, replaced);
             inStream = new FileInputStream(file);
