5df4fb6efafcfcb1a694b2b0a3f3d061e6b3f9df
Federico Grilli <federico.grilli@magnolia-cms.com>
2011-09-15 16:31:44 +0000

    SCRUM-371: uncommented some styles which are used by dialogs. Some temp tweaks for FF.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@49317 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-templating-editor/src/main/java/info/magnolia/templating/editor/client/EditBarWidget.java
+++ magnolia-templating-editor/src/main/java/info/magnolia/templating/editor/client/EditBarWidget.java
@@ -84,7 +84,7 @@ public class EditBarWidget extends AbstractBarWidget {
 
         createMouseEventsHandlers(pageEditor);
 
-        setClassName("mgnlControlBar");
+        setClassName("mgnlControlBarSmall");
 
     }
 
