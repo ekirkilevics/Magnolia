9a0521d36f5ad9ede681ead772ac1a169ce70f2d
Jan Haderka <jan.haderka@magnolia-cms.com>
2011-09-22 13:42:03 +0000

    SCRUM-396 test author mode as well
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@49469 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-templating/src/test/java/info/magnolia/templating/renderers/PlainTextRendererTest.java
+++ magnolia-templating/src/test/java/info/magnolia/templating/renderers/PlainTextRendererTest.java
@@ -34,9 +34,18 @@
 package info.magnolia.templating.renderers;
 
 import static org.junit.Assert.assertEquals;
+import info.magnolia.cms.beans.config.ServerConfiguration;
 import info.magnolia.cms.core.AggregationState;
 import info.magnolia.cms.core.Content;
+import info.magnolia.cms.gui.i18n.DefaultI18nAuthoringSupport;
+import info.magnolia.cms.gui.i18n.I18nAuthoringSupport;
+import info.magnolia.cms.i18n.DefaultI18nContentSupport;
+import info.magnolia.cms.i18n.DefaultMessagesManager;
+import info.magnolia.cms.i18n.I18nContentSupport;
+import info.magnolia.cms.i18n.MessagesManager;
+import info.magnolia.cms.security.DummyUser;
 import info.magnolia.context.MgnlContext;
+import info.magnolia.context.SystemContext;
 import info.magnolia.rendering.context.AggregationStateBasedRenderingContext;
 import info.magnolia.rendering.engine.AppendableOnlyOutputProvider;
 import info.magnolia.rendering.model.RenderingModel;
@@ -44,10 +53,12 @@ import info.magnolia.rendering.model.RenderingModelImpl;
 import info.magnolia.rendering.template.configured.ConfiguredRenderableDefinition;
 import info.magnolia.test.ComponentsTestUtil;
 import info.magnolia.test.mock.MockContent;
+import info.magnolia.test.mock.MockHierarchyManager;
 import info.magnolia.test.mock.MockWebContext;
 
 import java.io.StringWriter;
 import java.util.HashMap;
+import java.util.Locale;
 
 import javax.jcr.RepositoryException;
 
@@ -55,6 +66,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.mockrunner.mock.web.MockHttpServletRequest;
 import com.mockrunner.mock.web.MockHttpServletResponse;
 
 /**
@@ -71,7 +83,14 @@ public class PlainTextRendererTest {
         webctx = new MockWebContext();
         MgnlContext.setInstance(webctx);
         webctx.setResponse(new MockHttpServletResponse());
+        webctx.setRequest(new MockHttpServletRequest());
+        webctx.setUser(new DummyUser());
         ComponentsTestUtil.setImplementation(RenderingModel.class, RenderingModelImpl.class);
+        ComponentsTestUtil.setInstance(I18nContentSupport.class, new DefaultI18nContentSupport());
+        ComponentsTestUtil.setInstance(I18nAuthoringSupport.class, new DefaultI18nAuthoringSupport());
+        ComponentsTestUtil.setInstance(SystemContext.class, webctx);
+        ComponentsTestUtil.setInstance(MessagesManager.class, new DefaultMessagesManager());
+        webctx.setLocale(new Locale("en"));
     }
 
     @After
@@ -81,7 +100,7 @@ public class PlainTextRendererTest {
     }
 
     @Test
-    public void testJustWorks() throws Exception {
+    public void testRender() throws Exception {
         final PlainTextTemplateRenderer r = new PlainTextTemplateRenderer();
         final Content content = getNode(CONTENTNODE1, "/foo/bar/MyPage");
         final StringWriter out = new StringWriter();
@@ -97,8 +116,50 @@ public class PlainTextRendererTest {
         assertEquals("In a crooked little town, they were lost and never found", out.toString());
     }
 
+    @Test
+    public void testRenderInPreview() throws Exception {
+        final PlainTextTemplateRenderer r = new PlainTextTemplateRenderer();
+        final Content content = getNode(CONTENTNODE1, "/foo/bar/MyPage");
+        final StringWriter out = new StringWriter();
+        AggregationState state = new AggregationState();
+        state.setPreviewMode(false);
+        ServerConfiguration.getInstance().setAdmin(true);
+        webctx.setAggregationState(state);
+        state.setCurrentContent(content);
+        state.setMainContent(content);
+        AggregationStateBasedRenderingContext ctx = new AggregationStateBasedRenderingContext(state);
+        ConfiguredRenderableDefinition renderableDefinition = new ConfiguredRenderableDefinition();
+
+        ctx.push(content.getJCRNode(), renderableDefinition, new AppendableOnlyOutputProvider(out));
+        r.render(ctx, new HashMap());
+        assertEquals("<html>\n"
+                + "<body>\n"
+                + "<link rel=\"stylesheet\" type=\"text/css\" href=\"null/.resources/admin-css/admin-all.css\" />\n"
+                + "\n"
+                + "<script type=\"text/javascript\" src=\"null/.magnolia/pages/javascript.js\"></script>\n"
+                + "<script type=\"text/javascript\" src=\"null/.magnolia/pages/messages.en.js\"></script>\n"
+                + "<script type=\"text/javascript\" src=\"null/.resources/admin-js/dialogs/dialogs.js\"></script>\n"
+                + "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"null/.resources/calendar/skins/aqua/theme.css\" title=\"Aqua\" />\n"
+                + "<script type=\"text/javascript\" src=\"null/.resources/calendar/calendar.js\"></script>\n"
+                + "<script type=\"text/javascript\" src=\"null/.resources/calendar/lang/calendar-en.js\"></script>\n"
+                + "<script type=\"text/javascript\" src=\"null/.resources/calendar/calendar-setup.js\"></script>\n"
+                + "\n"
+                + "<div class=\"mgnlMainbar\" style=\"top:0px;left:0px;width:100%;\">\n"
+                + "<table class=\"mgnlControlBar\"><tr><td class=\"mgnlBtnsLeft\"><span onmousedown=\"mgnlShiftPushButtonDown(this);\" "
+                + "onmouseout=\"mgnlShiftPushButtonOut(this);\" onclick=\"mgnlShiftPushButtonClick(this);mgnlPreview(true);\" class=\"mgnlControlButton\" "
+                + "style=\"background:transparent;\">???buttons.preview???</span><span onmousedown=\"mgnlShiftPushButtonDown(this);\" "
+                + "onmouseout=\"mgnlShiftPushButtonOut(this);\" onclick=\"mgnlShiftPushButtonClick(this);MgnlAdminCentral.showTree('null','//foo/bar/MyPage');\" "
+                + "class=\"mgnlControlButton\" style=\"background:transparent;\">???buttons.admincentral???</span></td></tr></table>\n"
+                + "</div>\n"
+                + "<h2 style=\"padding-top: 30px;\">//foo/bar/MyPage : mgnl:contentNode</h2><pre>\n"
+                + "In a crooked little town, they were lost and never found</pre>\n"
+                + "</body>\n"
+                + "</html>\n", out.toString());
+    }
+
     private Content getNode(String configNode, String path) throws RepositoryException {
         MockContent content = new MockContent(path);
+        content.setHierarchyManager(new MockHierarchyManager("website"));
         content.setNodeData("text", configNode);
         content.setNodeData("contentType", "mgnl:contentNode");
 
