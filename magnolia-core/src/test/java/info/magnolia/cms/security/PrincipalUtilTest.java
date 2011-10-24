4c7a5948143bcf0bfe64294488165d819f2c26f4
Daniel Lipp <daniel.lipp@magnolia-cms.com>
2011-10-24 08:15:26 +0000

    MAGNOLIA-3853: merged delta from trunk to this branch (continued)
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/branches/magnolia-4.5-jcr@50292 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/test/java/info/magnolia/cms/security/PrincipalUtilTest.java
@@ -0,0 +1,115 @@
+/**
+ * This file Copyright (c) 2011 Magnolia International
+ * Ltd.  (http://www.magnolia.info). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia.info/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.cms.security;
+
+import java.util.ArrayList;
+import javax.security.auth.Subject;
+
+import org.junit.Test;
+
+import info.magnolia.cms.security.auth.PrincipalCollectionImpl;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Test case for PrincipalUtil.
+ *
+ * @version $Id$
+ */
+public class PrincipalUtilTest {
+
+    @Test
+    public void testFindPrincipal() throws Exception {
+
+        Subject subject = new Subject();
+        User user = mock(User.class);
+        subject.getPrincipals().add(user);
+
+        assertSame(user, PrincipalUtil.findPrincipal(subject, User.class));
+    }
+
+    @Test
+    public void testFindPrincipalInCollection() throws Exception {
+
+        Subject subject = new Subject();
+        User user = mock(User.class);
+
+        PrincipalCollectionImpl collection = new PrincipalCollectionImpl();
+        collection.add(user);
+        subject.getPrincipals().add(collection);
+
+        assertSame(user, PrincipalUtil.findPrincipal(subject, User.class));
+    }
+
+    @Test
+    public void testFindACLByName() throws Exception {
+
+        Subject subject = new Subject();
+
+        PrincipalCollectionImpl collection = new PrincipalCollectionImpl();
+        ACLImpl acl = new ACLImpl("website", new ArrayList<Permission>());
+        collection.add(acl);
+        subject.getPrincipals().add(collection);
+
+        assertSame(acl, PrincipalUtil.findAccessControlList(subject, "website"));
+        assertSame(acl, PrincipalUtil.findAccessControlList(subject.getPrincipals(), "website"));
+    }
+
+    @Test
+    public void testRemovePrincipal() throws Exception {
+
+        Subject subject = new Subject();
+        User user = mock(User.class);
+        subject.getPrincipals().add(user);
+
+        assertSame(user, PrincipalUtil.removePrincipal(subject.getPrincipals(), User.class));
+        assertTrue(subject.getPrincipals().isEmpty());
+    }
+
+    @Test
+    public void testRemovePrincipalFromCollection() throws Exception {
+
+        Subject subject = new Subject();
+        User user = mock(User.class);
+
+        PrincipalCollectionImpl collection = new PrincipalCollectionImpl();
+        collection.add(user);
+        subject.getPrincipals().add(collection);
+
+        assertSame(user, PrincipalUtil.removePrincipal(subject.getPrincipals(), User.class));
+        assertFalse(collection.contains(user));
+    }
+}
