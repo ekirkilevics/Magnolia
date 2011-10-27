b71242b69e7b3a541ef0204ed979f8399645d6cb
Tobias Mattsson <tobias.mattsson@magnolia-cms.com>
2011-10-27 08:20:03 +0000

    SCRUM-487 extracted parsing of repositories.xml to RepositoryMappingDefinitionReader
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/branches/magnolia-4.5-jcr@50452 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/test/java/info/magnolia/repository/definition/RepositoryMappingDefinitionReaderTest.java
@@ -0,0 +1,75 @@
+package info.magnolia.repository.definition;
+
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class RepositoryMappingDefinitionReaderTest {
+
+    @Test
+    public void testParse() throws Exception {
+
+        InputStream stream = getClass().getResourceAsStream("test-repositories.xml");
+
+        RepositoryMappingDefinition definition = new RepositoryMappingDefinitionReader().read(stream);
+
+        assertNotNull(definition);
+
+        Collection<WorkspaceMappingDefinition> mappings = definition.getWorkspaceMappings();
+        assertEquals(3, mappings.size());
+
+        Iterator<WorkspaceMappingDefinition> iterator = mappings.iterator();
+        WorkspaceMappingDefinition mapping1 = iterator.next();
+        assertEquals("website", mapping1.getLogicalWorkspaceName());
+        assertEquals("magnolia", mapping1.getRepositoryName());
+        assertEquals("website", mapping1.getWorkspaceName());
+
+        WorkspaceMappingDefinition mapping2 = iterator.next();
+        assertEquals("data", mapping2.getLogicalWorkspaceName());
+        assertEquals("anotherRepository", mapping2.getRepositoryName());
+        assertEquals("physicalName", mapping2.getWorkspaceName());
+
+        WorkspaceMappingDefinition mapping3 = iterator.next();
+        assertEquals("config", mapping3.getLogicalWorkspaceName());
+        assertEquals("magnolia", mapping3.getRepositoryName());
+        assertEquals("config", mapping3.getWorkspaceName());
+
+        Collection<RepositoryDefinition> repositories = definition.getRepositories();
+        assertEquals(3, repositories.size());
+
+        Iterator<RepositoryDefinition> repositoryDefinitionIterator = repositories.iterator();
+        RepositoryDefinition magnolia = repositoryDefinitionIterator.next();
+        assertEquals("magnolia", magnolia.getName());
+        assertEquals("provider-class", magnolia.getProvider());
+        assertTrue(magnolia.isLoadOnStartup());
+        assertEquals(2, magnolia.getParameters().size());
+        assertEquals("value1", magnolia.getParameters().get("parameter1"));
+        assertEquals("value2", magnolia.getParameters().get("parameter2"));
+        assertEquals(2, magnolia.getWorkspaces().size());
+        assertTrue(magnolia.getWorkspaces().contains("website"));
+        assertTrue(magnolia.getWorkspaces().contains("config"));
+
+        RepositoryDefinition anotherRepository = repositoryDefinitionIterator.next();
+        assertEquals("anotherRepository", anotherRepository.getName());
+        assertEquals("another-provider-class", anotherRepository.getProvider());
+        assertFalse(anotherRepository.isLoadOnStartup());
+        assertEquals(0, anotherRepository.getParameters().size());
+        assertEquals(1, anotherRepository.getWorkspaces().size());
+        assertTrue(anotherRepository.getWorkspaces().contains("physicalName"));
+
+        RepositoryDefinition repositoryWithoutWorkspaces = repositoryDefinitionIterator.next();
+        assertEquals("repositoryWithoutWorkspaces", repositoryWithoutWorkspaces.getName());
+        assertEquals("third-provider-class", repositoryWithoutWorkspaces.getProvider());
+        assertFalse(repositoryWithoutWorkspaces.isLoadOnStartup());
+        assertEquals(0, repositoryWithoutWorkspaces.getParameters().size());
+        assertEquals(1, repositoryWithoutWorkspaces.getWorkspaces().size());
+        assertTrue(repositoryWithoutWorkspaces.getWorkspaces().contains("default"));
+    }
+}
