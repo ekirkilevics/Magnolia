337a0569659f1dc4f5f6ae51fcca11f0744c97c7
Christian Ringele <christian.ringele@magnolia-cms.com>
2011-11-11 09:26:24 +0000

    QA : Added missing @Overrides, and removed all tabs.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@51352 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-admininterface/src/test/java/info/magnolia/module/admininterface/pages/ImportPageTest.java
+++ magnolia-module-admininterface/src/test/java/info/magnolia/module/admininterface/pages/ImportPageTest.java
@@ -20,60 +20,62 @@ import info.magnolia.context.MgnlContext;
 import info.magnolia.context.WebContext;
 
 public class ImportPageTest extends TestCase{
-	
-	private HttpServletRequest request;
-	private HttpServletResponse response;
+
+    private HttpServletRequest request;
+    private HttpServletResponse response;
     private WebContext context;
     private HierarchyManager hm;
     private Workspace workspace;
-	private Session session;
-	private AccessManager access;
-    
-	private ImportPage importXml;
-	
-	private int uiid = 0;
-    
-	protected void setUp() throws Exception {
-		request = createStrictMock(HttpServletRequest.class);
+    private Session session;
+    private AccessManager access;
+
+    private ImportPage importXml;
+
+    private int uiid = 0;
+
+    @Override
+    protected void setUp() throws Exception {
+        request = createStrictMock(HttpServletRequest.class);
         response = createStrictMock(HttpServletResponse.class);
         context = createStrictMock(WebContext.class);
         hm = createStrictMock(HierarchyManager.class);
         workspace = createStrictMock(Workspace.class);
         session = createNiceMock(Session.class);
-        
+
         importXml = new ImportPage("import", request, response);
         MgnlContext.setInstance(context);
-	}
-	
-	public void tearDown(){
+    }
+
+    @Override
+    public void tearDown(){
         MgnlContext.setInstance(null);
     }
-    	
-	public void testXmlImport() throws Exception{
-		
-		File originalXml = new File(getClass().getResource("/test-import.xml").getFile());
-		File tmpXml = new File(originalXml.getParent() + "/tmp-test-import.xml");
-		FileUtils.copyFile(originalXml, tmpXml);
-		Document importFile = new Document(tmpXml, "text/xml");
 
-		expect(context.getAccessManager("website")).andReturn(access);
-		expect(context.getHierarchyManager("website")).andReturn(hm);
-		expect(hm.getWorkspace()).andReturn(workspace);
-		expect(hm.isExist("/")).andReturn(true);
-		expect(workspace.getSession()).andReturn(session);
-		
-		Object[] obj = new Object[]{request, response, context, hm, session, workspace};
+    public void testXmlImport() throws Exception{
+
+        File originalXml = new File(getClass().getResource("/test-import.xml").getFile());
+        File tmpXml = new File(originalXml.getParent() + "/tmp-test-import.xml");
+        FileUtils.copyFile(originalXml, tmpXml);
+        Document importFile = new Document(tmpXml, "text/xml");
+
+        expect(context.getAccessManager("website")).andReturn(access);
+        expect(context.getHierarchyManager("website")).andReturn(hm);
+        expect(hm.getWorkspace()).andReturn(workspace);
+        expect(hm.isExist("/")).andReturn(true);
+        expect(workspace.getSession()).andReturn(session);
+
+        Object[] obj = new Object[]{request, response, context, hm, session, workspace};
         replay(obj);
-        
-		importXml.setMgnlFileImport(importFile);
-		importXml.setMgnlKeepVersions(true);
-		importXml.setMgnlUuidBehavior(uiid);
-		
-		importXml.importxml();
 
-		verify(obj);
-	
-		assertEquals(false, importFile.getFile().exists());
-	}
-	
+        importXml.setMgnlFileImport(importFile);
+        importXml.setMgnlKeepVersions(true);
+        importXml.setMgnlUuidBehavior(uiid);
+
+        importXml.importxml();
+
+        verify(obj);
+
+        assertEquals(false, importFile.getFile().exists());
+    }
+
 }
