8569162022925f5f2a44adcfcebd400f4fbcec24
Federico Grilli <federico.grilli@magnolia-cms.com>
2011-11-25 13:22:09 +0000

    tab
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@51979 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-admininterface/src/main/java/info/magnolia/module/admininterface/trees/TemplateColumn.java
+++ magnolia-module-admininterface/src/main/java/info/magnolia/module/admininterface/trees/TemplateColumn.java
@@ -61,7 +61,7 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class TemplateColumn extends TreeColumn {
-	private static final Logger log = LoggerFactory.getLogger(TemplateColumn.class);
+    private static final Logger log = LoggerFactory.getLogger(TemplateColumn.class);
 
     private final TemplateDefinitionRegistry templateManager;
     Select templateSelect;
