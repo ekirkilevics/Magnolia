43486b03373f53013fe50c9bae459f7cfd97acce
Ondrej Chytil <ondrej.chytil@magnolia-cms.com>
2012-01-12 11:41:01 +0000

    MAGNOLIA-3937, MAGNOLIA-3940 Unused import.
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@53343 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-cache/src/main/java/info/magnolia/module/cache/filter/CacheFilter.java
+++ magnolia-module-cache/src/main/java/info/magnolia/module/cache/filter/CacheFilter.java
@@ -52,7 +52,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.exception.ExceptionUtils;
 
 import net.sf.ehcache.constructs.blocking.LockTimeoutException;
