734693528d854f49729ebecde732694ec1990824
Daniel Lipp <daniel.lipp@magnolia-cms.com>
2012-02-08 13:09:57 +0000

    MAGNOLIA-3962, SCRUM-929: removed code only required when upgrading from a version < 4.4.6
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@54162 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-workflow/src/main/java/info/magnolia/module/workflow/setup/WorkflowModuleVersionHandler.java
+++ magnolia-module-workflow/src/main/java/info/magnolia/module/workflow/setup/WorkflowModuleVersionHandler.java
@@ -33,7 +33,6 @@
  */
 package info.magnolia.module.workflow.setup;
 
-import info.magnolia.cms.core.Content;
 import info.magnolia.cms.security.Permission;
 import info.magnolia.module.DefaultModuleVersionHandler;
 import info.magnolia.module.InstallContext;
@@ -43,35 +42,16 @@ import info.magnolia.module.admininterface.trees.WebsiteTreeConfiguration;
 import info.magnolia.module.delta.AddPermissionTask;
 import info.magnolia.module.delta.AddRoleToGroupTask;
 import info.magnolia.module.delta.AddUserToGroupTask;
-import info.magnolia.module.delta.ArrayDelegateTask;
-import info.magnolia.module.delta.BackupTask;
 import info.magnolia.module.delta.BootstrapResourcesTask;
-import info.magnolia.module.delta.BootstrapSingleModuleResource;
-import info.magnolia.module.delta.BootstrapSingleResource;
 import info.magnolia.module.delta.CheckAndModifyPropertyValueTask;
-import info.magnolia.module.delta.ConditionalDelegateTask;
-import info.magnolia.module.delta.Delta;
-import info.magnolia.module.delta.DeltaBuilder;
 import info.magnolia.module.delta.IsModuleInstalledOrRegistered;
 import info.magnolia.module.delta.ModuleDependencyBootstrapTask;
-import info.magnolia.module.delta.NodeExistsDelegateTask;
-import info.magnolia.module.delta.PropertyValueDelegateTask;
-import info.magnolia.module.delta.RemovePermissionTask;
 import info.magnolia.module.delta.Task;
-import info.magnolia.module.delta.TaskExecutionException;
-import info.magnolia.module.delta.WarnTask;
-import info.magnolia.module.workflow.setup.for3_5.AddNewDefaultConfig;
-import info.magnolia.module.workflow.setup.for3_5.CheckAndUpdateDefaultWorkflowDefinition;
-import info.magnolia.module.workflow.setup.for3_5.RemoveMetadataFromExpressionsWorkspace;
-import info.magnolia.module.workflow.setup.for3_5.SetDefaultWorkflowForActivationFlowCommands;
 import info.magnolia.module.workflow.trees.WorkflowWebsiteTreeConfiguration;
 import info.magnolia.repository.RepositoryConstants;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.jcr.ImportUUIDBehavior;
-import javax.jcr.RepositoryException;
 
 /**
  * The {@link info.magnolia.module.ModuleVersionHandler} for the workflow module.
@@ -116,97 +96,6 @@ public class WorkflowModuleVersionHandler extends DefaultModuleVersionHandler {
                     "info.magnolia.module.dms.WorkflowDMSAdminTreeConfig"
             ));
 
-
-    public WorkflowModuleVersionHandler() {
-        final Delta delta35 = DeltaBuilder.update("3.5", "")
-        .addTask(inboxMenu)
-        .addTask(flowsPageMenu)
-        .addTask(new BootstrapSingleResource("Bootstrap", "Bootstraps the Workflow Utils page.", "/mgnl-bootstrap/workflow/config.modules.workflow.pages.flows.xml"))
-        .addTask(new BootstrapSingleResource("Bootstrap", "Bootstraps the Inbox Subpages.", "/mgnl-bootstrap/workflow/config.modules.workflow.pages.inboxSubPages.xml"))
-        // TODO refactor the following ArrayDelegateTask into a single one: BackupAndBootstrap("resource.xml", "/backup/location", "Message", MessageType.WARNING), will be fixed with MAGNOLIA-1945
-        .addTask(new ArrayDelegateTask("Backup and Bootstrap", "Makes a backup of the current 'EditWorkItem' dialog and re-installs it.", new Task[] {
-                new BackupTask(RepositoryConstants.CONFIG, "/modules/workflow/dialogs/editWorkItem", true),
-                new BootstrapSingleResource("Bootstrap", "Bootstraps the Inbox Subpages.", "/mgnl-bootstrap/workflow/config.modules.workflow.dialogs.editWorkItem.xml", ImportUUIDBehavior.IMPORT_UUID_CREATE_NEW),
-                new WarnTask("Note: ", "Note that the 'EditWorkItem' dialog was re-installed. Magnolia put a backup of original dialog into '" + BACKUP_PATH + "/editWorkItem'. Please review the changes manually.")
-        }))
-        .addTask(new BootstrapSingleResource("Bootstrap", "Bootstraps the 'EditActivationWorkItem' dialog", "/mgnl-bootstrap/workflow/config.modules.workflow.dialogs.editActivationWorkItem.xml"))
-        .addTask(new AddNewDefaultConfig())
-        .addTask(new CheckAndUpdateDefaultWorkflowDefinition())
-        .addTask(new RemoveMetadataFromExpressionsWorkspace())
-        .addTask(new SetDefaultWorkflowForActivationFlowCommands())
-        .addTask(bootstrapSecurityConfig)
-        // TODO we might want to backup the following configurations before overwriting them, as well.
-        .addTask(new BootstrapSingleResource("Bootstrap", "Bootstraps the 'editors' group.", "/mgnl-bootstrap/workflow/usergroups.editors.xml", ImportUUIDBehavior.IMPORT_UUID_COLLISION_REPLACE_EXISTING))
-        .addTask(new BootstrapSingleResource("Bootstrap", "Bootstraps the 'publishers' group.", "/mgnl-bootstrap/workflow/usergroups.publishers.xml", ImportUUIDBehavior.IMPORT_UUID_COLLISION_REPLACE_EXISTING))
-        .addTask(new BootstrapSingleResource("Bootstrap", "Bootstraps the 'workflow-base' role.", "/mgnl-bootstrap/workflow/userroles.workflow-base.xml"));
-        register(delta35);
-
-        register(DeltaBuilder.update("3.6", "")
-                .addTask(changeWebsiteTreeConfigurationTask)
-                .addTask(changeDMSTreeConfigurationTask)
-                .addTask(new BootstrapSingleResource("Comment on activation dialog", "Registers the dialog for activation comments.", "/mgnl-bootstrap/workflow/config.modules.workflow.dialogs.startActivationWorkflow.xml"))
-                .addTask(new IsModuleInstalledOrRegistered("Sample users and groups", "Adds sample users to sample editors and publishers group, if the sample module is installed or registered.",
-                        "samples", new ArrayDelegateTask("",
-                                new AddUserToGroupTask("Sample user", "eve", "editors"),
-                                new AddUserToGroupTask("Sample user", "patrick", "publishers"))
-                )));
-
-        register(DeltaBuilder.update("3.6.4", "")
-                .addTask(new ArrayDelegateTask("Update tree configuration",
-                        new BootstrapSingleResource("Update tree configuration", "Adds Store workspace tree configuration.", "/mgnl-bootstrap/workflow/config.modules.adminInterface.trees.Store.xml"),
-                        new BootstrapSingleResource("Update tree configuration", "Adds Expressions workspace tree configuration.", "/mgnl-bootstrap/workflow/config.modules.adminInterface.trees.Expressions.xml")))
-        );
-        register(DeltaBuilder.update("4.0", "")
-                .addTask(new AddUserToGroupTask("Superuser", "superuser", "publishers"))
-        );
-        register(DeltaBuilder.update("4.2", "")
-                // while this is checked and changed by DMS in 1.4 (bundled w/ Mgnl 4.1), there was a brief period of time (between 4.1 to 4.2) when workflow might have had overridden it.
-                // if the property exists with a wrong value, fix it otherwise let it be as someone might have just removed the versioning or it uses custom or correct command
-                .addTask(new IsModuleInstalledOrRegistered("Versioning", "Update of DMS specific versioning command.", "dms",
-                        new PropertyValueDelegateTask("", "", RepositoryConstants.CONFIG, "/modules/dms/commands/dms/activate/version", "class", "info.magnolia.module.admininterface.commands.VersionCommand", false,
-                                new CheckAndModifyPropertyValueTask("", "", RepositoryConstants.CONFIG, "/modules/dms/commands/dms/activate/version", "class", "info.magnolia.module.admininterface.commands.VersionCommand", "info.magnolia.module.dms.commands.DocumentVersionCommand"))))
-                                .addTask(new IsModuleInstalledOrRegistered("Activation update","Enable workflow use for activation of data entries.", "data",
-                                        new ConditionalDelegateTask("", "", new ArrayDelegateTask("","", new BackupTask(RepositoryConstants.CONFIG, "/modules/data/commands/data/activate"), new BootstrapSingleResource("","", "/info/magnolia/module/workflow/setup/data/config.modules.data.commands.data.activate.xml"))) {
-                                    @Override
-                                    protected boolean condition(InstallContext installContext) throws TaskExecutionException {
-                                        // the installation of the activation command is backported also to 1.2 branch, so it is possible that this particular activation command have been already bootstrapped. Do not backup & re-bootstrap it in this case again
-                                        try {
-                                            Content activation = installContext.getHierarchyManager(RepositoryConstants.CONFIG).getContentByUUID("a71a96a9-0c2b-4358-90f0-0be55e79361c");
-                                            return activation == null;
-                                        } catch (RepositoryException e) {
-                                            // doesn't exist, install
-                                            return true;
-                                        }
-                                    }
-                                }))
-        );
-
-        register(DeltaBuilder.update("4.2.3", "")
-                // MAGNOLIA-2971
-                .addTask(new AddPermissionTask("Update base role", "Adds permission to read the workflow definitions.", "workflow-base", "config", "/modules/workflow/config/flows", Permission.READ, true))
-        );
-
-        register(DeltaBuilder.update("4.3", "")
-                // TODO use node builder instead of overwriting the dialogs completely
-                .addTask(new BootstrapSingleModuleResource("Publication date","Add new controls to the activation dialog.", "config.modules.workflow.dialogs.startActivationWorkflow.xml"))
-                .addTask(new BootstrapSingleModuleResource("Publication date","Add new controls to the workitem dialog", "config.modules.workflow.dialogs.editActivationWorkItem.xml"))
-                .addTask(new RemovePermissionTask("Update workflow-base role", "Updates the workflow-base role, removing unnecessary permission to self.", "workflow-base", "userroles", "/workflow-base", Permission.READ)));
-
-        register(DeltaBuilder.update("4.3.3", "")
-                .addTask(new NodeExistsDelegateTask("StartActivationWorkflowDialog", "Checks if startActivationWorkflow node exists", RepositoryConstants.CONFIG, "/modules/workflow/dialogs/startActivationWorkflow", new CheckAndModifyPropertyValueTask("StartActivationWorkflowDialog", "Increases the height to 450px to prevent visual artefacts on FF on Linux", RepositoryConstants.CONFIG, "/modules/workflow/dialogs/startActivationWorkflow", "height", "400", "450")))
-        );
-        register(DeltaBuilder.update("4.4", "")
-                .addTask(new InstallWorkflowDefinitionTask("Setup default deactivation workflow definition", "Adds the default deactivation workflow definition under the /modules/workflow/config/flows/deactivation config node.", "deactivation", "info/magnolia/module/workflow/deactivation-workflow.xml"))
-                .addTask(new BootstrapSingleModuleResource("Deactivation","Adds new deactivation dialog.", "config.modules.workflow.dialogs.startDeactivationWorkflow.xml"))
-                .addTask(new BootstrapSingleModuleResource("Deactivation","Adds deactivation tab to the workitem dialog", "config.modules.workflow.dialogs.editDeactivationWorkItem.xml"))
-                .addTask(new IsModuleInstalledOrRegistered("Deactivation (DMS)","Adds deactivation command.","dms", new BootstrapSingleResource("Deactivate Command", "Installs deactivation command.", "/info/magnolia/module/workflow/setup/dms/config.modules.dms.commands.dms.deactivate.xml")))
-                .addTask(new IsModuleInstalledOrRegistered("Deactivation (Data)","Adds deactivation command.","data", new BootstrapSingleResource("Deactivate Command", "Installs deactivation command.", "/info/magnolia/module/workflow/setup/data/config.modules.data.commands.data.deactivate.xml")))
-        );
-
-        register(DeltaBuilder.update("4.4.1", "")
-                .addTask(new BootstrapSingleModuleResource("Deactivation (Website)","Adds deactivation command.", "config.modules.adminInterface.commands.website.deactivate.xml"))
-        );
-    }
 
     @Override
     protected List<Task> getExtraInstallTasks(InstallContext ctx) {
