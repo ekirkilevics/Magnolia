ekirkilevics/Magnolia

commit 8ebbf8c4f833d75af572e8fa11357906eed531d5

MAGNOLIA-3962, SCRUM-920: more cleanup
git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@54221 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

magnolia-core/src/main/java/info/magnolia/setup/initial/GenericTasks.java
@@ -92,8 +92,6 @@ public class GenericTasks {
                         "Updates the existing ip access rules to match the new configuration structure or bootstraps the new default configuration.",
                         "/mgnl-bootstrap/core/config.server.IPConfig.xml"),
 
-                new UpdateI18nConfiguration(),
-
                 new BootstrapSingleResource("New security configuration", "Install new configuration for security managers.", "/mgnl-bootstrap/core/config.server.security.xml"),
                 new BootstrapSingleResource("New rendering strategy for links", "Install new configuration for link resolving.", "/mgnl-bootstrap/core/config.server.rendering.linkManagement.xml"),
 
@@ -154,7 +152,6 @@ public class GenericTasks {
 
                 // --- system-wide tasks (impact all modules)
                 new WarnIgnoredModuleFilters(),
-                new RenamedRenderersToTemplateRenderers(),
                 new UpdateURIMappings()
         );
     }

magnolia-core/src/main/java/info/magnolia/setup/initial/RenamedRenderersToTemplateRenderers.java
@@ -1,74 +1,0 @@
-/**
- * This file Copyright (c) 2003-2011 Magnolia International
- * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
- *
- *
- * This file is dual-licensed under both the Magnolia
- * Network Agreement and the GNU General Public License.
- * You may elect to use one or the other of these licenses.
- *
- * This file is distributed in the hope that it will be
- * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
- * Redistribution, except as permitted by whichever of the GPL
- * or MNA you select, is prohibited.
- *
- * 1. For the GPL license (GPL), you can redistribute and/or
- * modify this file under the terms of the GNU General
- * Public License, Version 3, as published by the Free Software
- * Foundation.  You should have received a copy of the GNU
- * General Public License, Version 3 along with this program;
- * if not, write to the Free Software Foundation, Inc., 51
- * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * 2. For the Magnolia Network Agreement (MNA), this file
- * and the accompanying materials are made available under the
- * terms of the MNA which accompanies this distribution, and
- * is available at http://www.magnolia-cms.com/mna.html
- *
- * Any modifications to this file must keep this entire header
- * intact.
- *
- */
-package info.magnolia.setup.initial;
-
-import info.magnolia.cms.core.Content;
-import info.magnolia.cms.core.HierarchyManager;
-import info.magnolia.module.InstallContext;
-import info.magnolia.module.delta.AllModulesNodeOperation;
-import info.magnolia.module.delta.TaskExecutionException;
-
-import javax.jcr.RepositoryException;
-
-/**
- * Rename renderers to match name used since 3.5.
- *
- * @version $Id$
- */
-public class RenamedRenderersToTemplateRenderers extends AllModulesNodeOperation {
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(RenamedRenderersToTemplateRenderers.class);
-
-    private static final String OLDNAME = "renderers";
-    private static final String NEWNAME = "template-renderers";
-
-    public RenamedRenderersToTemplateRenderers() {
-        super("Template renderers", "Modules' renderers nodes were renamed to template-renderers.");
-    }
-
-    @Override
-    protected void operateOnModuleNode(Content parent, HierarchyManager hm, InstallContext ctx) throws TaskExecutionException {
-        final String moduleNodePath = parent.getHandle();
-        final String oldPath = moduleNodePath + "/" + OLDNAME;
-        final String newPath = moduleNodePath + "/" + NEWNAME;
-        if (hm.isExist(oldPath)) {
-            log.debug("Will move " + oldPath + " to " + newPath);
-            try {
-                hm.moveTo(oldPath, newPath);
-            } catch (RepositoryException e) {
-                ctx.error("Could not rename " + oldPath + " to " + newPath, e);
-            }
-        }
-    }
-
-}

magnolia-core/src/main/java/info/magnolia/setup/initial/UpdateI18nConfiguration.java
@@ -1,130 +1,0 @@
-/**
- * This file Copyright (c) 2003-2011 Magnolia International
- * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
- *
- *
- * This file is dual-licensed under both the Magnolia
- * Network Agreement and the GNU General Public License.
- * You may elect to use one or the other of these licenses.
- *
- * This file is distributed in the hope that it will be
- * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
- * Redistribution, except as permitted by whichever of the GPL
- * or MNA you select, is prohibited.
- *
- * 1. For the GPL license (GPL), you can redistribute and/or
- * modify this file under the terms of the GNU General
- * Public License, Version 3, as published by the Free Software
- * Foundation.  You should have received a copy of the GNU
- * General Public License, Version 3 along with this program;
- * if not, write to the Free Software Foundation, Inc., 51
- * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * 2. For the Magnolia Network Agreement (MNA), this file
- * and the accompanying materials are made available under the
- * terms of the MNA which accompanies this distribution, and
- * is available at http://www.magnolia-cms.com/mna.html
- *
- * Any modifications to this file must keep this entire header
- * intact.
- *
- */
-package info.magnolia.setup.initial;
-
-import info.magnolia.cms.core.Content;
-import info.magnolia.cms.core.HierarchyManager;
-import info.magnolia.cms.core.NodeData;
-import info.magnolia.cms.util.NodeDataUtil;
-import info.magnolia.content2bean.Content2BeanException;
-import info.magnolia.content2bean.Content2BeanUtil;
-import info.magnolia.module.InstallContext;
-import info.magnolia.module.delta.AbstractRepositoryTask;
-import info.magnolia.module.delta.BootstrapResourcesTask;
-import info.magnolia.module.delta.NodeExistsDelegateTask;
-import info.magnolia.module.delta.TaskExecutionException;
-
-import javax.jcr.RepositoryException;
-import java.util.Collection;
-import java.util.Iterator;
-
-/**
- * Updates pre 3.5 internationalization configuration to the format used since 3.5.
- *
- * @version $Id$
- */
-public class UpdateI18nConfiguration extends NodeExistsDelegateTask {
-    private static final String I18N_NODEPATH = "/server/i18n";
-    private static final String I18N_SYSTEM_NODEPATH = I18N_NODEPATH + "/system";
-
-    public UpdateI18nConfiguration() {
-        super("I18N configuration", "The I18N configuration has changed considerably in Magnolia 3.5. Will update the existing configuration or bootstrap a new one.",
-                "config", I18N_NODEPATH, new UpdateFrom30(), new BootstrapI18nConfig());
-    }
-
-    /**
-     * Bootstrap task for loading new i18n configuration.
-     */
-    final static class BootstrapI18nConfig extends BootstrapResourcesTask {
-        BootstrapI18nConfig() {
-            super(null, null);
-        }
-
-        @Override
-        protected String[] getResourcesToBootstrap(final InstallContext installContext) {
-            return new String[]{
-                    "/mgnl-bootstrap/core/config.server.i18n.content.xml",
-                    "/mgnl-bootstrap/core/config.server.i18n.system.xml"
-            };
-        }
-    }
-
-    /**
-     * Update task for bootstrapping new configuration and then merging it with existing configuration in the upgraded instance.
-     */
-    static class UpdateFrom30 extends AbstractRepositoryTask {
-        UpdateFrom30() {
-            super(null, null);
-        }
-
-        @Override
-        protected void doExecute(InstallContext ctx) throws RepositoryException, TaskExecutionException {
-            final HierarchyManager configHM = ctx.getConfigHierarchyManager();
-
-            // get existing values and remove node
-            final Content i18nNode = configHM.getContent(I18N_NODEPATH);
-            final String defaultLangIn30 = i18nNode.getNodeData("language").getString();
-            final Content availLangsNode = i18nNode.getContent("availableLanguages");
-            final Collection availLangsIn30;
-            try {
-                availLangsIn30 = Content2BeanUtil.toMap(availLangsNode, false).values();
-            } catch (Content2BeanException e) {
-                throw new TaskExecutionException("Couldn't read availableLanguages", e);
-            }
-            i18nNode.delete();
-
-            // bootstrap new defaults
-            doBootstrap(ctx);
-
-            // adapt with previously existing values
-            final Content newI18nSystemNode = configHM.getContent(I18N_SYSTEM_NODEPATH);
-            final NodeData sysFallbackLang = newI18nSystemNode.getNodeData("fallbackLanguage");
-            sysFallbackLang.setValue(defaultLangIn30);
-
-            final Collection languages = newI18nSystemNode.getContent("languages").getChildren();
-            final Iterator it = languages.iterator();
-            while (it.hasNext()) {
-                final Content langNode = (Content) it.next();
-                if (!availLangsIn30.contains(langNode.getName())) {
-                    final NodeData enabledProperty = NodeDataUtil.getOrCreate(langNode, "enabled");
-                    enabledProperty.setValue(false);
-                }
-            }
-        }
-
-        protected void doBootstrap(InstallContext ctx) throws TaskExecutionException {
-            new BootstrapI18nConfig().execute(ctx);
-        }
-    }
-}

commit b7f924348b933166e34242db0c0306c32e8b5526

MAGNOLIA-3962, SCRUM-920: fixed defects introduced with r54221 - as UpdateI18nConfiguration is no longer present the two bootstrap files must now be loaded "manually" (CoreModuleVersionHandler is not extending DefaultModuleVersionHandler!)
git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@54250 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

magnolia-core/src/main/java/info/magnolia/setup/initial/GenericTasks.java
@@ -38,6 +38,7 @@ import info.magnolia.cms.core.SystemProperty;
 import info.magnolia.cms.security.Realm;
 import info.magnolia.module.delta.ArrayDelegateTask;
 import info.magnolia.module.delta.BootstrapConditionally;
+import info.magnolia.module.delta.BootstrapSingleModuleResource;
 import info.magnolia.module.delta.BootstrapSingleResource;
 import info.magnolia.module.delta.CheckOrCreatePropertyTask;
 import info.magnolia.module.delta.CreateNodeTask;
@@ -91,6 +92,10 @@ public class GenericTasks {
                 new BootstrapSingleResource("IPConfig rules changed",
                         "Updates the existing ip access rules to match the new configuration structure or bootstraps the new default configuration.",
                         "/mgnl-bootstrap/core/config.server.IPConfig.xml"),
+
+                new BootstrapSingleModuleResource("i18n content", "bootstrap the config", "/mgnl-bootstrap/core/config.server.i18n.content.xml"),
+                new BootstrapSingleModuleResource("i18n system", "bootstrap the config", "/mgnl-bootstrap/core/config.server.i18n.system.xml"),
+
 
                 new BootstrapSingleResource("New security configuration", "Install new configuration for security managers.", "/mgnl-bootstrap/core/config.server.security.xml"),
                 new BootstrapSingleResource("New rendering strategy for links", "Install new configuration for link resolving.", "/mgnl-bootstrap/core/config.server.rendering.linkManagement.xml"),