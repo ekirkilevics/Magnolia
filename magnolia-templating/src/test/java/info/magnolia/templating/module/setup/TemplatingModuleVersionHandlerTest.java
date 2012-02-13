757a01b8e8543151abba1766f8e8ba1546b80a10
Daniel Lipp <daniel.lipp@magnolia-cms.com>
2012-02-13 08:05:05 +0000

    SCRUM-930: re-animated test be adapting to changed implementation of class under test
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@54324 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-templating/src/test/java/info/magnolia/templating/module/setup/TemplatingModuleVersionHandlerTest.java
+++ magnolia-templating/src/test/java/info/magnolia/templating/module/setup/TemplatingModuleVersionHandlerTest.java
@@ -33,13 +33,6 @@
  */
 package info.magnolia.templating.module.setup;
 
-import static org.junit.Assert.*;
-import info.magnolia.cms.core.Content;
-import info.magnolia.cms.core.HierarchyManager;
-import info.magnolia.cms.core.ItemType;
-import info.magnolia.cms.filters.BackwardCompatibilityFilter;
-import info.magnolia.cms.util.ContentUtil;
-import info.magnolia.context.MgnlContext;
 import info.magnolia.module.InstallContext;
 import info.magnolia.module.ModuleVersionHandler;
 import info.magnolia.module.ModuleVersionHandlerTestCase;
@@ -48,14 +41,9 @@ import info.magnolia.module.model.Version;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.jcr.RepositoryException;
-
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
- *
- * @author gjoseph
  * @version $Revision: $ ($Author: $)
  */
 public class TemplatingModuleVersionHandlerTest extends ModuleVersionHandlerTestCase {
@@ -77,43 +65,21 @@ public class TemplatingModuleVersionHandlerTest extends ModuleVersionHandlerTest
     }
 
     @Test
-    @Ignore //FIXME momentarily ignored
-    public void testSilentIfUserFixedTemplatesHimself() throws Exception {
-        // fake pre-install
-        setupConfigProperty("/server/filters/cms/rendering", "class", "info.magnolia.cms.filters.RenderingFilter"); // old RenderingFilter fqn
-        setupConfigProperty("/server/filters/cms/backwardCompatibility", "class", BackwardCompatibilityFilter.class.getName());
-        setupConfigNode("/server/rendering/freemarker/sharedVariables"); // this is assumed to have been created by CoreModuleVersionHandler
+    public void testInstallFrom4_4_6() throws Exception {
+        // GIVEN
+        setupConfigNode("/server/filters/cms/backwardCompatibility");
+        setupConfigNode("/modules/templating/template-renderers");
+        setupConfigNode("/modules/templating/paragraph-renderers");
+        setupConfigNode("/modules/test/templates/myTemplate");
 
-        // setup a template, with pre-4.0 properties
         final String tplPath = "/modules/test/templates/myTemplate";
-        setupTemplateNode("test", "myTemplate");
-        setupConfigProperty(tplPath, "title", "My Test Template");
-        // 4.0.2 and 4.0 wrongly moved the "templatePath" property under /parameters - here the user fixed it already
-        setupConfigProperty(tplPath, "templatePath", "/some/path.ftl");
-        setupConfigProperty(tplPath + "/parameters", "someProperty", "someValue");
+        setupConfigProperty(tplPath, "templatePath","/some/path.ftl");
 
-        final InstallContext ctx = executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("4.0.2"));
+        // WHEN
+        final InstallContext ctx = executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("4.4.6"));
 
-        final HierarchyManager hm = MgnlContext.getHierarchyManager("config");
-        assertConfig("My Test Template", tplPath + "/title");
-
-        // 4.5 renames templatePath to templateScript
+        // THEN
         assertConfig("/some/path.ftl", tplPath + "/templateScript");
-        // non-standard props are supposed to be moved to the "parameters" subnode
-        assertConfig("someValue", tplPath + "/parameters/someProperty");
-        assertFalse("path property should have been removed", hm.isExist(tplPath + "/path"));
-        assertFalse("path property should have been removed", hm.isExist(tplPath + "/parameters/path"));
-        assertFalse("path property should have been removed", hm.isExist(tplPath + "/parameters/templatePath"));
-        assertFalse("path property should have been renamed to templatePath and be in the template's def. node", hm.isExist(tplPath + "/parameters/templatePath"));
-
         assertNoMessages(ctx);
     }
-
-    private void setupTemplateNode(String moduleName, String templateNodeName) throws RepositoryException {
-        final HierarchyManager hm = MgnlContext.getHierarchyManager("config");
-        final Content content = ContentUtil.createPath(hm, "/modules/" + moduleName + "/templates", ItemType.CONTENT);
-        content.createContent(templateNodeName, ItemType.CONTENTNODE);
-        hm.save();
-    }
-
 }
