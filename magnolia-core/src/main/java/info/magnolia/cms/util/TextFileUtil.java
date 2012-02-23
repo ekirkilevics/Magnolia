5b4844b10087abf26410a592badcc06b5557e7e8
Daniel Lipp <daniel.lipp@magnolia-cms.com>
2012-02-23 14:49:19 +0000

    SCRUM-987: also check jaas.config, enhanced workspace.xml-check - will now also check workspace.xml's when repo is not at default location (reading the corresponding property now) - do jaas.config-check using new TextFileUtil + TextFileConditionUtil that can be reused for other text-files as well - simplified file-structure for current + outdated config files
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@54812 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-core/src/main/java/info/magnolia/cms/util/TextFileUtil.java
@@ -0,0 +1,88 @@
+/**
+ * This file Copyright (c) 2007-2011 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.cms.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Utility to check textFiles.
+ *
+ * @version $Id$
+ */
+public class TextFileUtil {
+
+    public static List<String> getTrimmedLinesMatching(String fileName, String regExp) {
+        final List<String> lines = getLines(fileName);
+        final List<String> matchingLines = new ArrayList<String>();
+        String currentLine = null;
+        for (Iterator<String> iterator = lines.iterator(); iterator.hasNext();) {
+            currentLine = iterator.next().trim();
+            if (currentLine.matches(regExp)) {
+                matchingLines.add(currentLine);
+            }
+        }
+        return matchingLines;
+    }
+
+    public static List<String> getLines(String fileName) {
+        final List<String> names = new ArrayList<String>();
+        BufferedReader jaasConfig = null;
+        try {
+            jaasConfig = new BufferedReader(new FileReader(fileName));
+
+            String dataRow = jaasConfig.readLine();
+
+            while (dataRow != null) {
+                names.add(dataRow);
+                dataRow = jaasConfig.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (jaasConfig != null) {
+                    jaasConfig.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return names;
+    }
+}
---
+++ magnolia-core/src/main/java/info/magnolia/module/delta/TextFileConditionsUtil.java
@@ -0,0 +1,66 @@
+/**
+ * This file Copyright (c) 2007-2011 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.module.delta;
+
+import info.magnolia.cms.util.TextFileUtil;
+
+import java.util.List;
+
+/**
+ * Utility adding conditions for textFiles depending on provided regExps.
+ *
+ * @version $Id$
+ */
+public class TextFileConditionsUtil {
+    private final List<Condition> conditions;
+
+    public TextFileConditionsUtil(List<Condition> conditions) {
+        this.conditions = conditions;
+    }
+
+    public void addFalseConditionIfExpressionIsNotContained(String fileName, String regExp) {
+        List<String> matches = TextFileUtil.getTrimmedLinesMatching(fileName, regExp);
+        if (matches.isEmpty()) {
+            conditions.add(new FalseCondition(fileName + " is missing required entries", "It should contain a line matching " + regExp + ". Please add it."));
+        }
+    }
+
+    public void addFalseConditionIfExpressionIsContained(String fileName, String regExp) {
+        List<String> matches = TextFileUtil.getTrimmedLinesMatching(fileName, regExp);
+        if (matches.size() > 0) {
+            conditions.add(new FalseCondition(fileName + " containes invalid entries", "The file contains "
+                    + matches.size() + " line(s) matching " + regExp + ". Please remove these lines."));
+        }
+    }
+}
