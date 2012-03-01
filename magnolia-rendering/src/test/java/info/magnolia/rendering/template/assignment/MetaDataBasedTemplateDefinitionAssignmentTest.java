6e8a3071e116f529b3faf38a5f6c2f6897d6ffdb
Tobias Mattsson <tobias.mattsson@magnolia-cms.com>
2012-03-01 15:41:40 +0000

    QA test for available templates
    git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@55368 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-rendering/src/test/java/info/magnolia/rendering/template/assignment/MetaDataBasedTemplateDefinitionAssignmentTest.java
+++ magnolia-rendering/src/test/java/info/magnolia/rendering/template/assignment/MetaDataBasedTemplateDefinitionAssignmentTest.java
@@ -33,20 +33,65 @@
  */
 package info.magnolia.rendering.template.assignment;
 
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import info.magnolia.cms.core.MetaData;
+import info.magnolia.cms.core.MgnlNodeType;
+import info.magnolia.context.MgnlContext;
+import info.magnolia.registry.RegistrationException;
 import info.magnolia.rendering.template.TemplateDefinition;
 import info.magnolia.rendering.template.registry.TemplateDefinitionProvider;
 import info.magnolia.rendering.template.registry.TemplateDefinitionRegistry;
 import info.magnolia.repository.RepositoryConstants;
+import info.magnolia.test.ComponentsTestUtil;
+import info.magnolia.test.mock.MockUtil;
 import info.magnolia.test.mock.jcr.MockNode;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-import org.junit.Test;
+import info.magnolia.test.mock.jcr.MockSession;
+import info.magnolia.test.mock.jcr.MockWorkspace;
 
 /**
  * @version $Id$
  */
 public class MetaDataBasedTemplateDefinitionAssignmentTest {
+
+    private static class SimpleTemplateDefinitionProvider implements TemplateDefinitionProvider {
+
+        private String id;
+        private TemplateDefinition templateDefinition;
+
+        private SimpleTemplateDefinitionProvider(String id, TemplateDefinition templateDefinition) {
+            this.id = id;
+            this.templateDefinition = templateDefinition;
+        }
+
+        @Override
+        public String getId() {
+            return id;
+        }
+
+        @Override
+        public TemplateDefinition getTemplateDefinition() throws RegistrationException {
+            return templateDefinition;
+        }
+
+        @Override
+        public String toString() {
+            return "SimpleTemplateDefinitionProvider [id=" + id + ", templateDefinition=" + templateDefinition + "]";
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        MgnlContext.setInstance(null);
+        ComponentsTestUtil.clear(); // MgnlContext.initMockContext() uses ComponentsTestUtil
+    }
 
     @Test
     public void testGetAssignedTemplateDefinition() throws Exception {
@@ -70,5 +115,58 @@ public class MetaDataBasedTemplateDefinitionAssignmentTest {
 
         // THEN
         assertEquals(templateDefinition, result);
+    }
+
+    @Test
+    public void testGetAvailableTemplatesForDeletedNode() {
+
+        MockNode mockNode = new MockNode();
+        mockNode.addMixin(MgnlNodeType.MIX_DELETED);
+
+        // GIVEN
+        TemplateDefinitionRegistry registry = new TemplateDefinitionRegistry();
+        MetaDataBasedTemplateDefinitionAssignment assignment = new MetaDataBasedTemplateDefinitionAssignment(registry);
+
+        TemplateDefinition deletedTemplate = mock(TemplateDefinition.class);
+        registry.register(new SimpleTemplateDefinitionProvider("adminInterface:mgnlDeleted", deletedTemplate));
+        registry.register(new SimpleTemplateDefinitionProvider("some:other/template/that/wont/be/returned", mock(TemplateDefinition.class)));
+
+        // WHEN
+        Collection<TemplateDefinition> availableTemplates = assignment.getAvailableTemplates(mockNode);
+
+        // TEST
+        assertEquals(1, availableTemplates.size());
+        assertSame(deletedTemplate, availableTemplates.iterator().next());
+    }
+
+    @Test
+    public void testGetAvailableTemplatesReturnsOnlyVisibleTemplates() {
+
+        MockUtil.initMockContext();
+        MockSession session = new MockSession(new MockWorkspace("website"));
+        MockUtil.setSessionAndHierarchyManager(session);
+
+        // GIVEN
+        TemplateDefinitionRegistry registry = new TemplateDefinitionRegistry();
+        MetaDataBasedTemplateDefinitionAssignment assignment = new MetaDataBasedTemplateDefinitionAssignment(registry);
+
+        MockNode mockNode = new MockNode(session);
+
+        TemplateDefinition visibleTemplate = mock(TemplateDefinition.class);
+        when(visibleTemplate.getVisible()).thenReturn(Boolean.TRUE);
+        when(visibleTemplate.getId()).thenReturn("module:pages/visibleTemplate");
+        registry.register(new SimpleTemplateDefinitionProvider("module:pages/visibleTemplate", visibleTemplate));
+
+        TemplateDefinition invisibleTemplate = mock(TemplateDefinition.class);
+        when(invisibleTemplate.getVisible()).thenReturn(Boolean.FALSE);
+        when(invisibleTemplate.getId()).thenReturn("module:pages/invisibleTemplate");
+        registry.register(new SimpleTemplateDefinitionProvider("module:pages/invisibleTemplate", invisibleTemplate));
+
+        // WHEN
+        Collection<TemplateDefinition> availableTemplates = assignment.getAvailableTemplates(mockNode);
+
+        // TEST
+        assertEquals(1, availableTemplates.size());
+        assertSame(visibleTemplate, availableTemplates.iterator().next());
     }
 }
