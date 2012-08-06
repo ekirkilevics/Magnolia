837a25b80718439573e707fc5decda34805b4aa2
Daniel Lipp <daniel.lipp@magnolia-cms.com>
2012-08-06 14:37:05 +0200

    QA: fixed build by removing obsolete import

--- magnolia-core/src/test/java/info/magnolia/test/mock/jcr/MockValueTest.java
+++ magnolia-core/src/test/java/info/magnolia/test/mock/jcr/MockValueTest.java
@@ -34,7 +34,6 @@
 package info.magnolia.test.mock.jcr;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.math.BigDecimal;
