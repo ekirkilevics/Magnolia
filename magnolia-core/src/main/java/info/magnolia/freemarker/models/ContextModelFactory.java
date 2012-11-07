ec45d81da47627e6bd945d76720c6386264ebf82
Jan Haderka <jan.haderka@magnolia-cms.com>
2012-11-07 14:47:26 +0100

    MAGNOLIA-4000 Update Copyright to 2012

--- magnolia-core/src/main/java/info/magnolia/freemarker/models/ContextModelFactory.java
+++ magnolia-core/src/main/java/info/magnolia/freemarker/models/ContextModelFactory.java
@@ -1,5 +1,5 @@
 /**
- * This file Copyright (c) 2009-2011 Magnolia International
+ * This file Copyright (c) 2009-2012 Magnolia International
  * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
  *
  *
--- magnolia-core/src/main/java/info/magnolia/registry/Provider.java
+++ magnolia-core/src/main/java/info/magnolia/registry/Provider.java
@@ -1,5 +1,5 @@
 /**
- * This file Copyright (c) 2010-2011 Magnolia International
+ * This file Copyright (c) 2010-2012 Magnolia International
  * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
  *
  *
